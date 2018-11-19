6e3b05d697df04b7db686841c1febc28b1c0b05e
duanchuanxu <duanchuanxu@xiaomi.com>
2018-11-19 09:07:42 +0800

    i18n

--- locales/en-US.js
+++ locales/en-US.js
@@ -4,10 +4,36 @@ module.exports = {
     dropdown: 'Dropdown',
     pagination: 'Pagination',
     tabs: 'Tabs',
-    table: 'Table'
+    table: 'Table',
+    'date-picker': 'DatePicker'
   },
   misc: {
     components: 'Basic Components'
+  },
+  datePicker: {
+    ok: 'OK',
+    placeholder: 'Select Date',
+    dateChoose: 'Select Date',
+    timeChoose: 'Select Time',
+    undefinedType: 'undefined type',
+    lastWeek: 'Nearly week',
+    lastMonth: 'Nearly month',
+    lastThreeMonth: 'Nearly three months',
+    lastYear: 'Nearly year',
+    month: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
+    week: [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ],
+    placeholders: {
+      date: 'Select Date',
+      month: 'Select Month',
+      year: 'Select Year',
+      time: 'Select Time',
+      daterange: 'Select Date',
+      week: 'Select Week',
+      weekrange: 'Select Week'
+    },
+    weekrange: function (year, week) {
+      return year + 'year ' + week + 'week'
+    }
   },
   pagination: {
     total: function (total) {
@@ -16,7 +42,19 @@ module.exports = {
     item: 'items',
     goto: 'Goto'
   },
+  cascader: {
+    placeholder: 'Please select',
+    noFoundTip: 'Not found'
+  },
+  select: {
+    placeholder: 'Please select',
+    noFoundTip: 'Not found'
+  },
   upload: {
-    'buttonText': 'upload'
+    buttonText: 'Upload'
+  },
+  modal: {
+    confirmText: 'OK',
+    cancelText: 'Cancel'
   }
 }
