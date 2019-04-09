50bf901eb28e3a5ec3f249e7afd1cfd938183bd7
solarjoker <chenissolar@gmail.com>
2019-04-09 19:51:09 +0800

    新增兄弟节点

--- components/tree/TreeNode.js
+++ components/tree/TreeNode.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import Checkbox from '../table/checkbox/index'
 import classNames from 'classnames'
-
+import isEqual from 'lodash/isEqual'
+import cloneDeep from 'lodash/cloneDeep'
 export default class TreeNode extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      highlight: null
+      highlight: null,
+      showRightClickMenu: null,
+      dataCache: [],
+      prevData: []
     }
   }
-
+  static getDerivedStateFromProps (props, state) {
+    console.log('************', state.dataCache)
+    if (!isEqual(props.data, state.prevData)) {
+      return {
+        ...state,
+        prevData: props.data,
+        dataCache: props.data
+      }
+    }
+    return state
+  }
   onDragEnter (item, data, e) {
     this.props.onDragEnter(e, item, data)
   }
@@ -28,7 +42,7 @@ export default class TreeNode extends Component {
 
   getItem (name, treeItem) {
     let has = false
-    this.props[name].map((item) => {
+    this.props[name].map(item => {
       if (treeItem.id === item) {
         has = true
       }
@@ -43,31 +57,84 @@ export default class TreeNode extends Component {
   onExpanded (expanded, item) {
     this.props.onExpanded(expanded, item)
   }
-  nodeClick = (item) => {
+  nodeClick = item => {
     this.props.onNodeClick(item)
   }
 
-  renderSwitcher = (expanded) => {
-    const { prefixCls, openIcon, closeIcon } = this.props
-    const switcherClsName = classNames(`${prefixCls}-switcher`, 'hi-icon', `icon-${expanded ? (closeIcon || 'minus') : (openIcon || 'plus')}`)
-    return (
-      <i className={switcherClsName} />
+  renderSwitcher = expanded => {
+    // const { prefixCls, openIcon, closeIcon } = this.props
+    const { prefixCls } = this.props
+    const switcherClsName = classNames(
+      `${prefixCls}-switcher`,
+      'hi-icon',
+      `icon-${expanded ? 'down' : 'right'}`
     )
+    return <i className={switcherClsName} />
   }
 
   renderItemIcon = () => {
     const { prefixCls, itemIcon } = this.props
-    const switcherClsName = classNames(`${prefixCls}-switcher`, 'hi-icon', `icon-${itemIcon || 'document'}`)
-    return (
-      <i
-        className={switcherClsName}
-      />
+    const switcherClsName = classNames(
+      `${prefixCls}-switcher`,
+      'hi-icon',
+      `icon-${itemIcon || 'document'}`
     )
+    return <i className={switcherClsName} />
   }
 
+  // 添加兄弟节点
+  _addSibNode = (itemId, data) => {
+    data.forEach((d, index) => {
+      if (d.id === itemId) {
+        data.splice(index + 1, 0, { id: '', title: '测试' })
+      } else {
+        if (d.children) {
+          this._addSibNode(itemId, d.children)
+        }
+      }
+    })
+  }
+
+  addSiblingNode = itemId => {
+    console.log('^^^', itemId)
+    const { dataCache } = this.state
+    console.log(dataCache)
+    const _dataCache = cloneDeep(dataCache)
+    this._addSibNode(itemId, _dataCache)
+    console.log(_dataCache)
+    this.setState({ dataCache: _dataCache })
+  }
+  // 添加子节点
+  addChildNode = itemId => {}
+  // 编辑节点
+  editNode = itemId => {}
+  // 删除节点
+  deleteNode = itemId => {}
+  // 渲染右键菜单
+  renderRightClickMenu = itemId => {
+    return (
+      itemId === this.state.showRightClickMenu &&
+      <ul className='right-click-menu'>
+        <li onClick={() => this.addSiblingNode(itemId)}>添加节点</li>
+        <li onClick={() => this.addChildNode()}>添加子节点</li>
+        <li onClick={() => this.editNode()}>编辑</li>
+        <li onClick={() => this.deleteNode()}>删除</li>
+      </ul>
+    )
+  }
   renderTree (data) {
-    const {draggable, prefixCls, dragNodePosition, dragNode, withLine, semiChecked, onNodeClick, onClick, highlightable} = this.props
-    const {highlight} = this.state
+    const {
+      draggable,
+      prefixCls,
+      dragNodePosition,
+      dragNode,
+      withLine,
+      semiChecked,
+      onNodeClick,
+      onClick,
+      highlightable
+    } = this.props
+    const { highlight } = this.state
     return (
       <ul>
         {data.map(item => {
@@ -81,52 +148,83 @@ export default class TreeNode extends Component {
             this.props.checkable && 'has_checkbox'
           )
 
-          const itemContainerStyle = classNames(
-            withLine && 'with-line'
+          const itemContainerStyle = classNames(withLine && 'with-line')
+
+          return (
+            <li
+              onDragStart={this.onDragStart.bind(this, item, data)}
+              onDragEnter={this.onDragEnter.bind(this, item, data)}
+              onDragOver={this.onDragOver.bind(this, item, data)}
+              onDragLeave={this.onDragLeave.bind(this, item, data)}
+              onDrop={this.onDrop.bind(this, item, data)}
+              draggable={draggable}
+              key={item.id}
+              className={itemContainerStyle}
+            >
+              <span
+                onClick={() => this.onExpanded(expanded, item)}
+                className={`${prefixCls}_item-icon`}
+              >
+                {item.children && item.children.length > 0
+                  ? this.renderSwitcher(expanded)
+                  : withLine && this.renderItemIcon()}
+              </span>
+
+              {this.props.checkable
+                ? <Checkbox
+                  semi={semiChecked.includes(item.id)}
+                  checked={checked}
+                  onChange={() => this.onCheckChange(checked, item)}
+                  onTitleClick={e => {
+                    onNodeClick && onNodeClick(item)
+                    onClick && onClick(item)
+                    highlightable &&
+                        this.setState({
+                          highlight: item.id
+                        })
+                    e.stopPropagation()
+                  }}
+                  highlight={highlight === item.id}
+                  text={item.title}
+                  disabled={item.disabled}
+                />
+                : <span
+                  style={item.style}
+                  className={`${prefixCls}_item-text ${itemStyle} ${highlight === item.id
+                    ? 'highlight'
+                    : ''}`}
+                  onContextMenu={e => {
+                    //
+                    // if (this.props.editable) {
+                    //   e.preventDefault()
+                    // }
+                    e.preventDefault()
+                    this.setState({
+                      showRightClickMenu: item.id,
+                      highlight: item.id
+                    })
+                  }}
+                  onClick={e => {
+                    this.setState({
+                      showRightClickMenu: null
+                    })
+                    onNodeClick && onNodeClick(item)
+                    onClick && onClick(item)
+                    highlightable &&
+                        this.setState({
+                          highlight: item.id
+                        })
+                    e.stopPropagation()
+                  }}
+                >
+                  {this.renderText(item.title)}
+                  {this.renderRightClickMenu(item.id)}
+                </span>}
+              {item.children && item.children.length > 0 && expanded
+                ? this.renderTree(item.children)
+                : null}
+            </li>
           )
-
-          return (<li
-            onDragStart={this.onDragStart.bind(this, item, data)}
-            onDragEnter={this.onDragEnter.bind(this, item, data)}
-            onDragOver={this.onDragOver.bind(this, item, data)}
-            onDragLeave={this.onDragLeave.bind(this, item, data)}
-            onDrop={this.onDrop.bind(this, item, data)}
-            draggable={draggable}
-            key={item.id}
-            className={itemContainerStyle}
-          >
-            <span onClick={() => this.onExpanded(expanded, item)} className={`${prefixCls}_item-icon`}>{item.children && item.children.length > 0 ? this.renderSwitcher(expanded) : (withLine && this.renderItemIcon())}</span>
-
-            {this.props.checkable ? <Checkbox
-              semi={semiChecked.includes(item.id)}
-              checked={checked}
-              onChange={() => this.onCheckChange(checked, item)}
-              onTitleClick={(e) => {
-                onNodeClick && onNodeClick(item)
-                onClick && onClick(item)
-                highlightable && this.setState({
-                  highlight: item.id
-                })
-                e.stopPropagation()
-              }}
-              highlight={highlight === item.id}
-              text={item.title}
-              disabled={item.disabled} />
-              : <span
-                style={item.style}
-                className={`${prefixCls}_item-text ${itemStyle} ${highlight === item.id ? 'highlight' : ''}`}
-                onClick={(e) => {
-                  onNodeClick && onNodeClick(item)
-                  onClick && onClick(item)
-                  highlightable && this.setState({
-                    highlight: item.id
-                  })
-                  e.stopPropagation()
-                }}
-              >{this.renderText(item.title)}</span>
-            }
-            {item.children && item.children.length > 0 && expanded ? this.renderTree(item.children) : null}
-          </li>)
         })}
       </ul>
     )
@@ -135,10 +233,10 @@ export default class TreeNode extends Component {
     return text
   }
   render () {
-    const {data} = this.props
+    const { dataCache } = this.state
     return (
       <div>
-        {this.renderTree(data)}
+        {this.renderTree(dataCache)}
       </div>
     )
   }
