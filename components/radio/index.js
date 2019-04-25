3020c74acc9c4c95ace5edd8778199203868112b
solarjoker <chenissolar@gmail.com>
2019-04-25 11:42:17 +0800

    修复问题

--- components/radio/index.js
+++ components/radio/index.js
@@ -48,7 +48,7 @@ class Radio extends Component {
     }
   }
   componentDidMount () {
-    let {checked, list, disabled} = this.props
+    let { checked, list, disabled } = this.props
     this.setState(parse(checked, list, disabled))
   }
   static propTypes = {
@@ -69,16 +69,18 @@ class Radio extends Component {
     buttonStyle: 'default'
   }
   componentWillReceiveProps (props, state) {
-    this.setState({
-      checked: parse(props.checked, props.list, props.disabled).checked
-    })
+    if (props.checked !== this.props.checked) {
+      this.setState({
+        checked: parse(props.checked, props.list, props.disabled).checked
+      })
+    }
   }
   generateUUID () {
     var d = new Date().getTime()
     var uuid = 'xxxxxxxx'.replace(/[xy]/g, function (c) {
       var r = (d + Math.random() * 16) % 16 | 0
       d = Math.floor(d / 16)
-      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+      return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
     })
     return uuid
   }
@@ -86,17 +88,20 @@ class Radio extends Component {
     // console.log(labelText, index, event)
     // event.stopPropagation()
     if (disabled) return
-    this.setState({
-      checked: index
-    }, () => {
-      if (this.props.onChange) {
-        this.props.onChange(item.id || item.name, index, item)
+    this.setState(
+      {
+        checked: index
+      },
+      () => {
+        if (this.props.onChange) {
+          this.props.onChange(item.id || item.name, index, item)
+        }
       }
-    })
+    )
   }
   render () {
-    let {list, align, layout, mode, buttonStyle} = this.props
-    const {checked, disabled} = this.state
+    let { list, align, layout, mode, buttonStyle } = this.props
+    const { checked, disabled } = this.state
     const cls = classnames(
       'hi-radio',
       layout === 'vertical' && 'hi-radio--vertical',
@@ -104,38 +109,47 @@ class Radio extends Component {
     )
     return (
       <div className={cls}>
-        {
-          list.map((item, index) => {
-            let _item = item
-            if (typeof item === 'string') {
-              _item = {
-                name: item,
-                id: ''
-              }
+        {list.map((item, index) => {
+          let _item = item
+          if (typeof item === 'string') {
+            _item = {
+              name: item,
+              id: ''
             }
-            const _disabled = disabled.indexOf(index) > -1
-            const itemCls = classnames(
-              'hi-radio__item',
-              _disabled && 'hi-radio__item--disabled',
-              checked === index && 'hi-radio__item--checked'
-            )
-            const eles = [
-              <span className='hi-radio__label' key={'label' + index}> {_item.name} </span>,
-              <span className='hi-radio__simulation-input' key={'input' + index} />
-            ]
-            return (
-              <div className={itemCls} key={index} onClick={this.handleChange.bind(this, _item, index, _disabled)}>
-                {
-                  mode === 'normal'
-                    ? <React.Fragment>
-                      {align === 'left' ? eles : eles.reverse()}
-                    </React.Fragment>
-                    : <Button type={buttonStyle} disabled={_disabled} onClick={(e) => this.handleChange.bind(this, _item, index, _disabled)}>{_item.name}</Button>
-                }
-              </div>
-            )
-          })
-        }
+          }
+          const _disabled = disabled.indexOf(index) > -1
+          const itemCls = classnames(
+            'hi-radio__item',
+            _disabled && 'hi-radio__item--disabled',
+            checked === index && 'hi-radio__item--checked'
+          )
+          const eles = [
+            <span className='hi-radio__label' key={'label' + index}>
+              {' '}
+              {_item.name}{' '}
+            </span>,
+            <span className='hi-radio__simulation-input' key={'input' + index} />
+          ]
+          return (
+            <div
+              className={itemCls}
+              key={index}
+              onClick={this.handleChange.bind(this, _item, index, _disabled)}
+            >
+              {mode === 'normal' ? (
+                <React.Fragment>{align === 'left' ? eles : eles.reverse()}</React.Fragment>
+              ) : (
+                <Button
+                  type={buttonStyle}
+                  disabled={_disabled}
+                  onClick={e => this.handleChange.bind(this, _item, index, _disabled)}
+                >
+                  {_item.name}
+                </Button>
+              )}
+            </div>
+          )
+        })}
       </div>
     )
   }
