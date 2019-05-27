362df6aa65a6851c3fb1810bb8d3de1b004fc8de
bougieL <1742070326@qq.com>
2019-05-27 09:36:29 +0800

    feat: update prerender

--- build/prerenderSite.js
+++ build/prerenderSite.js
@@ -2,39 +2,42 @@ const path = require('path')
 const fs = require('fs')
 const {
   createSPAServer,
-  Renderer,
+  renderUrlsToString,
   getRelativePathFromUrl,
   writeFile
 } = require('@bougiel/puppeteer-prerenderer')
 
 const dist = path.resolve(__dirname, '../dist')
 const port = 4200
+const baseRoute = '/hiui'
 
 const server = createSPAServer({
-  path: dist,
+  dist,
   port,
-  base: '/hiui',
-  callback: renderUrls
+  base: baseRoute,
+  onCreated: renderUrls
 })
 
 async function renderUrls () {
-  const renderer = new Renderer()
-  await renderer.init()
-  for (const url of getUrls()) {
-    await render(url, renderer)
-  }
-  server.close()
-  console.log(`render completed.`)
-  process.exit(0)
+  renderUrlsToString({
+    urls: getUrls(),
+    onItemRendered: render,
+    onFinished () {
+      server.close()
+      process.exit(0)
+    }
+  })
 }
 
-async function render (url, renderer) {
-  const content = await renderer.renderToString(url)
-  const dir = getRelativePathFromUrl(url).replace('/hiui', '')
+async function render (content, url) {
+  const dir = getRelativePathFromUrl(url).replace(baseRoute, '')
   writeFile(path.join(dist, dir), content)
-  console.log(`rendered ${url}`)
 }
 
+/**
+ * get all prerender needed urls.
+ * @returns {string[]}
+ */
 function getUrls () {
   const urls = [...getComponentUrls(), ...getTemplateUrls(), ...getExtraUrls()]
   const zh = urls.map(v => `http://localhost:${port}/hiui/zh-CN/${v}`)
@@ -42,18 +45,31 @@ function getUrls () {
   return [...zh, ...en]
 }
 
+/**
+ * get components dir pages.
+ * @returns {string[]}
+ */
 function getComponentUrls () {
   const compPath = path.resolve(__dirname, '../docs/zh-CN/components')
   return fs.readdirSync(compPath).map(v => `docs/${v.replace('.md', '')}`)
 }
 
+/**
+ * get templates dir pages.
+ * @returns {string[]}
+ */
 function getTemplateUrls () {
   const tempPath = path.resolve(__dirname, '../docs/zh-CN/templates')
   return fs.readdirSync(tempPath).map(v => `templates/${v.replace('.md', '')}`)
 }
 
+/**
+ * get non-regular pages
+ * @returns {string[]}
+ */
 function getExtraUrls () {
   return [
+    '',
     'docs/quick-start',
     'docs/template',
     'docs/theme',
