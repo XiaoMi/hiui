703164aab214ba7165e02a06f6b16dfe8b336445
surui1 <surui1@xiaomi.com>
2019-06-06 10:53:17 +0800

    ## 增加表格功能 1. 增加表格头部 2. 增加表格底部 3. 增加点击高亮某行 4. 增加从底部插入数据 5. 增加从头部插入数据

--- components/table/header.js
+++ components/table/header.js
@@ -16,17 +16,15 @@ class Sorter extends Component {
   }
 
   sortDsec = () => {
-    if (!this.reversed) {
-      let {index, columns, name, kname} = this.props
-      let sorter = columns[index].sorter
-      let {cbs: {resetData}, dataSource} = this.props
+    let {index, columns, name, kname} = this.props
+    let sorter = columns[index].sorter
+    let {cbs: {resetData}, dataSource} = this.props
 
-      dataSource.sort(sorter)
-      dataSource.reverse()
-      resetData(dataSource)
-      this.reversed = true
-      window.localStorage.setItem(name + '-sorter', [kname, 1])
-    }
+    dataSource.sort(sorter)
+    dataSource.reverse()
+    resetData(dataSource)
+    this.reversed = true
+    window.localStorage.setItem(name + '-sorter', [kname, 1])
   }
 
   render () {
