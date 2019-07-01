eb2274099b919a00e6dfe943920a4ad396f4372d
solarjoker <chenissolar@gmail.com>
2019-07-01 15:07:27 +0800

    update doc

--- site/pages/components/transfer/index.jsx
+++ site/pages/components/transfer/index.jsx
@@ -1,10 +1,5 @@
-
 import React from 'react'
 import Doc from './index.mdx'
 
-const DocWrapper = () => (
-  <div className='doc-md'>
-    <Doc />
-  </div>
-)
+const DocWrapper = () => <Doc />
 export default DocWrapper
