0e528534a32ba227b1478bd4cf966b8fd9d918b6
Bougie <1742070326@qq.com>
2019-07-12 14:37:34 +0800

    chore: run fix

--- components/_util/depreactedPropsCompat.js
+++ components/_util/depreactedPropsCompat.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 /**
- *
  * compat old api
+ * 参数为一个二维数组，里面的一维数组的第一个参数为新 prop，第二个参数为旧 prop，第三个参数为转换函数，
+ * 回调值为旧 prop 传入的值。使用示例参考 Stepper 组件
  * @export
  * @param {[[string, string, Function], [string, string, Function]]} compatPair
  * @returns
