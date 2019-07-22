2d73f85a9d3b2027ca415f366bc0097af62fd713
solarjoker <chenissolar@gmail.com>
2019-07-22 16:28:50 +0800

    add version tag

--- site/view/index.js
+++ site/view/index.js
@@ -8,6 +8,7 @@ import designs from '../pages/designs'
 import pages from '../pages/components'
 import templates from '../pages/templates'
 import utils from '../utils'
+import { version } from '../../package.json'
 import {
   setDesignNavs,
   setComponentsNavs,
@@ -20,7 +21,7 @@ const logo = (
   <Logo
     url='<BASE_URL>/'
     logoUrl='<BASE_URL>/static/img/logo.png'
-    text='HIUI'
+    text={<div>HIUI<span className='version'>{`v${version}`}</span></div>}
     title='HIUI'
     alt='HIUI'
   />
