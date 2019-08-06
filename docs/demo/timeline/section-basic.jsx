c383c0bb0a3f8ee0fdb5ea706073769fbe16076c
solarjoker <chenissolar@gmail.com>
2019-08-06 15:36:18 +0800

    docs: fix timeline doc

--- docs/demo/timeline/section-basic.jsx
+++ docs/demo/timeline/section-basic.jsx
@@ -11,12 +11,12 @@ class Demo extends React.Component {
         groupTitle: '2月',
         children: [{
           title: 'Title - 1',
-          description: 'Here are some descriptions',
+          content: 'Here are some descriptions',
           timestamp: '10:00',
           extraTime: '02-23'
         }, {
           title: 'Title 2',
-          description: 'Here are some descriptions',
+          content: 'Here are some descriptions',
           timestamp: '10:00',
           extraTime: '02-27'
         }]
@@ -24,12 +24,12 @@ class Demo extends React.Component {
         groupTitle: '3月',
         children: [{
           title: 'Title 3',
-          description: 'Here are some descriptions',
+          content: 'Here are some descriptions',
           timestamp: '12:00',
           extraTime: '03-02'
         }, {
           title: 'Title 4',
-          description: 'Here are some descriptions',
+          content: 'Here are some descriptions',
           timestamp: '11:00',
           extraTime: '03-10'
         }]
