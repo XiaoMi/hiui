dfa7a2543a593dcba0cc3a82351bba327bd681ae
zhangjunjie <zhangjunjie1@xiaomi.com>
2019-08-28 18:17:51 +0800

    update

--- docs/demo/carousel/section-icon.jsx
+++ docs/demo/carousel/section-icon.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import DocViewer from '../../../libs/doc-viewer'
 import Carousel from '../../../components/carousel'
-const prefix = 'alert-base'
+const prefix = 'carousel-base'
 const code = `import React from 'react'
 import Carousel from '@hi-ui/hiui/es/carousel'\n
 class Demo extends React.Component {
