4afcd873396d4b6e2e39b8db3a37f59e9090f41a
solarjoker <chenissolar@gmail.com>
2019-10-25 10:35:18 +0800

    feat: select add onSearch

--- docs/demo/select/section-async-multiple.jsx
+++ docs/demo/select/section-async-multiple.jsx
@@ -5,27 +5,39 @@ const prefix = 'select-async-multiple'
 const code = `import React from 'react'
 import { Select } from '@hi-ui/hiui'\n
 class Demo extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      multipleList: [
+
+      ]
+    }
+  }
   render () {
     return (
       <Select
-        type='multiple'
-        autoload
-        style={{ width: 300 }}
-        defaultValue={1}
-        dataSource={{
-          type: 'get',
-          key: 'text',
-          keyword: 'xiaomi',
-          url: 'https://easy-mock.com/mock/5c1b42e3fe5907404e6540e9/hiui/select/options',
-          transformResponse: (res) => {
-            console.log('----', res)
-            return res.data
-          },
-          error: err => console.log('error:', err)
-        }}
-        onChange={(item) => {
-          console.log('异步多选结果', item)
-        }}
+      type='multiple'
+      style={{width: '300px'}}
+      data={this.state.multipleList}
+      placeholder='请选择...'
+      onSearch={(value) => {
+        return Promise.resolve(value).then((value)=>{
+          this.setState({multipleList:[
+            { title:'手机', id:'2' },
+            { title:'小米2', id:'2-1' },
+            { title:'小米3', id:'2-2' },
+            { title:'小米4', id:'2-3' },
+            { title:'小米5', id:'2-4' },
+            { title:'电脑', id:'3' },
+            { title:'笔记本', id:'4' },
+            { title:'生活周边', id:'5' },
+            { title:'其它', id:'6' }
+          ].splice(0,value)})
+        })
+      }}
+      onChange={(item) => {
+        console.log('多选结果', item)
+      }}
       />
     )
   }
