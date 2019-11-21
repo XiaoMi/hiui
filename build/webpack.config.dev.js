54061a073180de2bbce51f29c8a47b88518cc673
Wugaoliang <wugaoliang@xiaomi.com>
2019-11-21 14:42:32 +0800

    fix: upload bug

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -19,7 +19,7 @@ module.exports = merge(baseConfig, {
     new webpack.HotModuleReplacementPlugin()
   ],
   devServer: {
-    host: '10.221.68.181',
+    host: '0.0.0.0',
     port: 4200,
     disableHostCheck: false,
     hot: true,
