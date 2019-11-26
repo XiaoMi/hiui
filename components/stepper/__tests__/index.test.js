86ed9b888c2c77aa01001abd7aeb4b3d74e2f293
hzw <hzw@hzwdeiMac.local>
2019-11-26 09:52:24 +0800

    feat(stepper): 新增单元测试

--- components/stepper/__tests__/index.test.js
+++ components/stepper/__tests__/index.test.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { mount } from 'enzyme'
 import Stepper from '../index'
 
@@ -95,5 +95,77 @@ describe('stepper',()=>{
 
         })
     })
+    describe('oldProps',()=>{
+
+      // data
+      it('list',()=>{
+          const list1 = [
+              {
+                title: '账号信息',
+              },
+              {
+                title: '邮箱激活',
+              }
+            ]
+          
+          const wrapper = mount(<Stepper list={list1}/>)
+          expect(wrapper.find('li')).toHaveLength(2)
+          wrapper.setProps({list:list2})
+          expect(wrapper.find('li')).toHaveLength(3)
+
+      })
+
+      // placement
+      it('vertical',()=>{
+          const list = [
+              {
+                title: '账号信息',
+                content: '请输入账号信息',
+              },
+              {
+                title: '邮箱激活',
+                content: '请输入邮箱',
+              },
+              {
+                title: '信息登记',
+                content: '请输入个人信息',
+              },
+            ]
+        
+          const wrapper = mount(<Stepper list={list} vertical/>)
+          expect(wrapper.find('.hi-stepper').hasClass('hi-stepper--horizontal')).toBeFalsy()
+          expect(wrapper.find('.hi-stepper').hasClass('hi-stepper--vertical')).toBeTruthy()
+
+          wrapper.setProps({vertical:false})
+          expect(wrapper.find('.hi-stepper').hasClass('hi-stepper--horizontal')).toBeTruthy()
+          expect(wrapper.find('.hi-stepper').hasClass('hi-stepper--vertical')).toBeFalsy()
+
+      })
+
+      // itemLayout
+      it('up',()=>{
+          const list = [
+              {
+                title: '账号信息',
+                content: '请输入账号信息',
+                icon:'111'
+              },
+              {
+                title: '邮箱激活',
+                content: '请输入邮箱',
+              },
+              {
+                title: '信息登记',
+                content: '请输入个人信息',
+              },
+            ]
+          const wrapper = mount(<Stepper list={list} up/>)
+          expect(wrapper.find('.hi-stepper__item--up')).toHaveLength(3)
+
+          wrapper.setProps({up:false})
+          expect(wrapper.find('.hi-stepper__item--up')).toHaveLength(0)
+
+      })
+  })
     
 })
