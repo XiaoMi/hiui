15039d480392abbc159558e955983731533c7f09
solarjoker <chenissolar@gmail.com>
2019-12-13 09:25:08 +0800

    test：update transfer

--- components/transfer/__tests__/index.test.js
+++ components/transfer/__tests__/index.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Transfer from '../'
+import _Transfer from '../Transfer'
+import { wrapInTestContext, getBackendFromInstance } from 'react-dnd-test-utils'
 
 const randomData = () => {
   let arr = []
@@ -155,6 +157,61 @@ describe('Transfer', () => {
     expect(
       wrapper.find('.hi-transfer__container').at(0).find('.hi-checkbox-legacy--checked')
     ).toHaveLength(9)
+    wrapper.unmount()
+  })
+
+  it('can search', () => {
+    const cb = jest.fn()
+    const wrapper = mount(
+      <Transfer
+        type="multiple"
+        emptyContent={['空', '无数据']}
+        title={['左标题', '右标题']}
+        targetIds={[2, 3]}
+        data={randomData()}
+        onChange={cb}
+        searchable
+        showCheckAll
+      />
+    )
+
+    wrapper
+      .find('.hi-transfer__searchbar')
+      .at(0)
+      .find('input')
+      .simulate('change', { target: { value: '选项10' } })
+    expect(wrapper.find('.hi-transfer__container').at(0).find('.hi-transfer__item')).toHaveLength(1)
+    wrapper.unmount()
+  })
+
+  it('can drag', () => {
+    const cb = jest.fn()
+    const DragWrapper = wrapInTestContext(_Transfer)
+    const wrapper = mount(
+      <DragWrapper
+        type="multiple"
+        emptyContent={['空', '无数据']}
+        title={['左标题', '右标题']}
+        targetIds={[1, 2, 4]}
+        data={randomData()}
+        onChange={cb}
+        draggable
+        showCheckAll
+      />
+    )
+    const backend = getBackendFromInstance(wrapper.instance())
+    const monitor = wrapper.instance().getManager().getMonitor()
+    const sourceItem = wrapper.find('.hi-transfer__container').at(1).find('DragSource(Item)').at(0)
+    backend.simulateBeginDrag([sourceItem.instance().getHandlerId()])
+    expect(monitor.isDragging()).toBeTruthy()
+    const targetItem = wrapper
+      .find('.hi-transfer__container')
+      .at(1)
+      .find('DropTarget(DragSource(Item))')
+      .at(1)
+    backend.simulateHover([targetItem.instance().getHandlerId()])
+    backend.simulateDrop()
+    backend.simulateEndDrag()
     wrapper.unmount()
   })
 })
