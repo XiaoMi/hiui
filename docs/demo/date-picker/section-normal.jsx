8fb8e482f4ffa12793238617bee3bab57a9a417b
solarjoker <chenissolar@gmail.com>
2020-02-06 20:47:52 +0800

    docs: update datepicker

--- docs/demo/date-picker/section-normal.jsx
+++ docs/demo/date-picker/section-normal.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 import DocViewer from '../../../libs/doc-viewer'
 import DatePicker from '../../../components/date-picker'
 const prefix = 'date-picker-normal'
-const desc = '以天为粒度，展示“YYYYMMDD”'
-const code = `import React from 'react'
+const desc = '以天为粒度，展示“YYYY-MM-DD”'
+const rightOptions = ['基础', '带默认值', '禁用', '限制范围']
+const code = [
+  {
+    code: `import React from 'react'
+import DatePicker from '@hi-ui/hiui/es/date-picker'\n
+class Demo extends React.Component {
+  render () {
+    return (
+      <div style={{display:'flex', flexWrap: 'wrap'}}>
+        <DatePicker
+          onChange={(date, dateStr) => {console.log('onChange', date, dateStr)}}
+        />
+      </div>
+    )
+  }
+}`,
+    opt: ['基础']
+  },
+  {
+    code: `import React from 'react'
 import DatePicker from '@hi-ui/hiui/es/date-picker'\n
 class Demo extends React.Component {
   constructor() {
@@ -22,6 +41,52 @@ class Demo extends React.Component {
       </div>
     )
   }
-}`
-const DemoNormal = () => <DocViewer code={code} scope={{ DatePicker }} prefix={prefix} desc={desc} />
+}`,
+    opt: ['带默认值']
+  },
+  {
+    code: `import React from 'react'
+import DatePicker from '@hi-ui/hiui/es/date-picker'\n
+class Demo extends React.Component {
+  render () {
+    return (
+      <div style={{display:'flex', flexWrap: 'wrap'}}>
+        <DatePicker
+          disabled
+          onChange={(date, dateStr) => {console.log('onChange', date, dateStr)}}
+        />
+      </div>
+    )
+  }
+}`,
+    opt: ['禁用']
+  },
+  {
+    code: `import React from 'react'
+import DatePicker from '@hi-ui/hiui/es/date-picker'\n
+class Demo extends React.Component {
+  render () {
+    return (
+      <div style={{display:'flex', flexWrap: 'wrap'}}>
+        <DatePicker
+          min={new Date()}
+          max={new Date().getTime() + 30 * 24 * 60 * 60 * 1000}
+          onChange={(date, dateStr) => {console.log('onChange', date, dateStr)}}
+        />
+      </div>
+    )
+  }
+}`,
+    opt: ['限制范围']
+  }
+]
+const DemoNormal = () => (
+  <DocViewer
+    code={code}
+    scope={{ DatePicker }}
+    prefix={prefix}
+    desc={desc}
+    rightOptions={rightOptions}
+  />
+)
 export default DemoNormal
--- docs/demo/date-picker/section-scope.jsx
+++ docs/demo/date-picker/section-scope.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import DocViewer from '../../../libs/doc-viewer'
 import DatePicker from '../../../components/date-picker'
 const prefix = 'date-picker-scope'
-const code = `import React from 'react'
+const desc = '以天、周、月、年等粒度展示一个时间的范围'
+const rightOptions = ['日期', '年份', '月份', '周', '日期时间']
+const code = [
+  {
+    code: `import React from 'react'
 import DatePicker from '@hi-ui/hiui/es/date-picker'\n
 class Demo extends React.Component {
   constructor() {
@@ -23,12 +27,115 @@ class Demo extends React.Component {
       </div>
     )
   }
-}`
+}`,
+    opt: ['日期']
+  },
+  {
+    code: `import React from 'react'
+import DatePicker from '@hi-ui/hiui/es/date-picker'\n
+class Demo extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      rangeDate: {start: new Date(), end: new Date()} // 或 rangeDate: new Date()
+    }
+  }
+  render () {
+    return (
+      <div>
+        <DatePicker
+          type='daterange'
+          format='yyyy'
+          defaultValue={this.state.rangeDate}
+          onChange={(date, dateStr) => {console.log('onChange', date, dateStr)}}
+        />
+      </div>
+    )
+  }
+}`,
+    opt: ['年份']
+  },
+  {
+    code: `import React from 'react'
+import DatePicker from '@hi-ui/hiui/es/date-picker'\n
+class Demo extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      rangeDate: {start: new Date(), end: new Date()} // 或 rangeDate: new Date()
+    }
+  }
+  render () {
+    return (
+      <div>
+        <DatePicker
+          type='daterange'
+          format='yyyy-MM'
+          defaultValue={this.state.rangeDate}
+          onChange={(date, dateStr) => {console.log('onChange', date, dateStr)}}
+        />
+      </div>
+    )
+  }
+}`,
+    opt: ['月份']
+  },
+  {
+    code: `import React from 'react'
+import DatePicker from '@hi-ui/hiui/es/date-picker'\n
+class Demo extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      rangeDate: {start: new Date(), end: new Date()} // 或 rangeDate: new Date()
+    }
+  }
+  render () {
+    return (
+      <div>
+        <DatePicker
+          type='weekrange'
+          defaultValue={this.state.rangeDate}
+          onChange={(date, dateStr) => {console.log('onChange', date, dateStr)}}
+        />
+      </div>
+    )
+  }
+}`,
+    opt: ['周']
+  },
+  {
+    code: `import React from 'react'
+import DatePicker from '@hi-ui/hiui/es/date-picker'\n
+class Demo extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      rangeDate: {start: new Date(), end: new Date()} // 或 rangeDate: new Date()
+    }
+  }
+  render () {
+    return (
+      <div>
+        <DatePicker
+          type="timeperiod"
+          value={this.state.rangeDate}
+          onChange={(date, dateStr) => {console.log('onChange', date, dateStr)}}
+        />
+      </div>
+    )
+  }
+}`,
+    opt: ['日期时间']
+  }
+]
 const DemoScopeBan = () => (
   <DocViewer
     code={code}
     scope={{ DatePicker }}
     prefix={prefix}
+    desc={desc}
+    rightOptions={rightOptions}
   />
 )
 export default DemoScopeBan
