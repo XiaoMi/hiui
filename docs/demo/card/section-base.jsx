c4da1967c9e6c67727521f884ab25012d236ddf2
solarjoker <chenissolar@gmail.com>
2020-02-27 17:31:37 +0800

    docs: update card doc

--- docs/demo/card/section-base.jsx
+++ docs/demo/card/section-base.jsx
@@ -2,26 +2,61 @@ import React from 'react'
 import DocViewer from '../../../libs/doc-viewer'
 import Card from '../../../components/card'
 const prefix = 'card-base'
-const desc = '用卡片封装独立的信息实体，如模块、功能、项目、应用等，或还原实际生活中的卡片应用'
-const code = `import React from 'react'
+const rightOptions = ['基础', '无标题', '悬浮效果']
+const desc =
+  '用卡片封装独立的信息实体，如模块、功能、项目、应用等，或还原实际生活中的卡片应用'
+const code = [
+  {
+    code: `import React from 'react'
 import Card from '@hi-ui/hiui/es/card'\n
 class Demo extends React.Component {
   render() {
     return (
-      <div>
-        <Card title="标题" style={{width: 400,height: 200}}>
-          <span>普通 Card</span>
-        </Card>
-        <br />
-        <Card hoverable style={{width: 400,height: 200}}>
-          <p>无标题</p>
-          <p>鼠标移入悬浮效果</p>
-          <p>其它额外内容</p>
-        </Card>
-      </div>
+      <Card title="标题" size='default' >
+        <p>基础卡片</p>
+      </Card>
     )
   }
-}`
+}`,
+    opt: ['基础']
+  },
+  {
+    code: `import React from 'react'
+import Card from '@hi-ui/hiui/es/card'\n
+class Demo extends React.Component {
+  render() {
+    return (
+      <Card size='default' >
+        <p>无标题卡片</p>
+      </Card>
+    )
+  }
+}`,
+    opt: ['无标题']
+  },
+  {
+    code: `import React from 'react'
+import Card from '@hi-ui/hiui/es/card'\n
+class Demo extends React.Component {
+  render() {
+    return (
+      <Card title="标题" hoverable size='default' >
+        <p>鼠标移入悬浮效果</p>
+      </Card>
+    )
+  }
+}`,
+    opt: ['悬浮效果']
+  }
+]
 
-const DemoBase = () => <DocViewer code={code} scope={{ Card }} prefix={prefix} desc={desc} />
+const DemoBase = () => (
+  <DocViewer
+    code={code}
+    scope={{ Card }}
+    prefix={prefix}
+    desc={desc}
+    rightOptions={rightOptions}
+  />
+)
 export default DemoBase
--- docs/demo/card/section-size.jsx
+++ docs/demo/card/section-size.jsx
@@ -2,38 +2,29 @@ import React from 'react'
 import DocViewer from '../../../libs/doc-viewer'
 import Card from '../../../components/card'
 const prefix = 'card-size'
+const desc = '为卡片定义了大中小三种尺寸，根据页面的实际空间选用'
 const code = `import React from 'react'
 import Card from '@hi-ui/hiui/es/card'\n
 class Demo extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Card hoverable style={{width: 200,height: 200}} title='标题内容-标题内容-标题内容'>
-          <p>自定义宽度：200px</p>
-          <p>自定义高度：200px</p>
-          <p>其它额外内容</p>
+        <Card title='小号卡片'  size='small'>
+          <p>这是一个小号卡片</p>
         </Card>
         <br />
-        <Card hoverable size='small'>
-          <p>size = small</p>
-          <p>无标题</p>
-          <p>其它额外内容</p>
+        <Card title='中号卡片'  size='default'>
+          <p>这是一个中号卡片</p>
         </Card>
         <br />
-        <Card hoverable size='default'>
-          <p>size = default</p>
-          <p>无标题</p>
-          <p>其它额外内容</p>
-        </Card>
-        <br />
-        <Card hoverable size='large'>
-          <p>size = large</p>
-          <p>无标题</p>
-          <p>其它额外内容</p>
+        <Card title='大号卡片'  size='large'>
+          <p>这是一个大号卡片</p>
         </Card>
       </React.Fragment>
     )
   }
 }`
-const DemoSize = () => <DocViewer code={code} scope={{ Card }} prefix={prefix} />
+const DemoSize = () => (
+  <DocViewer desc={desc} code={code} scope={{ Card }} prefix={prefix} />
+)
 export default DemoSize
