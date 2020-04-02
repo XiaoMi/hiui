70abb0de85358853fba270e6277a81890c805ff5
Wugaoliang <wugaoliang@xiaomi.com>
2020-04-02 08:52:08 +0800

    feat: #1024

--- components/watermark/watermark.js
+++ components/watermark/watermark.js
@@ -101,7 +101,8 @@ const drawLogo = (ctx, logo, cb) => {
 }
 
 const toImage = (canvas, key, container, options) => {
-  var base64Url = canvas.toDataURL()
+  const base64Url = canvas.toDataURL()
+  const { opacity = 1 } = options
   const _top = (options._top || 0) + 'px'
   const __wm = document.querySelector(`.${key}`)
   const watermarkDiv = __wm || document.createElement('div')
@@ -117,7 +118,7 @@ const toImage = (canvas, key, container, options) => {
   background-repeat:repeat;
   visibility:visible !important;
   display: block !important;
-  opacity: 1 !important;
+  opacity: ${opacity} !important;
   background-image:url('${base64Url}');
   ${options.grayLogo ? '-webkit-filter: grayscale(100%);-moz-filter: grayscale(100%);-ms-filter: grayscale(100%);-o-filter: grayscale(100%);filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);_filter:none;' : ''}
   `
