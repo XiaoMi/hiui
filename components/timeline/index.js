c5a2badc303f626a57b871a5051fbf9e2f7c48a5
solarjoker <chenissolar@gmail.com>
2020-04-27 19:36:29 +0800

    feat: init new timeline

--- components/timeline/index.js
+++ components/timeline/index.js
@@ -5,14 +5,11 @@ import './style/index'
 import FoldingItem from './FoldingItem'
 import Icon from '../icon'
 
-const Time = props => {
+const Time = (props) => {
   return (
     <div className='hi-timeline__time'>
       {props.groupTitle && (
-        <div
-          style={{ marginTop: -22 }}
-          className='hi-timeline__group-title'
-        >
+        <div style={{ marginTop: -22 }} className='hi-timeline__group-title'>
           {props.groupTitle}
         </div>
       )}
@@ -29,88 +26,129 @@ class Timeline extends Component {
    * @param {*} groupTitle 分组标题
    * @param {*} isSub 是否是子项，只支持一级
    */
-  renderItem (item, index, groupTitle, isSub) {
-    const { layout, type } = this.props
-    // TODO: dot 废弃，使用 icon
-    const dot = item.dot
-    const icon = item.icon
-    let dotEl = isSub ? (
-      <div className='hi-timeline__dot hi-timeline__dot--sub' />
-    ) : (
-      <div className='hi-timeline__dot' />
-    )
-    if (dot && typeof dot === 'object') {
-      dotEl = <div className='hi-timeline__dot hi-timeline__dot--custom'>{dot}</div>
-    }
-    if (icon && typeof icon === 'string') {
-      dotEl = (
-        <div className='hi-timeline__dot hi-timeline__dot--custom'>
-          <Icon name={icon} />
-        </div>
-      )
-    } else if (icon && React.isValidElement(icon)) {
-      dotEl = <div className='hi-timeline__dot hi-timeline__dot--custom'>{icon}</div>
-    }
-    const itemCls = classNames('hi-timeline__item', isSub && 'hi-timeline__item--sub')
-    return (
-      <li className={itemCls} key={item.title + index}>
-        {layout === 'normal' && type === 'default' && (
-          <Time item={item} groupTitle={index === 0 && groupTitle} />
-        )}
-        <div className='hi-timeline__row'>
-          {((layout === 'cross' && type === 'default') || type === 'cross') && <Time item={item} />}
-          <div className='hi-timeline__content-container'>
-            <div className='hi-timeline__title'>{item.title}</div>
-            {/* TODO: description 废弃，使用 content */}
-            <div className='hi-timeline__desc'>{item.content || item.description}</div>
-            {((layout === 'right' && type === 'default') || type === 'right') && (
-              <div className='hi-timeline__time--extra'>{item.timestamp}</div>
-            )}
-          </div>
-        </div>
-        <div className='hi-timeline__line' />
-        {dotEl}
-      </li>
-    )
-  }
+  // renderItem (item, index, groupTitle, isSub) {
+  //   const { layout, type } = this.props
+  //   // TODO: dot 废弃，使用 icon
+  //   const dot = item.dot
+  //   const icon = item.icon
+  //   let dotEl = isSub ? (
+  //     <div className='hi-timeline__dot hi-timeline__dot--sub' />
+  //   ) : (
+  //     <div className='hi-timeline__dot' />
+  //   )
+  //   if (dot && typeof dot === 'object') {
+  //     dotEl = <div className='hi-timeline__dot hi-timeline__dot--custom'>{dot}</div>
+  //   }
+  //   if (icon && typeof icon === 'string') {
+  //     dotEl = (
+  //       <div className='hi-timeline__dot hi-timeline__dot--custom'>
+  //         <Icon name={icon} />
+  //       </div>
+  //     )
+  //   } else if (icon && React.isValidElement(icon)) {
+  //     dotEl = <div className='hi-timeline__dot hi-timeline__dot--custom'>{icon}</div>
+  //   }
+  //   const itemCls = classNames('hi-timeline__item', isSub && 'hi-timeline__item--sub')
+  //   return (
+  //     <li className={itemCls} key={item.title + index}>
+  //       {layout === 'normal' && type === 'default' && (
+  //         <Time item={item} groupTitle={index === 0 && groupTitle} />
+  //       )}
+  //       <div className='hi-timeline__row'>
+  //         {((layout === 'cross' && type === 'default') || type === 'cross') && <Time item={item} />}
+  //         <div className='hi-timeline__content-container'>
+  //           <div className='hi-timeline__title'>{item.title}</div>
+  //           {/* TODO: description 废弃，使用 content */}
+  //           <div className='hi-timeline__desc'>{item.content || item.description}</div>
+  //           {((layout === 'right' && type === 'default') || type === 'right') && (
+  //             <div className='hi-timeline__time--extra'>{item.timestamp}</div>
+  //           )}
+  //         </div>
+  //       </div>
+  //       <div className='hi-timeline__line' />
+  //       {dotEl}
+  //     </li>
+  //   )
+  // }
   /**
    * 渲染列表，被递归调用
    * @param {Array} datas 数据
    * @param {String | React.ReactNode} groupTitle  分组标题
    * @param {Number} originIndex  原始索引，用于判断是否为第一个分组标题
    */
-  renderItems (data, groupTitle, isSub) {
-    const { layout, localeDatas, type } = this.props
-    return data.map((item, index) => {
-      const { groupTitle: gt } = item
-      if (gt) {
-        // 渲染分组
-        return this.renderItems(item.children, gt)
-      }
-      if (item.children && layout !== 'cross' && type !== 'cross') {
-        // 子项 含 收起按钮
-        const foldingEl = (
-          <FoldingItem
-            key={index}
-            texts={localeDatas.timeline}
-            subChildren={this.renderItems(item.children, null, true)}
-          />
-        )
-        // 如果含有子项，需先渲染当前项
-        let el = this.renderItem(item, index, groupTitle)
-        return [el, foldingEl]
-      }
-      return this.renderItem(item, index, groupTitle, isSub)
-    })
+  // renderItems (data, groupTitle, isSub) {
+  //   const { layout, localeDatas, type } = this.props
+  //   return data.map((item, index) => {
+  //     const { groupTitle: gt } = item
+  //     if (gt) {
+  //       // 渲染分组
+  //       return this.renderItems(item.children, gt)
+  //     }
+  //     if (item.children && layout !== 'cross' && type !== 'cross') {
+  //       // 子项 含 收起按钮
+  //       const foldingEl = (
+  //         <FoldingItem
+  //           key={index}
+  //           texts={localeDatas.timeline}
+  //           subChildren={this.renderItems(item.children, null, true)}
+  //         />
+  //       )
+  //       // 如果含有子项，需先渲染当前项
+  //       let el = this.renderItem(item, index, groupTitle)
+  //       return [el, foldingEl]
+  //     }
+  //     return this.renderItem(item, index, groupTitle, isSub)
+  //   })
+  // }
+  renderGroup = (item) => {
+    return (
+      <div>
+        <div>{item.groupTitle}</div>
+        <div>{item.children.map((i) => this.renderItem(i))}</div>
+      </div>
+    )
   }
 
+  renderItem = (item) => {
+    return (
+      <div>
+        <div>{item.title}</div>
+        <div>{item.content}</div>
+        <div>{item.timestamp}</div>
+        <div>{item.extraTime}</div>
+      </div>
+    )
+  }
+
+  renderDefault = (item) => {
+    return (
+      <div style={{ display: 'flex' }}>
+        <div>
+          <div>{item.timestamp}</div>
+          <div>{item.extraTime}</div>
+        </div>
+        <div>
+          <div>{item.title}</div>
+          <div>{item.content}</div>
+        </div>
+      </div>
+    )
+  }
   render () {
     const { layout, type, list, data, theme } = this.props
     const _layout = type === 'default' ? layout : type
     const rootCls = classNames('hi-timeline', `theme__${theme}`, `hi-timeline--${_layout}`)
     return (
       <div className={rootCls}>
-        <ul>{this.renderItems(data || list)}</ul>
+        <ul>
+          {(data || list).map((item) => {
+            if (item.groupTitle) {
+              return this.renderGroup(item)
+            } else {
+              return this.renderItem(item)
+            }
+          })}
+        </ul>
       </div>
     )
   }
