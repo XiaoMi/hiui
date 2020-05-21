73e223fa4af0d853eec58be78f7001e88a23acd2
Wugaoliang <wugaoliang@xiaomi.com>
2020-05-21 15:48:36 +0800

    fix: #1064

--- docs/demo/stepper/section-icon.jsx
+++ docs/demo/stepper/section-icon.jsx
@@ -15,15 +15,15 @@ class Demo extends React.Component {
     const list = [
       {
         title: '账号信息',
-        icon: <Icon name='user' />,
+        icon: 'user',
       },
       {
         title: '邮箱激活',
-        icon: <Icon name='time' />
+        icon: 'time'
       },
       {
         title: '信息登记',
-        icon: <Icon name='list' />
+        icon: 'list'
       },
     ]
 
@@ -46,15 +46,15 @@ class Demo extends React.Component {
     const list = [
       {
         title: '账号信息',
-        icon: <Icon name='user' />,
+        icon: 'user',
       },
       {
         title: '邮箱激活',
-        icon: <Icon name='time' />
+        icon: 'time'
       },
       {
         title: '信息登记',
-        icon: <Icon name='list' />
+        icon: 'list'
       },
     ]
 
