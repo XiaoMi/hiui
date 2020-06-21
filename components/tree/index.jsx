f9b457a88f4eeffbb643d43d0c00bb0527f8cc88
solarjoker <chenissolar@gmail.com>
2020-06-21 20:23:20 +0800

    refactor: update tree

--- components/tree/index.jsx
+++ components/tree/index.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useCallback, useEffect } from 'react'
 import BaseTree from './BaseTree'
 import Input from '../Input'
-import Icon from '../Icon'
+import Button from '../button'
 import Popper from '../popper'
+import Modal from '../modal'
 import { getAncestorIds } from './util'
 import _ from 'lodash'
 import Classnames from 'classnames'
+import uuidv4 from 'uuid/v4'
+import CustomTreeNode from './customTreeNode'
 import './style/index.scss'
 
 const getMatchedNodes = (data, searchValue, matchedNodes = []) => {
@@ -38,6 +41,7 @@ const Tree = (props) => {
     searchable,
     searchConfig = {},
     data,
+    expandedIds,
     filter = false,
     contextMenu,
     editable,
@@ -54,166 +58,223 @@ const Tree = (props) => {
   }, [data])
 
   const [menuVisible, setMenuVisible] = useState(null)
+  const [modalVisible, setModalVisible] = useState(null)
 
   const [editingNodes, setEditingNodes] = useState([])
 
   const [searchValue, setSearchValue] = useState('')
   const [matchedNodes, setMatchedNodes] = useState([])
   const [filteredIds, setFilteredIds] = useState([])
+  const [expanded, setExpanded] = useState([])
+  useEffect(() => {
+    if (expandedIds) {
+      setExpanded(expandedIds)
+    }
+  }, [expandedIds])
   const showData = getShowData(
     _.cloneDeep(cacheData),
     matchedNodes.map((n) => n.id),
     filteredIds
   )
-
+  // 编辑节点
   const editNode = (node) => {
     setEditingNodes(editingNodes.concat(node))
   }
+  // 同步编辑值
+  const onValueChange = (value, nodeId) => {
+    setEditingNodes(
+      editingNodes
+        .filter((n) => n.id !== nodeId)
+        .concat(
+          Object.assign(
+            {},
+            editingNodes.find((n) => n.id === nodeId),
+            {
+              title: value
+            }
+          )
+        )
+    )
+  }
+  // 取消编辑
+  const cancelEdit = (nodeId) => {
+    setEditingNodes(editingNodes.filter((n) => n.id !== nodeId))
+  }
 
-  const menuRender = useCallback(
-    (node) => {
-      let menu = [
-        { title: '编辑节点', type: 'editNode' },
-        { title: '添加子节点', type: 'addChildNode' },
-        { title: '添加节点', type: 'addSiblingNode' },
-        { title: '删除', type: 'deleteNode' }
-      ]
-      if (contextMenu) {
-        menu = contextMenu(node)
+  // 添加兄弟节点
+  const _addSibNode = (node, data, addNode) => {
+    data.forEach((d, index) => {
+      if (d.id === node.id) {
+        data.splice(index + 1, 0, addNode)
+      } else {
+        if (d.children) {
+          _addSibNode(node, d.children, addNode)
+        }
       }
-      return (
-        <ul className={`${PREFIX}__menu`}>
-          {menu.map((m, index) => (
-            <li
-              className={`menu-item`}
-              key={index}
-              onClick={() => {
-                if (m.onClick) {
-                  m.onClick(node)
-                } else {
-                  if (m.type === 'editNode') {
-                    editNode(node)
-                  }
+    })
+  }
+
+  const addSiblingNode = (node) => {
+    const dataCache = _.cloneDeep(cacheData)
+    const addNode = { id: uuidv4(), title: '', TREE_NODE_TYPE: 'add' }
+    _addSibNode(node, dataCache, addNode)
+    setEditingNodes(editingNodes.concat(addNode))
+    updateCacheData(dataCache)
+  }
+
+  // 添加子节点
+  const _addChildNode = (node, data, addNode) => {
+    data.forEach((d, index) => {
+      if (d.id === node.id) {
+        if (!d.children) {
+          d.children = []
+        }
+        d.children.push(addNode)
+      } else {
+        if (d.children) {
+          _addChildNode(node, d.children, addNode)
+        }
+      }
+    })
+  }
+  const addChildNode = (node) => {
+    const dataCache = _.cloneDeep(cacheData)
+    const addNode = { id: uuidv4(), title: '', TREE_NODE_TYPE: 'add' }
+    _addChildNode(node, dataCache, addNode)
+    setEditingNodes(editingNodes.concat(addNode))
+    updateCacheData(dataCache)
+    setExpanded(expanded.concat(node.id))
+  }
+
+  // 取消添加节点
+  const _cancelAddNode = (node, data) => {
+    data.forEach((d, index) => {
+      if (d.id === node.id) {
+        data.splice(index, 1)
+      } else {
+        if (d.children) {
+          _cancelAddNode(node, d.children)
+        }
+      }
+    })
+  }
+  const cancelAddNode = (node) => {
+    const dataCache = _.cloneDeep(cacheData)
+    _cancelAddNode(node, dataCache)
+    updateCacheData(dataCache)
+  }
+  // 保存
+  const _saveEdit = (itemId, data, nodeEdited) => {
+    data.forEach((d, index) => {
+      if (d.id === itemId) {
+        d.title = nodeEdited.title
+        delete d['TREE_NODE_TYPE']
+      } else {
+        if (d.children) {
+          _saveEdit(itemId, d.children, nodeEdited)
+        }
+      }
+    })
+  }
+  const saveEdit = (nodeId) => {
+    const nodeEdited = { ...editingNodes.find((node) => node.id === nodeId) }
+    const dataCache = _.cloneDeep(cacheData)
+    _saveEdit(nodeId, dataCache, nodeEdited)
+    updateCacheData(dataCache)
+    setEditingNodes(editingNodes.filter((n) => n.id !== nodeId))
+  }
+
+  // 删除节点
+  const _deleteNode = (itemId, data) => {
+    data.forEach((d, index) => {
+      if (d.id === itemId) {
+        data.splice(index, 1)
+      } else {
+        if (d.children) {
+          _deleteNode(itemId, d.children)
+        }
+      }
+    })
+  }
+  const deleteNode = (node) => {
+    const dataCache = _.cloneDeep(cacheData)
+    _deleteNode(node.id, dataCache)
+    if (onBeforeDelete) {
+      const result = onBeforeDelete(node, { before: dataCache, after: cacheData }, node.depth)
+      if (result === true) {
+        updateCacheData(dataCache)
+        onDelete(node, dataCache)
+      }
+    } else {
+      updateCacheData(dataCache)
+      onDelete(node, dataCache)
+    }
+  }
+
+  const menuRender = useCallback((node) => {
+    let menu = [
+      { title: '编辑节点', type: 'editNode' },
+      { title: '添加子节点', type: 'addChildNode' },
+      { title: '添加节点', type: 'addSiblingNode' },
+      { title: '删除', type: 'deleteNode' }
+    ]
+    if (contextMenu) {
+      menu = contextMenu(node)
+    }
+    return (
+      <ul className={`${PREFIX}__menu`}>
+        {menu.map((m, index) => (
+          <li
+            className={`menu-item`}
+            key={index}
+            onClick={(e) => {
+              e.stopPropagation()
+              if (m.onClick) {
+                m.onClick(node)
+              } else {
+                if (m.type === 'editNode') {
+                  editNode(node)
                 }
-              }}
-            >
-              {m.title}
-            </li>
-          ))}
-        </ul>
-      )
-    },
-    [editNode]
-  )
+                if (m.type === 'addSiblingNode') {
+                  addSiblingNode(node)
+                }
+                if (m.type === 'addChildNode') {
+                  addChildNode(node)
+                }
+                if (m.type === 'deleteNode') {
+                  setModalVisible(node)
+                }
+              }
+              setMenuVisible(null)
+            }}
+          >
+            {m.title}
+          </li>
+        ))}
+      </ul>
+    )
+  }, [])
 
   const treeNodeRender = useCallback(
     (node, { selected }, treeNodeRef, onSelectNode) => {
-      if (editingNodes.map((n) => n.id).includes(node.id)) {
-        return (
-          <div className='tree-node__title--editing'>
-            <Input
-              style={{ width: 240, marginRight: 20 }}
-              value={(editingNodes.find((n) => n.id === node.id) || {}).title}
-              onChange={(e) => {
-                onValueChange(e.target.value, node.id)
-              }}
-            />
-            <span
-            // style={
-            //   (editingNodes.find((node) => node.id === item.id) || {}).title === ''
-            //     ? { marginRight: 12, color: '#999', cursor: 'not-allowed' }
-            //     : { cursor: 'pointer', marginRight: 12, color: themeColor[theme] }
-            // }
-            // onClick={() => {
-            //   if ((editingNodes.find((node) => node.id === item.id) || {}).title !== '') {
-            //     saveEditNode(item.id, level)
-            //   }
-            // }}
-            >
-              确定
-            </span>
-            <span
-              style={{ cursor: 'pointer', color: '#999' }}
-              // onClick={() => {
-              //   if (editNodes.map((node) => node.id).includes(item.id)) {
-              //     cancelEditNode(item.id)
-              //   } else {
-              //     cancelAddSiblingNode(item.id)
-              //   }
-              // }}
-            >
-              取消
-            </span>
-          </div>
-        )
-      } else {
-        if (
-          searchValue !== '' &&
-          searchable &&
-          typeof node.title === 'string' &&
-          node.title.includes(searchValue)
-        ) {
-          const index = node.title.indexOf(searchValue)
-          const beforeStr = node.title.substr(0, index)
-          const afterStr = node.title.substr(index + searchValue.length)
-          return (
-            <div
-              className={Classnames('title__text', {
-                'title__text--selected': selected
-              })}
-              onClick={() => {
-                onSelectNode(node)
-              }}
-              onContextMenu={(e) => {
-                if (editable) {
-                  e.preventDefault()
-                  setMenuVisible(node.id)
-                }
-              }}
-            >
-              <span>
-                {beforeStr}
-                <span style={{ color: '#4284f5' }}>{searchValue}</span>
-                {afterStr}
-              </span>
-            </div>
-          )
-        } else {
-          return (
-            <div
-              className={Classnames('title__text', {
-                'title__text--selected': selected
-              })}
-              onClick={() => {
-                onSelectNode(node)
-              }}
-              onContextMenu={(e) => {
-                console.log(111)
-                if (editable) {
-                  e.preventDefault()
-                  setMenuVisible(node.id)
-                }
-              }}
-            >
-              {node.title}
-              {editable && (
-                <Popper
-                  show={menuVisible === node.id}
-                  attachEle={treeNodeRef.current}
-                  width={false}
-                  zIndex={1040}
-                  placement='right-start'
-                  onClickOutside={() => {
-                    setMenuVisible(null)
-                  }}
-                >
-                  {menuRender(node)}
-                </Popper>
-              )}
-            </div>
-          )
-        }
-      }
+      return (
+        <CustomTreeNode
+          node={node}
+          status={{ selected }}
+          onSelectNode={onSelectNode}
+          searchValue={searchValue}
+          editingNodes={editingNodes}
+          menuVisible={menuVisible}
+          searchable={searchable}
+          onValueChange={onValueChange}
+          saveEdit={saveEdit}
+          cancelEdit={cancelEdit}
+          cancelAddNode={cancelAddNode}
+          setMenuVisible={setMenuVisible}
+          editable={editable}
+          menuRender={menuRender}
+        />
+      )
     },
     [searchValue, editingNodes, menuVisible, searchable]
   )
@@ -236,7 +297,7 @@ const Tree = (props) => {
               setMatchedNodes(matchedNodes)
               setFilteredIds(_.uniq(filteredNodes))
             }}
-            append={<Icon name='search' style={{ fontSize: '16px' }} />}
+            append={<Button icon='search' />}
             style={{ width: '250px', marginBottom: '24px' }}
           />
           <div />
@@ -248,9 +309,22 @@ const Tree = (props) => {
       <BaseTree
         {...props}
         treeNodeRender={treeNodeRender}
-        expandedIds={filteredIds}
+        expandedIds={filteredIds.length ? filteredIds : expanded}
         data={filter && searchable && searchValue !== '' ? showData : cacheData}
       />
+      <Modal
+        title={'提示'}
+        visible={modalVisible !== null}
+        onConfirm={() => {
+          deleteNode(modalVisible)
+          setModalVisible(null)
+        }}
+        onCancel={() => {
+          setModalVisible(null)
+        }}
+      >
+        删除节点将删除所有子节点，确定删除吗？
+      </Modal>
     </React.Fragment>
   )
 }
