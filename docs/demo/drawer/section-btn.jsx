0c91a2ac25f9d0d5fc094e02cd5d03cdf233684c
solarjoker <chenissolar@gmail.com>
2020-06-29 11:14:20 +0800

    feat: update drawer

--- docs/demo/drawer/section-btn.jsx
+++ docs/demo/drawer/section-btn.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import DocViewer from '../../../libs/doc-viewer'
 import Button from '../../../components/button'
-import Modal from '../../../components/modal'
-const prefix = 'modal-btn'
+import Drawer from '../../../components/drawer'
+const prefix = 'drawer-btn'
 const desc = '通过 footers 自定义底部的按钮，并执行对应自定义事件，通过 closeBtn 取消右上角关闭按钮'
 
 const code = `import React from 'react'
 import Button from '@hi-ui/hiui/es/button'
-import Modal from '@hi-ui/hiui/es/modal'\n
+import Drawer from '@hi-ui/hiui/es/drawer'\n
 class Demo extends React.Component {
   constructor (props) {
     super(props)
@@ -25,28 +25,26 @@ class Demo extends React.Component {
     return(
       <div>
         <Button type="primary" onClick={() => this.setState({visible: true})}>打开</Button>
-        <Modal
+        <Drawer
           title="提示消息"
           closeBtn={false}
           visible={this.state.visible}
-          onConfirm={()=>{console.log('自定义确定事件')}}
-          onCancel={this.cancelEvent.bind(this)}
-          footer={[
-            <Button type="primary" key={0} onClick={() => console.log(1)}>自定义按钮1</Button>,
-            <Button type="success" key={1} onClick={() => console.log(2)}>自定义按钮2</Button>,
-            <Button type="danger" key={2} onClick={() => console.log(3)}>自定义按钮3</Button>,
-            <Button type="default" key={3} onClick={this.cancelEvent.bind(this)}>关闭</Button>
-          ]}
+          onClose={this.cancelEvent.bind(this)}
+          footer={<div style={{textAlign:'right'}}>
+              <Button type="primary" key={0} onClick={() => console.log(1)}>确认</Button>
+              <Button type="line" key={1} onClick={() => console.log(2)}>取消</Button>
+            </div>
+          }
         >
           <span>一些消息</span>
           <span>一些消息</span>
-        </Modal>
+        </Drawer>
 
       </div>
     )
   }
 }`
 const DemoBtn = () => (
-  <DocViewer code={code} scope={{ Button, Modal }} prefix={prefix} desc={desc} />
+  <DocViewer code={code} scope={{ Button, Drawer }} prefix={prefix} desc={desc} />
 )
 export default DemoBtn
