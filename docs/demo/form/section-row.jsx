7541f280f6ae4ca5a40fe29dc34502acdb2fc5a9
wugaoliang <wugaoliang@xiaomi.com>
2020-07-11 18:01:22 +0800

    fix: query demo

--- docs/demo/form/section-row.jsx
+++ docs/demo/form/section-row.jsx
@@ -3,73 +3,25 @@ import DocViewer from '../../../libs/doc-viewer'
 import Form from '../../../components/form/index'
 import Input from '../../../components/input'
 import Button from '../../../components/button'
-import Grid from '../../../components/grid'
-
 const prefix = 'form-row'
 const desc = '适用于筛选或查询数据的场景，和表格配合使用'
 const code = `import React from 'react'
-import { Form, Input, Grid } from '@hi-ui/hiui'
+import { Form, Input } from '@hi-ui/hiui'
 class Demo extends React.Component {
-  constructor () {
-    super()
-    this.form = React.createRef()
-  }
-  renderField () {
-    const formItems = [];
-    const count = 9
-    for (let i = 0; i < count; i++) {
-      formItems.push(
-          <Form.Item
-            field={'field'+i}
-            label={'Field'+i}
-            rules={[
-              {
-                required: true,
-                message: 'Input something!',
-              },
-            ]}
-          >
-            <Input placeholder="placeholder" style={{ width: 200 }}/>
-          </Form.Item>
-      );
-    }
-
-    return formItems
-  }
-  
   render (){
     const FormItem = Form.Item
-    const FormSubmit = Form.Submit
-    const Row = Grid.Row
-    const Col = Grid.Col
-    
     return (
-      <div style={{minWidth:'864px'}}>
-        <Form placement='horizontal' labelPlacement='right' ref={this.form}>
-          {
-            this.renderField()
-          }
-        </Form>
-        <div style = {{textAlign: 'right', paddingRight: '34px'}}>
-          <Button type="primary"  onClick={()=>{
-            console.log('填充表单')
-            this.form.current.validate((values,errors)=>{
-              console.log('Get form value:',values,errors)
-            })
-          }}
-          >
-          查询
-        </Button>
-        <Button type="line"  onClick={()=>{
-            this.form.current.resetValidates(()=>{
-              console.log('重置表单')
-            })
-         }}
-        >
-        重置
-        </Button>
-      </div>
-    </div>
+      <Form placement='horizontal' labelPlacement='right'>
+        <FormItem label='账号' labelWidth='50'>
+          <Input placeholder={'请输入'} />
+        </FormItem>
+        <FormItem label='密码' labelWidth='50'>
+          <Input type='password' placeholder={'请输入'} />
+        </FormItem>
+        <FormItem>
+          <Button type={'primary'}>提交</Button>
+        </FormItem>
+      </Form>
     )
   }
 }`
@@ -77,7 +29,7 @@ class Demo extends React.Component {
 const DemoRow = () => (
   <DocViewer
     code={code}
-    scope={{ Form, Button, Input, Grid }}
+    scope={{ Form, Button, Input }}
     prefix={prefix}
     desc={desc}
   />
