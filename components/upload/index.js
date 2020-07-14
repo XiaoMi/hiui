2d97c8ef8914f3f771c60ef0812f9fb68406023e
solarjoker <chenissolar@gmail.com>
2020-07-14 16:26:48 +0800

    refactor: update upload

--- components/upload/index.js
+++ components/upload/index.js
@@ -1,6 +1,4 @@
 import Upload from './main.js'
-import SwitchVersion from '../_util/SwitchVersion'
-import UploadLegacy from './upload-legacy'
 import Provider from '../context'
 
-export default SwitchVersion(Provider(Upload), UploadLegacy)
+export default Provider(Upload)
