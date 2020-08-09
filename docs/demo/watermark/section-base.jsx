ee8e30eba2c98dea02b4eaf732ff57d6fbff002f
wugaoliang <wugaoliang@xiaomi.com>
2020-08-09 16:30:33 +0800

    fix: watermark

--- docs/demo/watermark/section-base.jsx
+++ docs/demo/watermark/section-base.jsx
@@ -6,14 +6,14 @@ const prefix = 'watermark-base'
 const desc = ''
 const code = `import React from 'react'
 import logo from '本地图片路径或者base64'
-import Watermark from '@hi-ui/hiui/es/Watermark'\n
+import Watermark from '@hi-ui/hiui/es/watermark'
+
 class Demo extends React.Component {
   constructor(props) {
     super(props)
     this.options = {
       logo: logo, // 本地图片路径或者base64
       content: ['HIUI', '做中台，就用 HIUI'],
-      density:'high'
     }
   }
   render () {
@@ -21,7 +21,7 @@ class Demo extends React.Component {
       <Watermark
         {...this.options}
       >
-      <div id="watermark-box" 
+      <div id="watermark-box"
         style={{width: '100%', height: 400, border: '1px solid rgb(230, 231, 232)'}}
         />
       </Watermark>
@@ -35,5 +35,6 @@ const DemoBase = () => (
     code={code}
     scope={{ Watermark, logo }}
     prefix={prefix}
-  />)
+  />
+)
 export default DemoBase
