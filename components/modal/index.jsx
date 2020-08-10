ad81538622f8bb690b43e01e656b492bba259c5d
solarjoker <chenissolar@gmail.com>
2020-08-10 12:58:12 +0800

    refactor: update upload and modal

--- components/modal/index.jsx
+++ components/modal/index.jsx
@@ -27,7 +27,9 @@ const ModalComp = ({
   size = 'default',
   showHeaderDivider = true,
   showFooterDivider = true,
-  footer
+  footer,
+  confirmText,
+  cancelText
 }) => {
   // TODO: 整体可以抽成一个 hooks 供 modal 和 drawer 复用
   const defaultContainer = useRef(false)
@@ -83,7 +85,7 @@ const ModalComp = ({
             </div>
             <div className={`${PREFIX}__content`}>{children}</div>
             <div className={Classnames(`${PREFIX}__footer`, { [`${PREFIX}__footer--divided`]: showFooterDivider })}>
-              {footer === undefined && (
+              {footer === undefined && cancelText !== null && (
                 <Button
                   type={'line'}
                   onClick={() => {
@@ -92,10 +94,10 @@ const ModalComp = ({
                     }
                   }}
                 >
-                  取消
+                  {cancelText || '取消'}
                 </Button>
               )}
-              {footer === undefined && (
+              {footer === undefined && confirmText !== null && (
                 <Button
                   type={'primary'}
                   onClick={() => {
@@ -104,7 +106,7 @@ const ModalComp = ({
                     }
                   }}
                 >
-                  确认
+                  {confirmText || '确认'}
                 </Button>
               )}
               {footer}
@@ -124,7 +126,7 @@ const confirmIconMap = {
   info: { name: 'info-circle-o', color: '#4284F5' }
 }
 
-const confirm = ({ onConfirm, onCancel, title = '提示', content, type = 'default' }) => {
+const confirm = ({ onConfirm, onCancel, title = '提示', content, type = 'default', confirmText, cancelText }) => {
   const confirmContainer = document.createElement('div')
 
   document.body.appendChild(confirmContainer)
@@ -133,6 +135,8 @@ const confirm = ({ onConfirm, onCancel, title = '提示', content, type = 'defau
     title,
     size: 'small',
     visible: true,
+    confirmText,
+    cancelText,
     onConfirm: () => {
       onConfirm && onConfirm()
       confirmContainer.parentNode.style.removeProperty('overflow')
