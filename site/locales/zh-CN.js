c6e409b8f63ecd388f06614fc69bd338eec75fcd
Wugaoliang <wugaoliang@xiaomi.com>
2020-08-11 17:27:49 +0800

    fix: filter name

--- site/locales/zh-CN.js
+++ site/locales/zh-CN.js
@@ -60,7 +60,7 @@ module.exports = {
     carousel: 'Carousel 走马灯',
     watermark: 'Watermark 水印',
     list: 'List 列表',
-    filter: 'Fiter 筛选'
+    filter: 'Filter 筛选'
   },
   designs: {
     summarize: '概述',
