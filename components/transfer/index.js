1d0fd2d84380c291758a2e4b601c1396a02781ee
solarjoker <chenissolar@gmail.com>
2020-09-08 09:46:44 +0800

    fix: transfer drag

--- components/transfer/index.js
+++ components/transfer/index.js
@@ -1,5 +1,3 @@
 import Transfer from './Transfer'
-// import withDragDropContext from '../lib/withDragDropContext'
-// export default withDragDropContext(Transfer)
-// TODO：临时注释，待处理
-export default Transfer
+import withDragDropContext from '../lib/withDragDropContext'
+export default withDragDropContext(Transfer)
