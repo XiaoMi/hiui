0abd29d640e4371e36d6f62fa1b225e0f6f0067b
Wugaoliang <wugaoliang@xiaomi.com>
2020-09-10 15:28:50 +0800

    fix: #1172

--- docs/demo/form/section-two-col.jsx
+++ docs/demo/form/section-two-col.jsx
@@ -21,29 +21,49 @@ class Demo extends React.Component {
     const Col = Grid.Col
     const FormItem = Form.Item
     return (
-      <div style={{width: 500}}>
+      <div >
         <Form labelWidth='70' labelPlacement='left'>
           <Row>
             <Col span={12}>
               <FormItem label='姓名' >
-                <Input placeholder={'请输入'} style={{width: '200'}} />
+                <Input placeholder={'请输入'} />
               </FormItem>
             </Col>
             <Col span={12}>
-              <FormItem label='密码' >
-                <Select type='password' placeholder={'请输入'} style={{width: '200'}} />
+              <FormItem label='城市' >
+                <Select
+                  type='multiple'
+                  data={[
+                    { title:'北京', id:'2' },
+                    { title:'上海', id:'2-1' },
+                    { title:'南京', id:'2-2' },
+                    { title:'深圳', id:'2-3' },
+                    { title:'武汉', id:'2-4' },
+                    { title:'杭州', id:'3' },
+                    { title:'重庆', id:'4' },
+                    { title:'成都', id:'5' },
+                    { title:'郑州', id:'6' }
+                  ]}
+                  searchable
+                  showCheckAll
+                  placeholder='请选择'
+                  emptyContent='无匹配数据'
+                  onChange={(item) => {
+                    console.log('多选结果', item)
+                  }}
+                />
               </FormItem>
             </Col>
           </Row>
           <Row>
             <Col span={12}>
               <FormItem label='手机号' >
-                <Input placeholder={'请输入'} style={{width: '200'}} />
+                <Input placeholder={'请输入'} />
               </FormItem>
             </Col>
             <Col span={12}>
               <FormItem label='邮箱' >
-                <Input type='password' placeholder={'请输入'} style={{width: '200'}} />
+                <Input type='password' placeholder={'请输入'} />
               </FormItem>
             </Col>
           </Row>
@@ -67,29 +87,29 @@ class Demo extends React.Component {
     const Col = Grid.Col
     const FormItem = Form.Item
     return (
-      <div style={{width: 500}}>
+      <div >
         <Form labelWidth='70' labelPlacement='left'>
           <Row>
             <Col span={12}>
               <FormItem label='姓名' >
-                <Input placeholder={'请输入'} style={{width: '200'}} />
+                <Input placeholder={'请输入'} />
               </FormItem>
             </Col>
             <Col span={12}>
               <FormItem label='密码' >
-                <Input type='password' placeholder={'请输入'} style={{width: '200'}} />
+                <Input type='password' placeholder={'请输入'} />
               </FormItem>
             </Col>
           </Row>
           <Row>
             <Col span={12}>
               <FormItem label='手机号' >
-                <Input placeholder={'请输入'} style={{width: '200'}} />
+                <Input placeholder={'请输入'} />
               </FormItem>
             </Col>
             <Col span={12}>
               <FormItem label='邮箱' >
-                <Input type='password' placeholder={'请输入'} style={{width: '200'}} />
+                <Input type='password' placeholder={'请输入'} />
               </FormItem>
             </Col>
           </Row>
