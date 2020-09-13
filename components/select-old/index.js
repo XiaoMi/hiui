e8040e9d3667fa84120eba3684a0545d35e09363
solarjoker <chenissolar@gmail.com>
2020-09-13 22:25:33 +0800

    chore: remove useless code

--- components/select-old/index.js
+++
@@ -1,6 +0,0 @@
-import Select from './Select'
-import SelectLegacy from './select-legacy'
-import './style/index'
-import SwitchVersion from '../_util/SwitchVersion'
-
-export default SwitchVersion(Select, SelectLegacy)
