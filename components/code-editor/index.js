941fc0f1df69bb8cd81b4be33091d470be128aaa
Wugaoliang <wugaoliang@xiaomi.com>
2020-10-12 11:28:06 +0800

    feat: code editor

--- components/code-editor/index.js
+++ components/code-editor/index.js
@@ -1,49 +1,18 @@
-import React, { forwardRef } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 
-import ReactQuill from 'react-quill'
-
-import Tooltip from '../tooltip'
-import './style/index'
-
-const QuillBarTooltip = props => {
-  const { toolbarsName, tooltipTitle, children, showTooltip = true } = props
+import { UnControlled, Controlled } from 'react-codemirror2'
+import 'codemirror/lib/codemirror.css'
+import './style/index.js'
+const CodeEditor = ({ value, className, ...props }) => {
   return (
-    <span className='hi-quill-toolbar-tooltip'>
-      {showTooltip ? (
-        <Tooltip
-          className='hi-quill-toolbar'
-          title={tooltipTitle || toolbarsName}
-        >
-          {children || <button className={`ql-${toolbarsName}`} />}
-        </Tooltip>
-      ) : (
-        <div className='hi-quill-toolbar hi-quill-custom'>
-          {children || <button className={`ql-${toolbarsName}`} />}
-        </div>
-      )}
-    </span>
-  )
-}
-const InternalQuill = props => {
-  const { className, innerRef, theme } = props
-
-  return (
-    <ReactQuill
-      {...props}
-      ref={el => {
-        innerRef && (innerRef.current = el)
-      }}
-      className={classNames({
-        'hi-quill-content': !theme,
+    <UnControlled
+      value={value}
+      className={classNames('hi-codeEditor', {
         className
       })}
+      {...props}
     />
   )
 }
-
-const RichTextEditor = forwardRef((props, ref) => {
-  return <InternalQuill {...props} innerRef={ref} />
-})
-export default RichTextEditor
-export { QuillBarTooltip }
+export default CodeEditor
