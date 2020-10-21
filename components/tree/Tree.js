bcc4264b593f40083f053b15878a6845d1622857
Wugaoliang <wugaoliang@xiaomi.com>
2020-10-21 15:15:15 +0800

    feat: legacy tree

---
+++ components/tree/Tree.js
@@ -0,0 +1,507 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import BaseTree from './BaseTree'
+import Input from '../input'
+import Button from '../button'
+import Modal from '../modal'
+import { getAncestorIds, findNode } from './util'
+import _ from 'lodash'
+import { v4 as uuidv4 } from 'uuid'
+import CustomTreeNode from './customTreeNode'
+import axios from 'axios'
+import Provider from '../context'
+import './style/index'
+
+const PREFIX = 'hi-tree'
+
+const getMatchedNodes = (data, searchValue, matchedNodes = []) => {
+  data.forEach((item) => {
+    if (searchValue !== '' && item.title.includes(searchValue)) {
+      matchedNodes.push(item)
+    }
+    if (item.children) {
+      getMatchedNodes(item.children, searchValue, matchedNodes)
+    }
+  })
+  return matchedNodes
+}
+const getShowData = (data, matchedIds, filtedIds) => {
+  for (let i = 0; i < data.length; i++) {
+    if (matchedIds.includes(data[i].id)) {
+    } else if (filtedIds.includes(data[i].id)) {
+      getShowData(data[i].children, matchedIds, filtedIds)
+    } else {
+      data.splice(i, 1)
+      i = i - 1
+    }
+  }
+  return data
+}
+
+const getDefaultIds = (data, collections = []) => {
+  data.forEach((d) => {
+    if (d.children) {
+      collections.push(d.id)
+      getDefaultIds(d.children, collections)
+    }
+  })
+  return collections
+}
+
+const Tree = (props) => {
+  const {
+    searchable,
+    searchConfig = {},
+    data,
+    expandedIds,
+    defaultExpandedIds,
+    defaultExpandAll = false,
+    filter = false,
+    contextMenu,
+    editable,
+    onBeforeSave,
+    onSave,
+    onBeforeDelete,
+    onDelete,
+    onLoadChildren,
+    onExpand,
+    onDragStart,
+    onDrop,
+    onDropEnd,
+    theme,
+    localeDatas
+  } = props
+  const localMap = localeDatas.tree || {}
+  const { placeholder = localMap.searchPlaceholder, emptyContent = localMap.searchEmptyResult } = searchConfig
+  const [cacheData, updateCacheData] = useState(data)
+  useEffect(() => {
+    updateCacheData(data)
+  }, [data])
+
+  const [menuVisible, setMenuVisible] = useState(null)
+  const [modalVisible, setModalVisible] = useState(null)
+
+  const [editingNodes, setEditingNodes] = useState([])
+
+  const [searchValue, setSearchValue] = useState('')
+  const [matchedNodes, setMatchedNodes] = useState([])
+  const [filteredIds, setFilteredIds] = useState([])
+  const [expanded, setExpanded] = useState(
+    expandedIds || defaultExpandedIds || (defaultExpandAll && getDefaultIds(data)) || []
+  )
+
+  useEffect(() => {
+    if (expandedIds) {
+      setExpanded(expandedIds)
+    }
+  }, [expandedIds])
+
+  const showData = getShowData(
+    _.cloneDeep(cacheData),
+    matchedNodes.map((n) => n.id),
+    filteredIds
+  )
+  // 加载节点
+  const loadChildren = useCallback(
+    (node) => {
+      if (onLoadChildren) {
+        return axios(onLoadChildren(node)).then(
+          (res) => {
+            const dataCache = _.cloneDeep(cacheData)
+            const loadNode = findNode(node.id, dataCache)
+            loadNode.children = res.data
+            updateCacheData(dataCache)
+            return res
+          },
+          (error) => {
+            return error
+          }
+        )
+      }
+    },
+    [onLoadChildren, cacheData]
+  )
+  // 移动节点
+  const switchNode = useCallback((targetItemId, sourceItemId, data, allData, dropDividerPosition) => {
+    const sourceNode = findNode(sourceItemId, allData)
+    const _data = [...data]
+    _data.forEach((item, idx) => {
+      if (item.id === targetItemId) {
+        const position = dropDividerPosition === 'down' ? idx + 1 : idx
+        data.splice(position, 0, sourceNode)
+      } else {
+        if (item.children) {
+          if (item.children.some((e) => e.id === targetItemId)) {
+            const index = item.children.findIndex((i) => i.id === targetItemId)
+            const position = dropDividerPosition === 'down' ? index + 1 : index
+            item.children.splice(position, 0, sourceNode)
+          } else {
+            switchNode(targetItemId, sourceItemId, item.children, allData, dropDividerPosition)
+          }
+        }
+      }
+    })
+  }, [])
+
+  const addDropNode = useCallback((targetItemId, sourceItemId, data, allData) => {
+    data.forEach((d, index) => {
+      if (d.id === targetItemId) {
+        const sourceNode = findNode(sourceItemId, allData)
+        if (!d.children) {
+          d.children = []
+        }
+        d.children.push(sourceNode)
+      } else {
+        if (d.children) {
+          addDropNode(targetItemId, sourceItemId, d.children, allData)
+        }
+      }
+    })
+  }, [])
+  // 删除拖动的节点
+  const _delDragNode = useCallback((itemId, data) => {
+    data.forEach((d, index) => {
+      if (d.id === itemId) {
+        data.splice(index, 1)
+      } else {
+        if (d.children) {
+          _delDragNode(itemId, d.children)
+        }
+      }
+    })
+  }, [])
+
+  const moveNode = useCallback(
+    ({ targetId, sourceId, direction, depth }) => {
+      const _dataCache = _.cloneDeep(cacheData)
+      _delDragNode(sourceId, _dataCache)
+      if (direction === 'in') {
+        addDropNode(targetId, sourceId, _dataCache, cacheData)
+      } else {
+        switchNode(targetId, sourceId, _dataCache, cacheData, direction)
+      }
+      if (onDrop) {
+        const sourceNode = findNode(sourceId, cacheData)
+        const dragNode = findNode(targetId, cacheData)
+        const result = onDrop(
+          sourceNode,
+          dragNode,
+          { before: cacheData, after: _dataCache },
+          { before: depth.source, after: direction === 'in' ? depth.target + 1 : depth.target }
+        )
+        if (result === true) {
+          updateCacheData(_dataCache)
+          if (onDropEnd) {
+            onDropEnd(sourceNode, dragNode)
+          }
+        } else if (result && typeof result.then === 'function') {
+          result.then((res) => {
+            updateCacheData(_dataCache)
+            if (onDropEnd) {
+              onDropEnd(sourceNode, dragNode)
+            }
+          })
+        }
+      } else {
+        updateCacheData(_dataCache)
+      }
+    },
+    [cacheData, _delDragNode, addDropNode, switchNode, onDrop, onDropEnd]
+  )
+
+  // 编辑节点
+  const editNode = (node) => {
+    setEditingNodes(editingNodes.concat(node))
+  }
+  // 同步编辑值
+  const onValueChange = useCallback(
+    (value, nodeId) => {
+      setEditingNodes(
+        editingNodes
+          .filter((n) => n.id !== nodeId)
+          .concat(
+            Object.assign(
+              {},
+              editingNodes.find((n) => n.id === nodeId),
+              {
+                title: value
+              }
+            )
+          )
+      )
+    },
+    [editingNodes]
+  )
+  // 取消编辑
+  const cancelEdit = useCallback(
+    (nodeId) => {
+      setEditingNodes(editingNodes.filter((n) => n.id !== nodeId))
+    },
+    [editingNodes]
+  )
+
+  // 添加兄弟节点
+  const _addSibNode = useCallback((node, data, addNode) => {
+    data.forEach((d, index) => {
+      if (d.id === node.id) {
+        data.splice(index + 1, 0, addNode)
+      } else {
+        if (d.children) {
+          _addSibNode(node, d.children, addNode)
+        }
+      }
+    })
+  }, [])
+
+  const addSiblingNode = useCallback(
+    (node) => {
+      const dataCache = _.cloneDeep(cacheData)
+      const addNode = { id: uuidv4(), title: '', TREE_NODE_TYPE: 'add' }
+      _addSibNode(node, dataCache, addNode)
+      setEditingNodes(editingNodes.concat(addNode))
+      updateCacheData(dataCache)
+    },
+    [cacheData, editingNodes, _addSibNode]
+  )
+
+  // 添加子节点
+  const _addChildNode = useCallback((node, data, addNode) => {
+    data.forEach((d, index) => {
+      if (d.id === node.id) {
+        if (!d.children) {
+          d.children = []
+        }
+        d.children.push(addNode)
+      } else {
+        if (d.children) {
+          _addChildNode(node, d.children, addNode)
+        }
+      }
+    })
+  }, [])
+  const addChildNode = useCallback(
+    (node) => {
+      const dataCache = _.cloneDeep(cacheData)
+      const addNode = { id: uuidv4(), title: '', TREE_NODE_TYPE: 'add' }
+      _addChildNode(node, dataCache, addNode)
+      setEditingNodes(editingNodes.concat(addNode))
+      updateCacheData(dataCache)
+      setExpanded(expanded.concat(node.id))
+    },
+    [editingNodes, cacheData, _addChildNode, expanded]
+  )
+
+  // 取消添加节点
+  const _cancelAddNode = (node, data) => {
+    data.forEach((d, index) => {
+      if (d.id === node.id) {
+        data.splice(index, 1)
+      } else {
+        if (d.children) {
+          _cancelAddNode(node, d.children)
+        }
+      }
+    })
+  }
+  const cancelAddNode = useCallback(
+    (node) => {
+      const dataCache = _.cloneDeep(cacheData)
+      _cancelAddNode(node, dataCache)
+      updateCacheData(dataCache)
+    },
+    [cacheData, _cancelAddNode]
+  )
+  // 保存
+  const _saveEdit = (itemId, data, nodeEdited) => {
+    data.forEach((d, index) => {
+      if (d.id === itemId) {
+        d.title = nodeEdited.title
+        delete d.TREE_NODE_TYPE
+      } else {
+        if (d.children) {
+          _saveEdit(itemId, d.children, nodeEdited)
+        }
+      }
+    })
+  }
+  const saveEdit = useCallback(
+    (enode) => {
+      const nodeEdited = { ...editingNodes.find((node) => node.id === enode.id) }
+      const dataCache = _.cloneDeep(cacheData)
+      _saveEdit(enode.id, dataCache, nodeEdited)
+      if (onBeforeSave) {
+        const result = onBeforeSave(nodeEdited, { before: cacheData, after: dataCache }, enode.depth)
+        if (result === true) {
+          updateCacheData(dataCache)
+          onSave(nodeEdited, dataCache)
+        }
+      } else {
+        updateCacheData(dataCache)
+        onSave(enode, dataCache)
+      }
+      setEditingNodes(editingNodes.filter((n) => n.id !== enode.id))
+    },
+    [editingNodes, cacheData, _saveEdit]
+  )
+
+  // 删除节点
+  const _deleteNode = (itemId, data) => {
+    data.forEach((d, index) => {
+      if (d.id === itemId) {
+        data.splice(index, 1)
+      } else {
+        if (d.children) {
+          _deleteNode(itemId, d.children)
+        }
+      }
+    })
+  }
+  const deleteNode = useCallback(
+    (node) => {
+      const dataCache = _.cloneDeep(cacheData)
+      _deleteNode(node.id, dataCache)
+      if (onBeforeDelete) {
+        const result = onBeforeDelete(node, { before: cacheData, after: dataCache }, node.depth)
+        if (result === true) {
+          updateCacheData(dataCache)
+          onDelete(node, dataCache)
+        }
+      } else {
+        updateCacheData(dataCache)
+        onDelete(node, dataCache)
+      }
+    },
+    [cacheData, _deleteNode]
+  )
+
+  const menuRender = useCallback(
+    (node) => {
+      let menu = [
+        { title: localMap.edit, type: 'editNode' },
+        { title: localMap.addChildNode, type: 'addChildNode' },
+        { title: localMap.addNode, type: 'addSiblingNode' },
+        { title: localMap.del, type: 'deleteNode' }
+      ]
+      if (contextMenu) {
+        menu = contextMenu(node)
+      }
+      return (
+        <ul className={`${PREFIX}__menu theme__${theme}`}>
+          {menu.map((m, index) => (
+            <li
+              className={`menu-item`}
+              key={index}
+              onClick={(e) => {
+                e.stopPropagation()
+                if (m.onClick) {
+                  m.onClick(node)
+                } else {
+                  if (m.type === 'editNode') {
+                    editNode(node)
+                  }
+                  if (m.type === 'addSiblingNode') {
+                    addSiblingNode(node)
+                  }
+                  if (m.type === 'addChildNode') {
+                    addChildNode(node)
+                  }
+                  if (m.type === 'deleteNode') {
+                    setModalVisible(node)
+                  }
+                }
+                setMenuVisible(null)
+              }}
+            >
+              {m.title}
+            </li>
+          ))}
+        </ul>
+      )
+    },
+    [editNode, addChildNode, addSiblingNode]
+  )
+
+  const treeNodeRender = useCallback(
+    (node, { selected }, treeNodeRef, onSelectNode) => {
+      return (
+        <CustomTreeNode
+          node={node}
+          status={{ selected }}
+          onSelectNode={onSelectNode}
+          searchValue={searchValue}
+          editingNodes={editingNodes}
+          menuVisible={menuVisible}
+          searchable={searchable}
+          onValueChange={onValueChange}
+          saveEdit={saveEdit}
+          cancelEdit={cancelEdit}
+          cancelAddNode={cancelAddNode}
+          setMenuVisible={setMenuVisible}
+          editable={editable}
+          menuRender={menuRender}
+          localeDatas={localeDatas}
+        />
+      )
+    },
+    [searchValue, editingNodes, menuVisible, searchable]
+  )
+  return (
+    <React.Fragment>
+      {searchable && (
+        <div className={`${PREFIX}__searcher theme__${theme}`}>
+          <Input
+            value={searchValue}
+            type="text"
+            placeholder={placeholder}
+            onChange={(e) => {
+              const matchedNodes = getMatchedNodes(cacheData, e.target.value)
+              let filteredNodes = []
+              matchedNodes.forEach((node) => {
+                const ancestors = getAncestorIds(node.id, cacheData, [])
+                filteredNodes = filteredNodes.concat(ancestors)
+              })
+              setSearchValue(e.target.value)
+              setMatchedNodes(matchedNodes)
+              setFilteredIds(_.uniq(filteredNodes))
+              setExpanded(_.uniq(filteredNodes))
+            }}
+            append={<Button icon="search" />}
+            style={{ width: '250px', marginBottom: '24px' }}
+          />
+          {matchedNodes.length === 0 && searchValue !== '' && (
+            <div className="searcher__result--empty">{emptyContent}</div>
+          )}
+        </div>
+      )}
+      <BaseTree
+        {...props}
+        className={`theme__${theme}`}
+        onLoadChildren={onLoadChildren ? loadChildren : null}
+        treeNodeRender={treeNodeRender}
+        expandedIds={expanded}
+        onExpand={(expandedNode, isExpanded, ids) => {
+          setExpanded(ids)
+          if (onExpand) {
+            onExpand(expandedNode, isExpanded, ids)
+          }
+        }}
+        onDrop={moveNode}
+        onDragStart={onDragStart}
+        data={filter && searchable && searchValue !== '' ? showData : cacheData}
+      />
+      <Modal
+        title={localMap.modalTitle}
+        visible={modalVisible !== null}
+        onConfirm={() => {
+          deleteNode(modalVisible)
+          setModalVisible(null)
+        }}
+        onCancel={() => {
+          setModalVisible(null)
+        }}
+      >
+        {localMap.delTips}
+      </Modal>
+    </React.Fragment>
+  )
+}
+export default Provider(Tree)
---
+++ components/tree/tree-legacy/tree-v1/util.js
@@ -0,0 +1,319 @@
+/**
+ * @Author lishuaishuai <lishuaishuai@xiaomi.com>
+ * @Date 2018-03-22 11:14:48
+ * @Description tree相关工具封装
+ */
+
+/**
+ * toggleSlide动画
+ * @param {element} el 目标节点
+ * @param {boolen} isClose 当前状态是否为关闭
+ * @param {function} func 动画结束后的回调行数
+ */
+export function toggleSlide(el, isClose, func) {
+  if (!isClose) {
+    el.style.display = 'block'
+    el.style.height = 0
+  }
+  const maxDelay = 300
+  const height = el.scrollHeight
+  const speed = Math.max(height / maxDelay, 0.5)
+  let sum = 0
+
+  let start = null
+  const animate = (timestamp) => {
+    if (!start) start = timestamp
+    const progress = timestamp - start
+    sum = progress * speed
+    el.style.height = `${isClose ? height - sum : sum}px`
+    if (height < sum) {
+      if (isClose) {
+        el.style.display = 'none'
+      }
+      el.style.height = ''
+      func()
+    } else {
+      window.requestAnimationFrame(animate)
+    }
+  }
+  window.requestAnimationFrame(animate)
+}
+/**
+ * 计算鼠标在当前节点的位置
+ * @param {*} event
+ * @param {*} treeNode 拖拽进入的当前节点
+ */
+export function calcDropPosition(event, treeNode) {
+  const offsetTop = getOffset(treeNode).top
+  const offsetHeight = treeNode.offsetHeight
+  const pageY = event.pageY
+  const gapHeight = 2
+  if (pageY > offsetTop + offsetHeight - gapHeight) {
+    return 1
+  }
+  if (pageY < offsetTop + gapHeight) {
+    return -1
+  }
+  return 0
+}
+/**
+ * 计算offset
+ * @param {*} ele
+ */
+export function getOffset(ele) {
+  if (!ele.getClientRects().length) {
+    return { top: 0, left: 0 }
+  }
+
+  const rect = ele.getBoundingClientRect()
+  if (rect.width || rect.height) {
+    const doc = ele.ownerDocument
+    const win = doc.defaultView
+    const docElem = doc.documentElement
+
+    return {
+      top: rect.top + win.pageYOffset - docElem.clientTop,
+      left: rect.left + win.pageXOffset - docElem.clientLeft
+    }
+  }
+
+  return rect
+}
+/**
+ * 将当前节点移动到目标节点之前
+ * @param {ele} cur 当前节点
+ * @param {ele} tar 目标节点
+ */
+export function insBefore(cur, tar) {
+  const current = cur.parentNode.parentNode // li
+  const target = tar.parentNode // li
+  target.parentNode.insertBefore(current, target)
+}
+/**
+ * 将当前节点移动到目标节点之后
+ * @param {ele} cur 当前节点
+ * @param {ele} tar 目标节点
+ */
+export function insAfter(cur, tar) {
+  const current = cur.parentNode.parentNode // li
+  const target = tar.parentNode // li
+  if (target.parentNode.lastChild === target) {
+    target.parentNode.appendChild(current)
+  } else {
+    target.parentNode.insertBefore(current, target.nextSibling)
+  }
+}
+/**
+ * 将当前节点移动为目标节点子节点
+ * @param {ele} cur 当前节点
+ * @param {ele} tar 目标节点
+ */
+export function insChild(cur, tar) {
+  const current = cur.parentNode.parentNode // li
+  const target = tar.parentNode.parentNode // li
+  if (current === target) return
+  if (target.childNodes.length > 1) {
+    tar.parentNode.nextSibling.appendChild(current)
+  } else {
+    const ul = document.createElement('ul')
+    ul.setAttribute('class', 'hi-tree-child')
+    ul.appendChild(current)
+    target.appendChild(ul)
+  }
+  target.classList.remove('switcher-none')
+}
+
+/**
+ * 深拷贝
+ * @param {*} arr
+ */
+export function deepClone(arr) {
+  let i
+  let copy
+
+  if (Array.isArray(arr)) {
+    copy = arr.slice(0)
+    for (i = 0; i < copy.length; i += 1) {
+      copy[i] = deepClone(copy[i])
+    }
+    return copy
+  } else if (typeof arr === 'object') {
+    return Object.assign({}, arr)
+  }
+  return arr
+}
+
+export function deepMap(data, parent) {
+  let arr = []
+  for (const key in data) {
+    const item = { ...data[key] }
+    if (parent) {
+      item.parent = parent
+    } else {
+      item.parent = []
+    }
+    if (item.children && item.children.length > 0) {
+      const childParent = [...item.parent]
+      childParent.unshift(item.id)
+      arr = arr.concat(deepMap(item.children, childParent))
+      delete item.children
+    } else {
+      // item.parent = parent
+    }
+    arr.push(item)
+  }
+  return arr
+}
+
+export function getChild(data, id) {
+  const arr = []
+  data.forEach((item) => {
+    if (!item.parent.includes(id)) {
+      return
+    }
+    if (arr.includes(id)) {
+      return
+    }
+    arr.push(item.id)
+  })
+  return arr
+}
+
+export function getSemi(data, checks) {
+  const all = deepMap(data)
+  const arr = all
+    .map((item) => {
+      item.child = getChild(all, item.id)
+      item.family = item.parent.concat(item.child)
+      item.semi = false
+      let num = 0
+      checks.forEach((c) => {
+        if (item.child.includes(c)) {
+          num = num + 1
+        }
+      })
+      item.num = num
+      item.semi = num !== 0 && num !== item.child.length
+      return item
+    })
+    .filter((item) => item.semi)
+    .map((item) => item.id)
+  return arr
+}
+
+export function getChildren(data, id) {
+  const all = deepMap(data)
+  return all
+    .map((item) => {
+      item.child = getChild(all, item.id)
+      return item
+    })
+    .find((item) => item.id === id).child
+}
+
+export function getDisabled(data) {
+  const all = deepMap(data)
+  return all.filter((item) => item.disabled).map((item) => item.id)
+}
+
+export function getItem(data, id) {
+  const all = deepMap(data)
+  return all.find((item) => item.id === id)
+}
+export function getAll(data, checkedKeys) {
+  let all = deepMap(data)
+  all = all.map((item) => {
+    item.child = getChild(all, item.id)
+    item.family = item.parent.concat(item.child)
+    item.semi = false
+    let num = 0
+    checkedKeys.forEach((c) => {
+      if (item.child.includes(c)) {
+        num = num + 1
+      }
+    })
+    item.num = num
+    item.semi = num !== 0 && num !== item.child.length
+    return item
+  })
+  return all
+}
+
+export const dealData = (data, tempData = {}, parent = null) => {
+  if (data.length === 0) {
+    return data
+  }
+  data.map((item) => {
+    tempData[item.id] = { ...item }
+    if (parent) {
+      tempData[item.id].parent = parent
+    }
+    if (item.children && item.children.length > 0) {
+      const tempArr = []
+      item.children.map((i) => {
+        tempArr.push(i.id)
+      })
+      tempData[item.id].children = tempArr
+      dealData(item.children, tempData, item.id)
+    }
+  })
+}
+
+// 寻找某一节点的父节点
+export const getParentId = (id, data) => {
+  let parentId
+  data.forEach((item) => {
+    if (item.children) {
+      if (item.children.some((item) => item.id === id)) {
+        parentId = item.id
+      } else if (getParentId(id, item.children)) {
+        parentId = getParentId(id, item.children)
+      }
+    }
+  })
+  return parentId
+}
+// 寻找某一节点的所有子节点
+export const getChildrenIds = (node, arr = []) => {
+  if (node.children) {
+    arr = node.children.map((i) => i.id).concat(arr)
+  }
+  node.children.forEach((c) => getChildrenIds(c, arr))
+  return arr
+}
+// 寻找某一节点的所有祖先节点
+export const getAncestorIds = (id, data, arr = []) => {
+  if (getParentId(id, data)) {
+    arr.push(getParentId(id, data))
+    getAncestorIds(getParentId(id, data), data, arr)
+  }
+  return arr
+}
+
+// 收集所有需要展开的节点 id
+export const collectExpandId = (data, searchValue, collection = [], allData) => {
+  data.forEach((item) => {
+    if (searchValue && item.title.includes(searchValue)) {
+      const parentIds = getAncestorIds(item.id, allData, [])
+      collection.splice(collection.length - 1, 0, ...parentIds)
+    }
+    if (item.children) {
+      collectExpandId(item.children, searchValue, collection, allData)
+    }
+  })
+  return collection
+}
+// 给定一个结合，根据 id 寻找节点
+export const findNode = (itemId, data) => {
+  let node
+  data.forEach((d, index) => {
+    if (d.id === itemId) {
+      node = d
+    } else {
+      if (d.children && findNode(itemId, d.children)) {
+        node = findNode(itemId, d.children)
+      }
+    }
+  })
+  return node
+}
