9e4426e8d9df19449cc3d752dd43227f86946dc3
Wugaoliang <wugaoliang@xiaomi.com>
2020-10-22 16:19:49 +0800

    fix: datepicker

--- docs/demo/table/section-base.jsx
+++ docs/demo/table/section-base.jsx
@@ -4,7 +4,7 @@ import Table, { LegacyTable } from '../../../components/table'
 import Watermark from '../../../components/Watermark'
 const prefix = 'table-base'
 const desc = ['基础：展示二维数据', '斑马行：优化以行为主的阅读体验']
-const rightOptions = ['基础', '斑马行', 'v2']
+const rightOptions = ['基础', '斑马行']
 const code = [
   {
     code: `import React from 'react'
