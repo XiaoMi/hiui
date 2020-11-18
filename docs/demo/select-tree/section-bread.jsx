b8fe45f1d55de07023790f16ff60412b7de34475
Wugaoliang <wugaoliang@xiaomi.com>
2020-11-18 10:39:57 +0800

    fix: #1283

--- docs/demo/select-tree/section-bread.jsx
+++ docs/demo/select-tree/section-bread.jsx
@@ -334,7 +334,7 @@ const code = [
                 method: 'GET',
                 key: 'id',
                 params: {pId: key},
-                url: 'http://localhost:8001/tree',
+                url: 'http://my-json-server.typicode.com/hiui-group/db/fulldata',
                 transformResponse: (res) => {
                   return res.map(r => {
                     return {
