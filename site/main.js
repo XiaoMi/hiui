c4ae2a7dc94c8086e32b6aea1fcc6ac311b82f1a
solarjoker <chenissolar@gmail.com>
2020-11-26 10:59:52 +0800

    fix: #1381

--- site/main.js
+++ site/main.js
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux'
 import App from './view'
 import store from './redux/store'
 import './style/index.scss'
+import { LocaleContext } from '../components/context'
 render(
   <Provider store={store}>
-    <App />
+    <LocaleContext.Provider value="en-US">
+      <App />
+    </LocaleContext.Provider>
   </Provider>,
   document.getElementById('app')
 )
