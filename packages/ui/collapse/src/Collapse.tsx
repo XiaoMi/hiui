87064f950472d1e321bfc3d59350392d8453f11b
laoyao <ordinary_person@outlook.com>
2021-07-09 17:54:29 +0800

    feat(collapse): 初步功能迁移完成

--- packages/ui/collapse/src/Collapse.tsx
+++ packages/ui/collapse/src/Collapse.tsx
@@ -1,13 +1,17 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useCallback, useMemo } from 'react'
 import { cx, getPrefixCls } from '@hi-ui/classname'
 import { __DEV__ } from '@hi-ui/env'
+import { CollapseProvider, ICollapseContext } from './context'
+import { useUncontrolledState } from 'packages/hooks/use-uncontrolled-state/lib/types'
 
 const _role = 'collapse'
 const _prefix = getPrefixCls(_role)
 
+const EmptyArray = [] as string[]
+
 /**
-* TODO: What is Collapse
-*/
+ * 折叠面板
+ */
 export const Collapse = forwardRef<HTMLDivElement | null, CollapseProps>(
   (
     {
@@ -15,16 +19,63 @@ export const Collapse = forwardRef<HTMLDivElement | null, CollapseProps>(
       role = _role,
       className,
       children,
-      ...rest
+      style,
+      defaultActiveId = EmptyArray,
+      activeId,
+      accordion,
+      onChange,
+      arrowPlacement = 'right',
+      showArrow = true,
     },
     ref
   ) => {
     const cls = cx(prefixCls, className)
 
+    // 兼容受控与非受控
+    const [useActiveIds, tryChange] = useUncontrolledState<string[]>(
+      defaultActiveId,
+      activeId,
+      onChange
+    )
+
+    const onClickPanel = useCallback(
+      (key: string) => {
+        let result = [...useActiveIds]
+
+        // 根据是否为手风琴模式来执行不同数据传递
+        if (accordion) {
+          result = result[0] === key ? [] : [key]
+        } else {
+          const searchIndex = result.indexOf(key)
+
+          if (searchIndex >= 0) {
+            result.splice(searchIndex, 1)
+          } else {
+            result.push(key)
+          }
+        }
+
+        tryChange(result)
+      },
+      [accordion, tryChange, useActiveIds]
+    )
+
+    const provideData = useMemo<ICollapseContext>(
+      () => ({
+        judgeIsActive: (id) => (accordion ? useActiveIds[0] === id : useActiveIds.includes(id)),
+        onClickPanel: onClickPanel,
+        arrowPlacement,
+        showArrow,
+      }),
+      [accordion, onClickPanel, useActiveIds, showArrow, arrowPlacement]
+    )
+
     return (
-      <div ref={ref} role={role} className={cls} {...rest}>
-        {children}
-      </div>
+      <CollapseProvider value={provideData}>
+        <div ref={ref} role={role} className={cls} style={style}>
+          {children}
+        </div>
+      </CollapseProvider>
     )
   }
 )
@@ -46,6 +97,36 @@ export interface CollapseProps {
    * 组件的注入样式
    */
   style?: React.CSSProperties
+
+  /**
+   * 开启手风琴模式
+   * @default false
+   */
+  accordion?: boolean
+  /**
+   * 默认展开的面板 id
+   */
+  defaultActiveId?: string[]
+  /**
+   * 展开的面板 id
+   */
+  activeId?: string[]
+  /**
+   * 箭头所在位置
+   */
+  arrowPlacement?: 'left' | 'right'
+  /**
+   * 是否显示展开箭头
+   */
+  showArrow?: boolean
+  /**
+   * 切换时的回调
+   */
+  onChange?: (ids: string[]) => void
+  /**
+   * 折叠面板
+   */
+  children?: React.ReactNode
 }
 
 if (__DEV__) {
