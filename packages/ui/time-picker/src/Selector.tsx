9ba390432c3d6ef978194cf9a0fa089467d23c05
laoyao <ordinary_person@outlook.com>
2021-11-11 16:52:18 +0800

    feat(time-picker): 完成选择面板相关功能

---
+++ packages/ui/time-picker/src/Selector.tsx
@@ -0,0 +1,148 @@
+import React, { FC, useCallback, useEffect, useMemo, useRef } from 'react'
+import { cx } from '@hi-ui/classname'
+import { UpOutlined, DownOutlined } from '@hi-ui/icons'
+
+export interface SelectorItem {
+  title: string
+  disabled: boolean
+  id: string
+}
+interface SelectorProps {
+  prefix: string
+  value: string
+  data: SelectorItem[]
+  onChange: (item: SelectorItem) => void
+  itemHeight: number
+  // 必须为奇数
+  fullDisplayItemNumber: number
+}
+
+export const Selector: FC<SelectorProps> = (props) => {
+  const { prefix, value, data, onChange, itemHeight, fullDisplayItemNumber } = props
+  const componentPrefix = `${prefix}__selector`
+  const stopScrollTimeoutHandler = useRef(-1)
+  // 滚动容器引用
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null)
+
+  const safePadding = useMemo(() => ((fullDisplayItemNumber - 1) * itemHeight) / 2, [
+    fullDisplayItemNumber,
+    itemHeight,
+  ])
+
+  const calcCurrentIndex = useCallback(
+    (scrollTop: number) => {
+      return Math.floor(
+        (scrollTop - safePadding + (fullDisplayItemNumber * itemHeight) / 2) / itemHeight
+      )
+    },
+    [safePadding, itemHeight, fullDisplayItemNumber]
+  )
+
+  const scrollToMatchIndex = useCallback(
+    (index: number) => {
+      if (scrollContainerRef.current) {
+        scrollContainerRef.current.scrollTop = index * itemHeight
+      }
+    },
+    [itemHeight]
+  )
+
+  const onScroll = useCallback(
+    (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+      e.persist()
+      e.stopPropagation()
+      // 清除用做判断是否滚动停止的定时器
+      clearTimeout(stopScrollTimeoutHandler.current)
+      // 计算当前选中的 index 下标
+      let currentIndex = calcCurrentIndex((e.target as HTMLDivElement).scrollTop)
+      if (currentIndex > data.length) {
+        currentIndex = data.length - 1
+      }
+
+      // 200ms 后如果事件没有再次响应，则视作滚动停止
+      // 不论是否禁用，我们都需要自动滚动到对应 index
+      stopScrollTimeoutHandler.current = (setTimeout(() => {
+        scrollToMatchIndex(currentIndex)
+        // 通知外部改变
+        // 如果当前 item 被禁用了，则无通知
+        if (data[currentIndex].id !== value && !data[currentIndex].disabled) {
+          onChange(data[currentIndex])
+        }
+      }, 200) as unknown) as number
+    },
+    [calcCurrentIndex, data, onChange, value, scrollToMatchIndex]
+  )
+
+  const onItemClick = useCallback(
+    (item: SelectorItem, index: number) => {
+      if (item.id !== value) {
+        onChange(item)
+      }
+    },
+    [onChange, value]
+  )
+
+  const onShortcutClick = useCallback(
+    (isLast: boolean) => {
+      const currentIndex = data.findIndex((item) => item.id === value)
+      let target = currentIndex
+      if (isLast && currentIndex !== 0) {
+        for (let counter = currentIndex - 1; counter >= 0; counter--) {
+          if (!data[counter].disabled) {
+            target = counter
+            break
+          }
+        }
+      } else if (!isLast && currentIndex !== data.length - 1) {
+        for (let counter = currentIndex + 1; counter <= data.length - 1; counter++) {
+          if (!data[counter].disabled) {
+            target = counter
+            break
+          }
+        }
+      }
+
+      if (target !== currentIndex) {
+        onChange(data[target])
+      }
+    },
+    [data, value, onChange]
+  )
+
+  useEffect(() => {
+    const currentIndex = data.findIndex((item) => item.id === value)
+    scrollToMatchIndex(currentIndex)
+  }, [data, value, scrollToMatchIndex])
+
+  return (
+    <div className={componentPrefix}>
+      <div className={`${componentPrefix}__shortcut`} onClick={() => onShortcutClick(true)}>
+        <UpOutlined />
+      </div>
+      <div
+        onScroll={onScroll}
+        ref={scrollContainerRef}
+        className={`${componentPrefix}__scroll-part`}
+        style={{ height: `${fullDisplayItemNumber * itemHeight}px`, padding: `${safePadding}px 0` }}
+      >
+        {data.map((item, index) => (
+          <div
+            className={cx(`${componentPrefix}__item`, {
+              [`${componentPrefix}__item--disabled`]: item.disabled,
+              [`${componentPrefix}__item--active`]: item.id === value,
+            })}
+            key={item.id}
+            onClick={() => onItemClick(item, index)}
+            style={{ height: `${itemHeight}px` }}
+          >
+            {item.title}
+          </div>
+        ))}
+      </div>
+      <div className={`${componentPrefix}__indicator`} style={{ height: `${itemHeight}px` }} />
+      <div className={`${componentPrefix}__shortcut`} onClick={() => onShortcutClick(false)}>
+        <DownOutlined />
+      </div>
+    </div>
+  )
+}
--- packages/ui/time-picker/src/TimePicker.tsx
+++ packages/ui/time-picker/src/TimePicker.tsx
@@ -2,36 +2,94 @@ import React, { forwardRef } from 'react'
 import { cx, getPrefixCls } from '@hi-ui/classname'
 import { __DEV__ } from '@hi-ui/env'
 import { HiBaseHTMLProps } from '@hi-ui/core'
+import { TimePickerFormat, TimePickerPanel, TimePickerType } from './@types'
 
 const _role = 'time-picker'
 const _prefix = getPrefixCls(_role)
 
 /**
-* TODO: What is TimePicker
-*/
+ * TODO: What is TimePicker
+ */
 export const TimePicker = forwardRef<HTMLDivElement | null, TimePickerProps>(
-  (
-    {
-      prefixCls = _prefix,
-      role = _role,
-      className,
-      children,
-      ...rest
-    },
-    ref
-  ) => {
+  ({ prefixCls = _prefix, role = _role, className, children, ...rest }, ref) => {
     const cls = cx(prefixCls, className)
 
     return (
-      <div ref={ref} role={role} className={cls} {...rest}>
+      <div ref={ref} role={role} className={cls}>
         {children}
       </div>
     )
   }
 )
 
-export interface TimePickerProps extends HiBaseHTMLProps<'div'> {
-
+export interface TimePickerProps extends Omit<HiBaseHTMLProps<'div'>, 'placeholder'> {
+  /**
+   * 选择器类型
+   * @default 'single'
+   */
+  type?: TimePickerType
+  /**
+   * 选择器格式
+   * @default 'HH:mm:ss'
+   */
+  format?: TimePickerFormat
+  /**
+   * 当前值（type='single'取数组第一个值，type='range'取数组第一个作为开始，第二个作为结束）
+   */
+  value?: string[]
+  /**
+   * 默认值
+   */
+  defaultValue?: string[]
+  /**
+   * 输入框是否不可编辑
+   * @default false
+   */
+  inputReadonly?: boolean
+  /**
+   * 是否有边框
+   * @default true
+   */
+  bordered?: boolean
+  /**
+   * 小时选项间隔
+   * @default 1
+   */
+  hourStep?: number
+  /**
+   * 分钟选项间隔
+   * @default 1
+   */
+  minuteStep?: number
+  /**
+   * 秒选项间隔
+   * @default 1
+   */
+  secondStep?: number
+  /**
+   * 是否禁用
+   * @default false
+   */
+  disabled?: boolean
+  /**
+   * 输入框占位符
+   */
+  placeholder?: string[]
+  /**
+   * 禁止选择的小时
+   * @default () => []
+   */
+  disabledHours?: (panel: TimePickerPanel) => number[]
+  /**
+   * 禁止选择的分钟
+   * @default () => []
+   */
+  disabledMinutes?: (hour: number, panel: TimePickerPanel) => number[]
+  /**
+   * 禁止选择的秒数
+   * @default () => []
+   */
+  disabledSeconds?: (hour: number, minute: number, panel: TimePickerPanel) => number[]
 }
 
 if (__DEV__) {
