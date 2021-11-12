68fc03ab6a74723ae2502b16ded029a911ed3b89
Flcwl <flcwl@vip.qq.com>
2021-11-12 18:52:48 +0800

    chore: update stories

--- packages/ui/popover/stories/basic.stories.tsx
+++ packages/ui/popover/stories/basic.stories.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Popover from '../src'
+import Button from '@hi-ui/button'
 
 export const Basic = () => {
+  const [btnEl, setBtnEl] = React.useState(null)
+  const [visible, setVisible] = React.useState(false)
+
   return (
     <>
       <h1>Basic</h1>
-      <div className="popover-basic__wrap">
-        <Popover></Popover>
+      <div className="popper-basic__wrap">
+        <Popover visible={visible} attachEl={btnEl} onClose={() => setVisible(false)}>
+          {/* <div style={{ width: 200 }}>HiUI</div> */}
+          {/* <div style={{ height: 200 }}>HiUI</div> */}
+          {/* <div style={{ width: 200, height: 200 }}>HiUI</div> */}
+        </Popover>
+
+        <Button onClick={() => setVisible((prev) => !prev)}>trigger</Button>
       </div>
     </>
   )
