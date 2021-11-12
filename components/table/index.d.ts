1a3cebe69961834e9a7c4cde5adf64b1de3d99a8
flcwl <flcwl@vip.qq.com>
2021-11-12 11:10:32 +0800

    feat: #1994

--- components/table/index.d.ts
+++ components/table/index.d.ts
@@ -93,6 +93,9 @@ export interface TableProps {
   onDragStart?: (rowData: object) => void
   onDrop?: (dragRowData: object, dropRowData: object, data: object, level: Level) => boolean | Promise
   onDropEnd?: (dragRowData: object, dropRowData: object, data: object) => void
+  settingKeyPropName?: string
+  hiddenColKeys?: string[]
+  onHiddenColKeysChange?: (hiddenColKeys: string[]) => void
 }
 declare const Table: React.ComponentType<TableProps>
 export default Table
