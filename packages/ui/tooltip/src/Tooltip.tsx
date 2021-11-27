119385ec615c08b368e514c93499874057306f76
Flcwl <flcwl@vip.qq.com>
2021-11-27 14:23:23 +0800

    feat: 添加 tooltip 组件

--- packages/ui/tooltip/src/Tooltip.tsx
+++ packages/ui/tooltip/src/Tooltip.tsx
@@ -1,133 +1,47 @@
-import React, { useState, useCallback, useMemo } from 'react'
-import { render, unmountComponentAtNode } from 'react-dom'
+import React, { cloneElement, isValidElement, forwardRef } from 'react'
 import { cx, getPrefixCls } from '@hi-ui/classname'
 import { __DEV__ } from '@hi-ui/env'
-import Popper, { PopperProps } from '@hi-ui/popper'
-import { useUncontrolledState } from '@hi-ui/use-uncontrolled-state'
+import { HiBaseHTMLProps } from '@hi-ui/core'
+import { PopperPortal as Popper } from '@hi-ui/popper'
+import { useTooltip, UseTooltipProps } from './use-tooltip'
 
 const _role = 'tooltip'
 const _prefix = getPrefixCls(_role)
 
-const tooltipInstance: Record<string, HTMLDivElement> = {}
-
-const open = (target: HTMLElement | null, tooltipProps: TooltipProps, key: string): void => {
-  const mountNode = document.createElement('div')
-  const { title, placement = 'top', prefixCls = _prefix, overlayClassName } = tooltipProps
-  const cls = cx(prefixCls, overlayClassName)
-  render(
-    <Popper className={cls} visible attachEl={target} placement={placement} arrow>
-      <div className={`${prefixCls}__content`}>{title}</div>
-    </Popper>,
-    mountNode
-  )
-  tooltipInstance[key] = mountNode
-}
-const close = (key: string): void => {
-  const instance = tooltipInstance[key]
-  if (instance) {
-    unmountComponentAtNode(instance)
-    instance.parentNode?.removeChild(instance)
-  }
-}
-
 /**
  * TODO: What is Tooltip
  */
+export const Tooltip = forwardRef<HTMLDivElement | null, TooltipProps>(
+  ({ prefixCls = _prefix, className, children, content, ...rest }, ref) => {
+    const { rootProps, getTriggerProps, getPopperProps, getOverlayProps } = useTooltip(rest)
 
-// TODO:需要考虑 children 禁用的情况
-// TODO:需要考虑 children 为数组的情况
-// TODO:考虑 delay 消失的情况
-// TODO: 目前 popper 有遮照组件的问题
-const TooltipComp: React.FC<TooltipProps> = ({
-  prefixCls = _prefix,
-  role = _role,
-  overlayClassName,
-  overlayStyle,
-  children,
-  title,
-  visible,
-  placement = 'top',
-}) => {
-  const cls = cx(prefixCls, overlayClassName)
-  const [tipVisible, setTipVisible] = useUncontrolledState(false, visible)
-  const [triggerRef, setTriggerRef] = useState<HTMLElement | null>(null)
+    const cls = cx(prefixCls, className)
 
-  const onMouseEnter = useCallback<(event: React.MouseEvent) => void>(
-    (e) => {
-      if (React.isValidElement(children)) {
-        if (children.props.onMouseEnter) {
-          children.props.onMouseEnter(e)
-        }
+    return (
+      <>
+        {/* TODO: 警告：子节点必须是合法的 React 元素 */}
+        {isValidElement(children)
+          ? cloneElement(
+              children,
+              // @ts-ignore
+              getTriggerProps(children.props, children.ref)
+            )
+          : null}
+        <Popper {...getPopperProps()} {...getOverlayProps()}>
+          <div ref={ref} className={cls} {...rootProps}>
+            <div className={`${prefixCls}__content`}>{content}</div>
+          </div>
+        </Popper>
+      </>
+    )
+  }
+)
 
-        setTipVisible(true)
-      }
-    },
-    [children, setTipVisible]
-  )
-
-  const onMouseLeave = useCallback<(event: React.MouseEvent) => void>(
-    (e) => {
-      if (React.isValidElement(children)) {
-        if (children.props.onMouseLeave) {
-          children.props?.onMouseLeave(e)
-        }
-
-        setTipVisible(false)
-      }
-    },
-    [children, setTipVisible]
-  )
-
-  const child = useMemo(() => {
-    if (React.isValidElement(children)) {
-      return React.cloneElement(children, { ref: setTriggerRef, onMouseEnter, onMouseLeave })
-    } else {
-      console.error('The children of Tooltip must be a valid element')
-    }
-  }, [children, onMouseEnter, onMouseLeave])
-
-  return (
-    <>
-      {child}
-      <Popper attachEl={triggerRef} visible={tipVisible} arrow placement="top" className={cls}>
-        <div className={`${prefixCls}__content`}>{title}</div>
-      </Popper>
-    </>
-  )
-}
-export const Tooltip = Object.assign(TooltipComp, { open, close })
-
-export interface TooltipProps {
+export interface TooltipProps extends HiBaseHTMLProps<'div'>, UseTooltipProps {
   /**
-   * 组件默认的选择器类
+   * 	提醒内容
    */
-  prefixCls?: string
-  /**
-   * 组件的语义化 Role 属性
-   */
-  role?: string
-  /**
-   * 组件的注入选择器类
-   */
-  overlayClassName?: string
-
-  /**
-   * 组件的注入选择器类
-   */
-  overlayStyle?: React.CSSProperties
-
-  /**
-   * 是否显示（受控）
-   */
-  visible?: boolean
-  /**
-   * 内容
-   */
-  title: React.ReactNode
-  /**
-   * 展示位置
-   */
-  placement?: PopperProps['placement']
+  content: React.ReactNode
 }
 
 if (__DEV__) {
--- packages/ui/tooltip/stories/basic.stories.tsx
+++ packages/ui/tooltip/stories/basic.stories.tsx
@@ -3,12 +3,14 @@ import Tooltip from '../src'
 import Button from '@hi-ui/button'
 
 export const Basic = () => {
+  const content = <span>Tooltip Title</span>
+
   return (
     <>
       <h1>Basic</h1>
-      <div className="tooltip-basic__wrap">
-        <Tooltip title={'triggerPropsEvent'}>
-          <Button disabled>按钮</Button>
+      <div className="Tooltip-basic__wrap">
+        <Tooltip content={content} trigger="click">
+          <Button>trigger</Button>
         </Tooltip>
       </div>
     </>
