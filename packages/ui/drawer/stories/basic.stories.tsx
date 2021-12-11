a60c1f6871e95e0b2587bae789a87e0bebf6b79c
solarjoker <chenissolar@gmail.com>
2021-12-11 17:29:42 +0800

    feat: update drawer story

--- packages/ui/drawer/stories/basic.stories.tsx
+++ packages/ui/drawer/stories/basic.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Drawer from '../src'
 import Button from '@hi-ui/button'
+import { EditOutlined } from '@hi-ui/icons'
 
 export const Basic = () => {
   const [visible, setVisible] = React.useState(false)
@@ -12,16 +13,17 @@ export const Basic = () => {
         <Button onClick={() => setVisible(!visible)}>open</Button>
         <Drawer
           title="Drawer Header Title"
+          extra={<EditOutlined />}
           visible={visible}
           closeOnOverlayClick={true}
           onClose={() => setVisible(false)}
           footer={
             <div style={{ textAlign: 'right' }}>
-              <Button type="primary" key={0} onClick={() => console.log(1)}>
-                确认
-              </Button>
               <Button type="default" key={1} onClick={() => console.log(2)}>
                 取消
+              </Button>
+              <Button type="primary" key={0} onClick={() => console.log(1)}>
+                确认
               </Button>
             </div>
           }
