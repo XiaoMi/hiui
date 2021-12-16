a90ffb11a5f2731de7e37e48f0767fbc891d8f8e
solarjoker <chenissolar@gmail.com>
2021-12-16 21:03:02 +0800

    feat: update button

--- packages/ui/button/stories/size.stories.tsx
+++ packages/ui/button/stories/size.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../src'
+import { PlusOutlined } from '@hi-ui/icons'
 
 export const Size = () => {
   const btnRef = React.useRef(null)
@@ -14,41 +15,53 @@ export const Size = () => {
           <Button type="primary" size="small">
             小号按钮
           </Button>
+          <Button type="primary" icon={<PlusOutlined />} size="small" />
           <Button type="primary">正常按钮</Button>
+          <Button type="primary" icon={<PlusOutlined />} />
           <Button type="primary" size="large">
             大号按钮
           </Button>
+          <Button type="primary" icon={<PlusOutlined />} size="large" />
           <Button type="primary" size="x-large">
             超大号按钮
           </Button>
+          <Button type="primary" icon={<PlusOutlined />} size="x-large" />
         </div>
         <div style={{ marginBottom: 24 }}>
-          <Button type="primary" size="small" appearance="line">
+          <Button type="primary" appearance="line" size="small">
             小号按钮
           </Button>
+          <Button type="primary" appearance="line" icon={<PlusOutlined />} size="small" />
           <Button type="primary" appearance="line">
             正常按钮
           </Button>
-          <Button type="primary" size="large" appearance="line">
+          <Button type="primary" appearance="line" icon={<PlusOutlined />} />
+          <Button type="primary" appearance="line" size="large">
             大号按钮
           </Button>
-          <Button type="primary" size="x-large" appearance="line">
+          <Button type="primary" appearance="line" icon={<PlusOutlined />} size="large" />
+          <Button type="primary" appearance="line" size="x-large">
             超大号按钮
           </Button>
+          <Button type="primary" appearance="line" icon={<PlusOutlined />} size="x-large" />
         </div>
         <div>
-          <Button type="primary" size="small" appearance="link">
+          <Button type="primary" appearance="link" size="small">
             小号按钮
           </Button>
+          <Button type="primary" appearance="link" icon={<PlusOutlined />} size="small" />
           <Button type="primary" appearance="link">
             正常按钮
           </Button>
-          <Button type="primary" size="large" appearance="link">
+          <Button type="primary" appearance="link" icon={<PlusOutlined />} />
+          <Button type="primary" appearance="link" size="large">
             大号按钮
           </Button>
-          <Button type="primary" size="x-large" appearance="link">
+          <Button type="primary" appearance="link" icon={<PlusOutlined />} size="large" />
+          <Button type="primary" appearance="link" size="x-large">
             超大号按钮
           </Button>
+          <Button type="primary" appearance="link" icon={<PlusOutlined />} size="x-large" />
         </div>
       </div>
     </>
