bd02f971d224477e6d8bce98d52671ff04155c10
Flcwl <flcwl@vip.qq.com>
2021-12-20 14:50:15 +0800

    refactor(locale-context): add locale as context value

--- packages/ui/locale-context/src/LocaleContext.tsx
+++ packages/ui/locale-context/src/LocaleContext.tsx
@@ -1,5 +1,9 @@
 import { createContext } from 'react'
 import { Locale } from './locale/interface'
 import defaultLocale from './locale/zh-CN'
+import { LocaleEnum } from './LocaleProvider'
 
-export const LocaleContext = createContext<Locale>(defaultLocale)
+export const LocaleContext = createContext<Locale & { locale: LocaleEnum }>({
+  ...defaultLocale,
+  locale: LocaleEnum['zh-CN'],
+})
--- packages/ui/locale-context/src/LocaleProvider.tsx
+++ packages/ui/locale-context/src/LocaleProvider.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ValueOf } from '@hi-ui/core'
 import { __DEV__ } from '@hi-ui/env'
 import { LocaleContext } from './LocaleContext'
 import localeMap from './locale'
@@ -7,14 +8,34 @@ import localeMap from './locale'
  * TODO: What is LocaleContext
  */
 export const LocaleProvider: React.FC<LocaleProviderProps> = ({ children, locale }) => {
-  return <LocaleContext.Provider value={localeMap[locale]}>{children}</LocaleContext.Provider>
+  return (
+    <LocaleContext.Provider
+      value={{
+        ...localeMap[locale],
+        locale,
+      }}
+    >
+      {children}
+    </LocaleContext.Provider>
+  )
 }
+
+export const LocaleEnum = {
+  'zh-CN': 'zh-CN',
+  'en-US': 'en-US',
+  'zh-HK': 'zh-HK',
+  'zh-TW': 'zh-TW',
+} as const
+
+// 对外暴露同名联合类型
+// eslint-disable-next-line no-redeclare
+export type LocaleEnum = ValueOf<typeof LocaleEnum>
 
 export interface LocaleProviderProps {
   /**
    * 组件默认的选择器类
    */
-  locale: 'zh-CN' | 'en-US' | 'zh-HK' | 'zh-TW'
+  locale: LocaleEnum
 }
 
 if (__DEV__) {
