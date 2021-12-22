d4de63c94f8305ad742bc603139331d1576bc1fe
solarjoker <chenissolar@gmail.com>
2021-12-22 17:28:26 +0800

    feat: finish search style

--- packages/ui/search/stories/basic.stories.tsx
+++ packages/ui/search/stories/basic.stories.tsx
@@ -8,45 +8,9 @@ export const Basic = () => {
       <div className="search-basic__wrap">
         <Search
           style={{ width: 260 }}
+          disabled
           placeholder="搜索关键字"
-          data={[
-            {
-              id: 'miphone',
-              title: '手机',
-              children: [
-                {
-                  id: 1,
-                  title: '小米9 Pro',
-                },
-                {
-                  id: 2,
-                  title: '小米9 探索版',
-                },
-                {
-                  id: 3,
-                  title: '小米9 CC 美图定制版',
-                },
-              ],
-            },
-            {
-              id: 'live',
-              title: '智能生活',
-              children: [
-                {
-                  id: 4,
-                  title: '小米 手环',
-                },
-                {
-                  id: 5,
-                  title: '小米 净水器',
-                },
-                {
-                  id: 6,
-                  title: '小米 小爱音响',
-                },
-              ],
-            },
-          ]}
+
           // onSearch={(keyword) => {
           //   console.log('Input Value', keyword)
           //   keyword && alert('Input Value: ' + keyword)
---
+++ packages/ui/search/stories/data.stories.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import Search from '../src'
+
+export const Data = () => {
+  return (
+    <>
+      <h1>带关联数据</h1>
+      <div className="search-basic__wrap">
+        <div>
+          <Search
+            style={{ width: 260 }}
+            placeholder="搜索关键字"
+            data={[
+              {
+                id: 'miphone',
+                title: '手机',
+              },
+              {
+                id: 'live',
+                title: '智能生活',
+              },
+            ]}
+            onSearch={(keyword) => {
+              console.log('Input Value', keyword)
+            }}
+          />
+        </div>
+        <div>
+          <Search
+            disabled
+            style={{ width: 260 }}
+            placeholder="搜索关键字"
+            data={[
+              {
+                id: 'miphone',
+                title: '手机',
+                children: [
+                  {
+                    id: 1,
+                    title: '小米9 Pro',
+                  },
+                  {
+                    id: 2,
+                    title: '小米9 探索版',
+                  },
+                  {
+                    id: 3,
+                    title: '小米9 CC 美图定制版',
+                  },
+                ],
+              },
+              {
+                id: 'live',
+                title: '智能生活',
+                children: [
+                  {
+                    id: 4,
+                    title: '小米 手环',
+                  },
+                  {
+                    id: 5,
+                    title: '小米 净水器',
+                  },
+                  {
+                    id: 6,
+                    title: '小米 小爱音响',
+                  },
+                ],
+              },
+            ]}
+            // onSearch={(keyword) => {
+            //   console.log('Input Value', keyword)
+            //   keyword && alert('Input Value: ' + keyword)
+            // }}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
