6ed683cd57b5fe31f6c75f4464d64049144a1869
Flcwl <flcwl@vip.qq.com>
2022-01-26 21:29:05 +0800

    chore(tree): update api

--- packages/ui/tooltip/src/index.ts
+++ packages/ui/tooltip/src/index.ts
@@ -3,8 +3,7 @@ import './styles/index.scss'
 import { Tooltip as PureTooltip } from './Tooltip'
 import { withTooltip } from './with-api'
 
-export * from './Tooltip'
-// export { Tooltip as default } from './Tooltip'
+export type { TooltipProps } from './Tooltip'
 
 export * from './use-tooltip'
 
