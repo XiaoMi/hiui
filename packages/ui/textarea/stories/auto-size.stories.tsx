1b28aa7072a4c0dfdbe60b39ead6836b831bac66
Flcwl <flcwl@vip.qq.com>
2022-05-12 14:28:33 +0800

    feat(textarea): add autoSize && showCount support

--- packages/ui/textarea/stories/auto-size.stories.tsx
+++ packages/ui/textarea/stories/auto-size.stories.tsx
@@ -2,37 +2,29 @@ import React from 'react'
 import { TextArea } from '../src'
 
 /**
- * @title 基础用法
+ * @title 自动适应高度
  */
-export const Basic = () => {
+export const AutoSize = () => {
   const [value, setValue] = React.useState('')
+
   return (
     <>
-      <h1>Basic</h1>
-      <div className="textarea-basic__wrap">
+      <h1>AutoSize</h1>
+      <div className="textarea-auto-size__wrap">
         <TextArea
           value={value}
           onChange={(evt) => setValue(evt.target.value)}
           placeholder="请输入"
+          minRows={2}
           appearance="line"
         />
-        <br />
         <br />
         <TextArea
           value={value}
           onChange={(evt) => setValue(evt.target.value)}
           placeholder="请输入"
           maxRows={3}
-          appearance="filled"
-        />
-        <br />
-        <br />
-        <TextArea
-          value={value}
-          onChange={(evt) => setValue(evt.target.value)}
-          placeholder="请输入"
-          rows={3}
-          appearance="unset"
+          appearance="line"
         />
       </div>
     </>
--- packages/ui/textarea/stories/index.stories.tsx
+++ packages/ui/textarea/stories/index.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export * from './basic.stories'
+export * from './auto-size.stories'
+export * from './show-count.stories'
 
 export default {
   title: 'Data Input/Textarea',
