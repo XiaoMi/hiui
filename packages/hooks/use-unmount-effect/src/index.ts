509dc9660140e626f5946c434a809e07bf18c96d
Flcwl <flcwl@vip.qq.com>
2022-05-25 17:23:04 +0800

    chore: force update version

--- packages/hooks/use-unmount-effect/src/index.ts
+++ packages/hooks/use-unmount-effect/src/index.ts
@@ -5,13 +5,15 @@ import { useLatestCallback } from '@hi-ui/use-latest'
  * A hook for running when component unmount, will return unmountRef sign
  */
 export const useUnmountEffect = (cleanup?: () => void) => {
-  const unmountRef = useRef(false)
   const cleanupLatest = useLatestCallback(cleanup)
+  const unmountRef = useRef(false)
 
   useEffect(
-    () => () => {
-      unmountRef.current = true
-      cleanupLatest()
+    () => {
+      return () => {
+        unmountRef.current = true
+        cleanupLatest()
+      }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
