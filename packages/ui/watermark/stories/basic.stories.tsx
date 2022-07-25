2af2ae198c51168509f2230297aea3423317d3c0
Flcwl <flcwl@vip.qq.com>
2022-07-25 20:35:13 +0800

    chore(watermark): update stories

--- packages/ui/watermark/stories/basic.stories.tsx
+++ packages/ui/watermark/stories/basic.stories.tsx
@@ -12,10 +12,7 @@ export const Basic = () => {
         className="watermark-basic__wrap"
         style={{ height: 402, minWidth: 660, position: 'relative', zIndex: 0 }}
       >
-        <Watermark
-          content={['HiUI', '做中台，就用 HiUI']}
-          logo="https://cdn.cnbj1.fds.api.mi-img.com/hiui-template/resources/images/HiUI/logo.png"
-        ></Watermark>
+        <Watermark content={['HiUI', '做中台，就用 HiUI']}></Watermark>
       </div>
     </>
   )
