e3c0d06a568e03989b7f8e7aed1ccd515911954e
zhouyun1 <zhouyun1@xiaomi.com>
2022-09-07 19:40:05 +0800

    fix(table): 修复冻结列动态调整宽度问题

--- packages/ui/table/src/use-table.ts
+++ packages/ui/table/src/use-table.ts
@@ -250,6 +250,16 @@ export const useTable = ({
         nextColumns,
         scrollWidth ? scrollWidth / lastColumns.length : 100
       )
+
+      if (colWidths) {
+        // colWidths 记录的是最新的列宽，当它有值时，重置一下列宽，否则会导致冻结列动态调整宽度后定位不准
+        nextColumns = nextColumns.map((item, index) => {
+          return {
+            ...item,
+            width: colWidths[index],
+          }
+        })
+      }
     }
 
     let leftColumns = [] as any[]
