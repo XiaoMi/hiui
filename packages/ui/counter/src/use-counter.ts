321a88e63e23d3598d409cdf9fec551a0a8c9a31
zhouyun1 <zhouyun1@xiaomi.com>
2022-09-26 10:35:23 +0800

    fix(counter): 修复输入时光标定位到末尾问题

--- packages/ui/counter/src/use-counter.ts
+++ packages/ui/counter/src/use-counter.ts
@@ -254,7 +254,9 @@ export const useCounter = ({
       'aria-valuenow': value,
       'aria-valuemin': minProp,
       'aria-valuemax': maxProp,
-      value: inputValue as string,
+      // 如果 value 变量不是数字，去掉 inputValue 非数字字符，并且将 inputValue 绑定到 value 属性（此时 react input 组件会刷新，从而也就无法输入中文）
+      // 如果 value 变量是数字，则直接将 value 变量绑定到 value 属性，由于 onChange 里面改变的是该 value 的值，此时 react 内部不会刷新 input，可以解决修改任意位置的值时光标都会定位到末尾的问题
+      value: isNaN(value) ? String(inputValue).replace(/\D/g, '') : value,
       tabIndex,
       autoFocus: autoFocus,
       disabled: disabled,
