a7d47168b519cacfd7b34edf6ba239c5b0b92284
zhouyun1 <zhouyun1@xiaomi.com>
2023-02-07 15:22:59 +0800

    fix(locale-context): 优化获取文案时的空值判断

--- packages/ui/locale-context/src/LocaleContext.tsx
+++ packages/ui/locale-context/src/LocaleContext.tsx
@@ -3,13 +3,14 @@ import { LocaleLanguage, LocaleEnum } from './types'
 import defaultLocale from './locale/zh-CN'
 import { getNested } from '@hi-ui/object-utils'
 import { invariant } from '@hi-ui/env'
+import { isNullish } from '@hi-ui/type-assertion'
 
 // 1. 赋予动态能力，获取 国际化文案值
 // 2. 查找异常拦截，进行报错提示
 export const getLanguage = (languageData: LocaleLanguage) => (key: string, data?: any) => {
   let value: any = getNested(languageData, key.split('.'))
 
-  invariant(!!value, `The ${key} in language package is missing.`)
+  invariant(!isNullish(value), `The ${key} in language package is missing.`)
 
   if (data) {
     Object.keys(data).forEach((key) => {
