f7fa610d9c65aa48c05efab6fe955a55dbcde8df
zhouyun1 <zhouyun1@xiaomi.com>
2023-03-06 17:44:39 +0800

    chore(ellipsis-tooltip): 代码调整

--- packages/ui/ellipsis-tooltip/src/EllipsisTooltip.tsx
+++ packages/ui/ellipsis-tooltip/src/EllipsisTooltip.tsx
@@ -77,6 +77,10 @@ export const EllipsisTooltip: FC<EllipsisTooltipProps> = ({
 
 export interface EllipsisTooltipProps extends HiBaseHTMLProps<'div'> {
   /**
+   * 子文本，待处理文本内容
+   */
+   children: string
+  /**
    * 超出行数展示省略号
    */
   numberOfLines?: number
