351f7ae35224395fa7412d08710207f54ffab017
zhouyun1 <zhouyun1@xiaomi.com>
2023-04-18 14:42:51 +0800

    chore: 代码优化

--- scripts/calendar-data/index.js
+++ scripts/calendar-data/index.js
@@ -13,14 +13,16 @@ const getCalendarData = (year, month) => {
 
     axios({
       url: api,
-      responseType: 'arraybuffer', // 关键步骤
+      // 解决中文乱码
+      responseType: 'arraybuffer',
       responseEncoding: 'utf8',
     })
       .then((res) => {
         const { data } = res
-        const utf8decoder = new TextDecoder('GBK') // 关键步骤
-        const html = utf8decoder.decode(data)
-        resolve(JSON.parse(html))
+        // 解决中文乱码
+        const utf8decoder = new TextDecoder('GBK')
+        const dataString = utf8decoder.decode(data)
+        resolve(JSON.parse(dataString))
       })
       .catch((err) => {
         console.log('Error: ', err.message)
@@ -46,6 +48,7 @@ const getPRCData = async (year) => {
 
     res.forEach((item) => {
       const { almanac } = item.data[0]
+
       almanac.forEach((item) => {
         const { year, month, day, term, desc, status } = item
         const holiday = term || desc
@@ -72,7 +75,7 @@ const getPRCData = async (year) => {
 }
 
 /**
- * 生成json 文件
+ * 生成 json 文件
  * @param {*} yearList
  */
 const generateDataJson = async (yearList) => {
@@ -90,5 +93,5 @@ const generateDataJson = async (yearList) => {
   })
 }
 
-// 生成 2015-今年 的数据
+// 生成 2015-今年 共 length 个年头的数据
 generateDataJson(Array.from({ length: 9 }, (v, k) => k + 2015))
