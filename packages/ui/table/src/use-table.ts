bdd913d7173de64952437014ab9ffd1947ed506c
zhouyun1 <zhouyun1@xiaomi.com>
2023-07-27 15:40:49 +0800

    feat(table): #2556

--- packages/ui/table/src/use-table.ts
+++ packages/ui/table/src/use-table.ts
@@ -5,6 +5,7 @@ import {
   getLeafChildren,
   // getNodeAncestors,
   getNodeRootParent,
+  flattedTreeSort,
   // getNodeRootParent,
 } from '@hi-ui/tree-utils'
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
@@ -526,17 +527,21 @@ export const useTable = ({
   //* *************** 根据排序列处理数据 ************** *//
 
   const showData = useMemo(() => {
-    let _data = transitionData.concat()
+    let _data = cloneTree(transitionData.concat())
 
     if (activeSorterColumn) {
-      const sorter =
-        columns.filter((d) => d.dataKey === activeSorterColumn)[0] &&
-        columns.filter((d) => d.dataKey === activeSorterColumn)[0].sorter
+      const sorter = columns.filter((d) => d.dataKey === activeSorterColumn)[0]?.sorter
+
       if (sorter) {
         _data =
           activeSorterType === 'ascend'
             ? [..._data].sort(sorter)
             : [..._data].sort(sorter).reverse()
+
+        // 平铺的树形结构排序
+        if (_data.some((d: any) => d.depth !== 0)) {
+          _data = flattedTreeSort(_data)
+        }
       }
     }
 
