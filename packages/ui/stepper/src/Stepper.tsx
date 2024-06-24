0ec094bfd5c4fafc5f3792d0c185c0634345156c
xiamiao <xiamiao@xiaomi.com>
2024-06-24 10:28:56 +0800

    feat(stepper): 支持配置字段别名(#2914)

--- packages/ui/stepper/src/Stepper.tsx
+++ packages/ui/stepper/src/Stepper.tsx
@@ -1,10 +1,11 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useMemo } from 'react'
 import { cx, getPrefixCls } from '@hi-ui/classname'
 import { __DEV__ } from '@hi-ui/env'
 import { HiBaseHTMLProps } from '@hi-ui/core'
 import { useUncontrolledState } from '@hi-ui/use-uncontrolled-state'
 import { StepperDataItem } from './types'
 import { StepperItem } from './StepperItem'
+import { transformData } from './utils'
 
 const _role = 'stepper'
 const _prefix = getPrefixCls('stepper')
@@ -21,6 +22,7 @@ export const Stepper = forwardRef<HTMLDivElement | null, StepperProps>(
       role = _role,
       className,
       data = NOOP_ARRAY,
+      fieldNames,
       current: currentProp,
       onChange,
       itemLayout = 'horizontal',
@@ -30,6 +32,7 @@ export const Stepper = forwardRef<HTMLDivElement | null, StepperProps>(
     },
     ref
   ) => {
+    data = useMemo((): StepperDataItem[] => transformData(data, fieldNames), [data, fieldNames])
     const [current, trySetCurrent] = useUncontrolledState(0, currentProp, onChange)
 
     const cls = cx(
@@ -67,6 +70,10 @@ export interface StepperProps extends HiBaseHTMLProps<'div'> {
    * 步骤条数据项
    */
   data: StepperDataItem[]
+  /**
+   * 设置data 中的每一项对应的key
+   */
+  fieldNames?: Record<string, string>
   /**
    * 当前步骤位置索引，从 1 开始计数
    */
---
+++ packages/ui/stepper/src/utils.ts
@@ -0,0 +1,22 @@
+import { StepperDataItem } from "./types";
+
+
+export const transformData = (data: StepperDataItem[], fieldNames?: Record<string, string>): StepperDataItem[] => {
+
+  const getKeyFields = (node: any, key: any) => {
+    if(fieldNames){
+      return node[(fieldNames as any)[key] || key]
+    }
+    return node[key]
+  }
+
+  const traverseNode = (node : StepperDataItem) => {
+    const newNode = { ...node }
+    newNode.title = getKeyFields(newNode, 'title')
+    newNode.content = getKeyFields(newNode, 'content')
+    newNode.icon = getKeyFields(newNode, 'icon')
+    return newNode
+  }
+
+  return data.map((node) => traverseNode(node))
+}
