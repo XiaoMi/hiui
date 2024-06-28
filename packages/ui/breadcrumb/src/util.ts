30ce64493789e61afaf29ec56c8100966e4b2aea
xiamiao1121 <74885998+xiamiao1121@users.noreply.github.com>
2024-06-28 15:36:07 +0800

    feat(breadcrumb): 支持配置字段别名(#2885)
    * feat(search): 支持配置字段别名(#2885)
    
    * chore: 生成变更记录文件
    
    * feat(breadcrumb): 修改fieldNames类型
    
    * feat(breadcrumb): 代码规范
    
    ---------
    
    Co-authored-by: xiamiao <xiamiao@xiaomi.com>

---
+++ packages/ui/breadcrumb/src/util.ts
@@ -0,0 +1,34 @@
+import { HiBaseFieldNameKeys, HiBaseFieldNames } from '@hi-ui/core'
+import { BreadcrumbDataItem, BreadcrumbDataItemTargetEnum } from './types'
+import React from 'react'
+
+export const transformData = (
+  data: BreadcrumbDataItem[],
+  fieldNames?: HiBaseFieldNames
+): BreadcrumbDataItem[] => {
+  /**
+   * 转换对象
+   */
+  const getKeyFields = (node: BreadcrumbDataItem, key: HiBaseFieldNameKeys) => {
+    if (fieldNames) {
+      return node[(fieldNames[key] || key) as keyof BreadcrumbDataItem]
+    }
+    return node[key as keyof BreadcrumbDataItem]
+  }
+
+  const traverseNode = (node: BreadcrumbDataItem): BreadcrumbDataItem => {
+    const newNode: BreadcrumbDataItem = { ...node }
+
+    newNode.title = getKeyFields(newNode, 'title')
+    newNode.href = getKeyFields(newNode, 'href' as HiBaseFieldNameKeys) as string
+    newNode.icon = getKeyFields(newNode, 'icon' as HiBaseFieldNameKeys) as React.ReactNode
+    newNode.target = getKeyFields(
+      newNode,
+      'target' as HiBaseFieldNameKeys
+    ) as BreadcrumbDataItemTargetEnum
+
+    return newNode
+  }
+
+  return data.map(traverseNode)
+}
