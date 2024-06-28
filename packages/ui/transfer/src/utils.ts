8b574297fde7aa7184cff1a24a4bb73d11714c60
xiamiao1121 <74885998+xiamiao1121@users.noreply.github.com>
2024-06-28 15:42:17 +0800

    feat(transfer): 支持配置字段别名(#2909)
    * feat(transfer): 支持配置字段别名(#2909)
    
    * chore: 生成变更记录文件
    
    * feat(transfer): 代码规范
    
    ---------
    
    Co-authored-by: xiamiao <xiamiao@xiaomi.com>

---
+++ packages/ui/transfer/src/utils.ts
@@ -0,0 +1,27 @@
+import { HiBaseFieldNameKeys, HiBaseFieldNames } from '@hi-ui/core'
+import { TransferDataItem } from './types'
+import React from 'react'
+
+export const transformData = (
+  data: TransferDataItem[],
+  fieldNames?: HiBaseFieldNames
+): TransferDataItem[] => {
+  const getKeyFields = (node: TransferDataItem, key: HiBaseFieldNameKeys) => {
+    if (fieldNames) {
+      return node[(fieldNames[key] || key) as keyof TransferDataItem]
+    }
+    return node[key as keyof TransferDataItem]
+  }
+
+  const traverseNode = (node: TransferDataItem): TransferDataItem => {
+    const newNode = { ...node }
+
+    newNode.id = getKeyFields(newNode, 'id') as React.ReactText
+    newNode.title = getKeyFields(newNode, 'title') as React.ReactNode
+    newNode.disabled = (getKeyFields(newNode, 'disabled') ?? false) as boolean
+
+    return newNode
+  }
+
+  return data.map(traverseNode) as TransferDataItem[]
+}
