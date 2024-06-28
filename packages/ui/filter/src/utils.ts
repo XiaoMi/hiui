9b2dc02aeaf3979d01145e3d5a279a03d75b4223
xiamiao1121 <74885998+xiamiao1121@users.noreply.github.com>
2024-06-28 15:41:34 +0800

    feat(filter): 支持配置字段别名(#2915)
    * feat(filter): 支持配置字段别名(#2915)
    
    * chore: 生成变更记录文件
    
    * feat(filter): 修改fieldNames类型
    
    * feat(filter): 增加类型约束
    
    * feat(filter): 代码规范
    
    ---------
    
    Co-authored-by: xiamiao <xiamiao@xiaomi.com>

---
+++ packages/ui/filter/src/utils.ts
@@ -0,0 +1,32 @@
+import { HiBaseFieldNameKeys, HiBaseFieldNames } from '@hi-ui/core'
+import { FilterDataItem } from './types'
+import React from 'react'
+
+export const transformTreeData = (
+  data: FilterDataItem[],
+  fieldNames?: HiBaseFieldNames
+): FilterDataItem[] => {
+  const getKeyFields = (node: FilterDataItem, key: HiBaseFieldNameKeys) => {
+    if (fieldNames) {
+      return node[(fieldNames[key] || key) as keyof FilterDataItem]
+    }
+    return node[key]
+  }
+
+  const traverseNode = (node: FilterDataItem): FilterDataItem => {
+    const newNode: FilterDataItem = { ...node }
+
+    newNode.id = getKeyFields(newNode, 'id') as React.ReactText
+    newNode.title = getKeyFields(newNode, 'title') as React.ReactText
+    newNode.disabled = (getKeyFields(newNode, 'disabled') ?? false) as boolean
+    newNode.children = getKeyFields(newNode, 'children') as FilterDataItem[]
+
+    if (newNode.children) {
+      newNode.children = newNode.children.map(traverseNode)
+    }
+
+    return newNode
+  }
+
+  return data.map(traverseNode)
+}
