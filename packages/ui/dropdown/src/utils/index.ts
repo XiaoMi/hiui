20be5f387c1b9198d11b1db97c7da48f3f19052d
xiamiao1121 <74885998+xiamiao1121@users.noreply.github.com>
2024-06-28 15:38:51 +0800

    feat(dropdown): 支持配置字段别名(#2883)
    * feat(dropdown): 新增支持配置字段别名(#2883)
    
    * chore: 生成变更记录文件
    
    * chore: 删除无用代码
    
    * feat(dropdown): 修改fieldNames类型
    
    * feat(dropdown): 代码规范
    
    * feat(dropdown): 去掉条件判断
    
    ---------
    
    Co-authored-by: xiamiao <xiamiao@xiaomi.com>

--- packages/ui/dropdown/src/utils/index.ts
+++ packages/ui/dropdown/src/utils/index.ts
@@ -1,6 +1,7 @@
 import { isArray } from '@hi-ui/type-assertion'
-import { DropdownTriggerActionEnum } from '../types'
-
+import { DropdownTriggerActionEnum, DropdownDataItem } from '../types'
+import { HiBaseFieldNameKeys, HiBaseFieldNames } from '@hi-ui/core'
+import React from 'react'
 /**
  * 抹平 trigger 结构为数组
  *
@@ -10,3 +11,36 @@ import { DropdownTriggerActionEnum } from '../types'
 export const normalizeTrigger = (
   trigger: DropdownTriggerActionEnum | DropdownTriggerActionEnum[]
 ) => (isArray(trigger) ? Array.from(new Set(trigger)) : [trigger])
+
+export const transformData = (
+  data: DropdownDataItem[],
+  fieldNames?: HiBaseFieldNames
+): DropdownDataItem[] => {
+  /**
+   * 转换对象
+   */
+  const getKeyFields = (node: DropdownDataItem, key: HiBaseFieldNameKeys) => {
+    if (fieldNames) {
+      return node[(fieldNames[key] || key) as keyof DropdownDataItem]
+    }
+    return node[key as keyof DropdownDataItem]
+  }
+
+  const traverseNode = (node: DropdownDataItem): DropdownDataItem => {
+    const newNode: DropdownDataItem = { ...node }
+    newNode.id = getKeyFields(newNode, 'id') as React.ReactText
+    newNode.title = getKeyFields(newNode, 'title')
+    newNode.href = getKeyFields(newNode, 'href' as HiBaseFieldNameKeys) as string
+    newNode.disabled = (getKeyFields(newNode, 'disabled') ?? false) as boolean
+    newNode.split = (getKeyFields(newNode, 'split' as HiBaseFieldNameKeys) ?? false) as boolean
+    newNode.target = getKeyFields(newNode, 'target' as HiBaseFieldNameKeys) as
+      | '_self'
+      | '_blank'
+      | '_parent'
+      | '_top'
+
+    return newNode
+  }
+
+  return data.map(traverseNode)
+}
