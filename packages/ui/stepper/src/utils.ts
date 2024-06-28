e33eb530bf0f5b1036e8ad8025d74a1be70c772a
xiamiao1121 <74885998+xiamiao1121@users.noreply.github.com>
2024-06-28 15:41:58 +0800

    feat(stepper): 支持配置字段别名(#2914)
    * feat(stepper): 支持配置字段别名(#2914)
    
    * chore: 生成变更记录文件
    
    * feat(stepper): 修改fieldNames类型
    
    * feat(stepper): 代码规范
    
    ---------
    
    Co-authored-by: xiamiao <xiamiao@xiaomi.com>

---
+++ packages/ui/stepper/src/utils.ts
@@ -0,0 +1,27 @@
+import { HiBaseFieldNameKeys, HiBaseFieldNames } from '@hi-ui/core'
+import { StepperDataItem } from './types'
+import React from 'react'
+
+export const transformData = (
+  data: StepperDataItem[],
+  fieldNames?: HiBaseFieldNames
+): StepperDataItem[] => {
+  const getKeyFields = (node: StepperDataItem, key: HiBaseFieldNameKeys) => {
+    if (fieldNames) {
+      return node[(fieldNames[key] || key) as keyof StepperDataItem]
+    }
+    return node[key as keyof StepperDataItem]
+  }
+
+  const traverseNode = (node: StepperDataItem) => {
+    const newNode: StepperDataItem = { ...node }
+
+    newNode.title = getKeyFields(newNode, 'title') as React.ReactNode
+    newNode.content = getKeyFields(newNode, 'content' as HiBaseFieldNameKeys) as React.ReactNode
+    newNode.icon = getKeyFields(newNode, 'icon' as HiBaseFieldNameKeys) as React.ReactNode
+
+    return newNode
+  }
+
+  return data.map((node) => traverseNode(node))
+}
