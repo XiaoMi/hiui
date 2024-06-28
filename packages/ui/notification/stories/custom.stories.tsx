a6d72b5576ba784d3d74295759d6f7f4b0428109
xiamiao1121 <74885998+xiamiao1121@users.noreply.github.com>
2024-06-28 15:31:01 +0800

    feat(notification): 支持自定义 notification 配置 (#2832)
    * feat(notification): 支持指定portal挂载(#2831)
    
    * chore(notification): 生成变更记录文件
    
    * chore: 修改container类型
    
    * feat(notification): 重写自定义portal和zIndex功能(#2831)
    
    * feat(notification): 重写自定义属性示例(#2831)
    
    * feat(notification): 删除toast修改部分
    
    ---------
    
    Co-authored-by: xiamiao <xiamiao@xiaomi.com>

---
+++ packages/ui/notification/stories/custom.stories.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useMemo } from 'react'
+import { createNotification } from '../src'
+import Button from '@hi-ui/button'
+
+/**
+ * @title notification 属性自定义
+ */
+export const Custom = () => {
+  const [container, setContainer] = useState<HTMLElement | undefined>()
+
+  const notification = useMemo(
+    () =>
+      createNotification({
+        container: container,
+        zIndex: 2000,
+      }),
+    [container]
+  )
+
+  return (
+    <>
+      <h1>Custom</h1>
+      <div
+        ref={(e) => {
+          setContainer(e)
+        }}
+        id="ddd"
+        style={{
+          width: 400,
+          height: 300,
+          background: 'rgb(245, 247, 250)',
+
+          // Need add it
+          position: 'relative',
+          overflow: 'hidden',
+        }}
+      ></div>
+
+      <div className="notification-custom__wrap">
+        <Button
+          onClick={() => {
+            notification.open({
+              title: '数据备份通知',
+              content:
+                '各位同学请注意，将于2019.08.10 00:00:00 -08:00：00 期间进行系统服务器升级维护，请做好数据备份工作，以防丢失。带来不便，敬请谅解！',
+            })
+          }}
+        >
+          Notice
+        </Button>
+      </div>
+    </>
+  )
+}
