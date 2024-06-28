bfba6f982d47c09d2a19583213e712f9165f0e91
xiamiao1121 <74885998+xiamiao1121@users.noreply.github.com>
2024-06-28 15:36:45 +0800

    feat(search): 支持配置字段别名(#2886)
    * feat(search): 新增支持配置字段别名(#2886)
    
    * chore: 生成变更记录文件
    
    * feat(search): 修改fieldNames类型
    
    * feat(search): 代码规范
    
    * feat(search): 去掉条件判断
    
    ---------
    
    Co-authored-by: xiamiao <xiamiao@xiaomi.com>

---
+++ packages/ui/search/src/util.ts
@@ -0,0 +1,31 @@
+import { HiBaseFieldNameKeys, HiBaseFieldNames } from '@hi-ui/core'
+import { SearchDataItem } from './types'
+import React from 'react'
+
+export const transformData = (
+  data: SearchDataItem[],
+  fieldNames?: HiBaseFieldNames
+): SearchDataItem[] => {
+  /**
+   * 转换对象
+   */
+  const getKeyFields = (node: SearchDataItem, key: HiBaseFieldNameKeys) => {
+    if (fieldNames) {
+      return node[(fieldNames[key] || key) as keyof SearchDataItem]
+    }
+    return node[key as keyof SearchDataItem]
+  }
+
+  const traverseTreeNode = (node: SearchDataItem): SearchDataItem => {
+    const newNode: SearchDataItem = { ...node }
+
+    newNode.id = getKeyFields(newNode, 'id') as React.ReactText
+    newNode.title = getKeyFields(newNode, 'title') as string
+    newNode.children = getKeyFields(newNode, 'children') as SearchDataItem[]
+    if (newNode.children) newNode.children = newNode.children.map(traverseTreeNode)
+
+    return newNode
+  }
+
+  return data.map(traverseTreeNode)
+}
