03d632998c8abbbd079081eb897799a09ccd3dfd
xiamiao <xiamiao@xiaomi.com>
2024-08-28 16:26:00 +0800

    chore: test

--- hi-test/playwright.config.ts
+++ hi-test/playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd ../ && yarn storybook',
+    command: 'yarn prestart',
     port: 6006,
     // url: 'http://127.0.0.1:3000',
     // reuseExistingServer: !process.env.CI,
