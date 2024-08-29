2a622126ca282871234fa86df55f27e797e710b5
xiamiao <xiamiao@xiaomi.com>
2024-08-29 16:22:32 +0800

    chore: test

--- hi-test/playwright.config.ts
+++ hi-test/playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 300000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
