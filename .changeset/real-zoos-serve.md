2ffa2809ca0e0a7c7d7fe07f44f0c715b54c3c0d
zhouyun1 <zhouyun1@xiaomi.com>
2025-02-17 20:22:15 +0800

    feat(date-picker): 完善 footerRender 功能 (3088)

---
+++ .changeset/real-zoos-serve.md
@@ -0,0 +1,6 @@
+---
+"@hi-ui/date-picker": minor
+"@hi-ui/hiui": minor
+---
+
+feat(date-picker): 完善 footerRender 功能 (3088)
--- packages/ui/date-picker/stories/footer-render.stories.tsx
+++ packages/ui/date-picker/stories/footer-render.stories.tsx
@@ -10,6 +10,80 @@ export const FooterRender = () => {
     <>
       <h1>FooterRender</h1>
       <div className="date-picker-footer-render__wrap">
+        <h2>日期</h2>
+        <DatePicker
+          style={{ width: 240 }}
+          onChange={(date, dateStr) => {
+            console.log('onChange', date, dateStr)
+          }}
+          onSelect={console.log}
+          footerRender={(action, date) => {
+            console.log(action, date)
+
+            return (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: '100%',
+                }}
+              >
+                <Button type="secondary" appearance="link">
+                  自定义操作
+                </Button>
+              </div>
+            )
+          }}
+        />
+        <h2>日期时间</h2>
+        <DatePicker
+          style={{ width: 240 }}
+          onChange={(date, dateStr) => {
+            console.log('onChange', date, dateStr)
+          }}
+          showTime
+          onSelect={console.log}
+          footerRender={(sureActionContent) => (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+              }}
+            >
+              <Button type="secondary" appearance="link">
+                自定义操作
+              </Button>
+              {sureActionContent}
+            </div>
+          )}
+        />
+        <h2>日期范围</h2>
+        <DatePicker
+          type="daterange"
+          style={{ width: 240 }}
+          onChange={(date, dateStr) => {
+            console.log('onChange', date, dateStr)
+          }}
+          onSelect={console.log}
+          footerRender={() => (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+              }}
+            >
+              <Button type="secondary" appearance="link">
+                自定义操作
+              </Button>
+            </div>
+          )}
+        />
+        <h2>日期时间范围</h2>
         <DatePicker
           style={{ width: 240 }}
           type="daterange"
