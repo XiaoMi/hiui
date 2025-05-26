961af603b01d6f7c5deca5465c8bc87a9345bf58
zhouyun <zhouyun1@xiaomi.com>
2025-05-26 11:12:47 +0800

    style(upload): 新版 Upload (5.0) (#3169)
    * feat(upload): 新版 Upload (5.0)
    
    * feat(upload): 新增上传成功后显示成功icon (5.0)

--- packages/ui/upload/stories/picture-card.stories.tsx
+++ packages/ui/upload/stories/picture-card.stories.tsx
@@ -11,7 +11,8 @@ export const PictureCard = () => {
       <div className="upload-picture-list__wrap">
         <Upload
           type="pictureCard"
-          uploadAction="http://www.mocky.io/v2/5dc3b4413000007600347501"
+          // uploadAction="http://www.mocky.io/v2/5dc3b4413000007600347501"
+          uploadAction="https://ae97d8f3-cdda-48e5-98a6-40ffe37a94bc.mock.pstmn.io/upload"
           headers={{ name: 'mi' }}
           onChange={(file, fileList, response) => {
             console.log('upload callback', file, fileList, response)
