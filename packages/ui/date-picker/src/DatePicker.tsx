0c59234393a928274662608a29d3d449aa8ba4c9
zhouyun <zhouyun1@xiaomi.com>
2025-06-12 19:59:18 +0800

    style: 修改样式问题 (5.0) (#3191)
    * style: 修改样式问题 (5.0)
    
    * style: 修改样式问题 (5.0)

--- packages/ui/date-picker/src/DatePicker.tsx
+++ packages/ui/date-picker/src/DatePicker.tsx
@@ -304,6 +304,7 @@ export const DatePicker = forwardRef<HTMLDivElement | null, DatePickerProps>(
           setShowPanel(isShowPanel)
         }, 0)
         if (!isShowPanel) {
+          setInputFocus(false)
           callback(dates)
           onClose?.()
         }
@@ -477,7 +478,9 @@ export const DatePicker = forwardRef<HTMLDivElement | null, DatePickerProps>(
             }
           }}
           onMouseLeave={() => {
-            setInputFocus(false)
+            if (!showPanel) {
+              setInputFocus(false)
+            }
           }}
         >
           <Root
@@ -487,6 +490,7 @@ export const DatePicker = forwardRef<HTMLDivElement | null, DatePickerProps>(
             onTrigger={(index) => {
               setDateRangeTimePanelNow(index)
               setShowPanel(true)
+              setInputFocus(true)
             }}
             setAttachEl={setAttachEl}
             dateRangeTimePanelNow={dateRangeTimePanelNow}
