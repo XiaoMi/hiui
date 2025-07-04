779ac8f2ce6a99cd09e114d308b8f9b9d70b073b
zhouyun <zhouyun1@xiaomi.com>
2025-07-04 18:06:21 +0800

    style(core-css): 更新灰色调色板的颜色值和 shadow 值 (5.0) (#3213)
    * style(core-css): 更新灰色调色板的颜色值 (5.0)
    
    * style: 下拉框间距改为 4px (5.0)
    
    * style: 统一调整选择类组件 hover 时的背景色 (5.0)
    
    * style: 统一调整组件 shadow (5.0)

--- packages/ui/picker/src/Picker.tsx
+++ packages/ui/picker/src/Picker.tsx
@@ -181,7 +181,7 @@ export const Picker = forwardRef<HTMLDivElement | null, PickerProps>(
           ref={popperRef}
           matchWidth={optionWidth === undefined}
           matchWidthStrictly
-          gutterGap={2}
+          gutterGap={4}
           // @DesignToken zIndex: overlay
           zIndex={1050}
           {...overlay}
