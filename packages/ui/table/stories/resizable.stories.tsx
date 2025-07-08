f6e711ab499af7bdad3b7a647e0901610f8304b5
zhouyun1 <zhouyun1@xiaomi.com>
2025-07-08 11:34:57 +0800

    feat(table): TableColumnItem 类型增加 minWidth 参数 (#3216)

--- packages/ui/table/stories/resizable.stories.tsx
+++ packages/ui/table/stories/resizable.stories.tsx
@@ -11,8 +11,9 @@ export const Resizable = () => {
       <h1>Resizable for Table</h1>
       <div className="table-resizable__wrap" style={{ minWidth: 660 }}>
         <Table
-          fixedToColumn={{ left: 'type', right: 'address' }}
+          fixedToColumn={{ right: 'stock' }}
           resizable
+          tableWidthAdjustOnResize
           onResizeStop={(e, data, index, columnsWidth) => {
             console.log('onResizeStop', e, data, index, columnsWidth)
           }}
@@ -26,9 +27,11 @@ export const Resizable = () => {
               },
             },
             {
-              title: '品类',
+              title: <EllipsisTooltip>这是个很长的标题</EllipsisTooltip>,
               dataKey: 'type',
               width: 80,
+              // 注：当 title 长度过长时，可设置 minWidth 来保证列头最小宽度
+              minWidth: 60,
             },
             {
               title: '规格',
@@ -168,7 +171,8 @@ export const Resizable = () => {
             {
               title: '库存',
               dataKey: 'stock',
-              width: 150,
+              width: 100,
+              fixed: true,
             },
           ]}
           data={[
