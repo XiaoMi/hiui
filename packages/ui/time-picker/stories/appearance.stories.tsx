41b3abbc6134a2dcb5412ea1be34e5e4260241ce
zhouyun <zhouyun1@xiaomi.com>
2025-07-10 10:53:47 +0800

    feat(time-picker): appearance 增加 contained 类型，以及最新风格的样式修改 (5.0) (#3217)

--- packages/ui/time-picker/stories/appearance.stories.tsx
+++ packages/ui/time-picker/stories/appearance.stories.tsx
@@ -11,11 +11,13 @@ export const Appearance = () => {
       <h1>Appearance</h1>
       <div className="time-picker-appearance__wrap">
         <h2>line</h2>
-        <TimePicker placeholder={['请选择时间']} appearance={'line'} />
-        <h2>solid</h2>
-        <TimePicker placeholder={['请选择时间']} appearance={'filled'} />
+        <TimePicker style={{ width: '240px' }} placeholder={['请选择时间']} appearance={'line'} />
+        <h2>filled</h2>
+        <TimePicker style={{ width: '240px' }} placeholder={['请选择时间']} appearance={'filled'} />
         <h2>unset</h2>
         <TimePicker placeholder={['请选择时间']} appearance={'unset'} />
+        <h2>contained</h2>
+        <TimePicker label="时间" appearance={'contained'} onChange={console.log} />
       </div>
     </>
   )
