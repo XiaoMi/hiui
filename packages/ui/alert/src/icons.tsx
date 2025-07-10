adad3f776dc0169e49abb062b30a4b3bd0e04f94
zhouyun <zhouyun1@xiaomi.com>
2025-07-10 15:08:45 +0800

    style(alert): 新版样式调整 (5.0) (#3220)

--- packages/ui/alert/src/icons.tsx
+++ packages/ui/alert/src/icons.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 
-import {
-  InfoCircleFilled,
-  CloseCircleFilled,
-  CheckCircleFilled,
-  ExclamationCircleFilled,
-  CloseOutlined,
-} from '@hi-ui/icons'
+import { InfoCircleFilled, CloseCircleFilled, CheckCircleFilled, CloseOutlined } from '@hi-ui/icons'
 import { AlertTypeEnum } from './types'
 
 export const alertIconMap = {
   [AlertTypeEnum.SUCCESS]: <CheckCircleFilled />,
   [AlertTypeEnum.DANGER]: <CloseCircleFilled />,
-  [AlertTypeEnum.WARNING]: <ExclamationCircleFilled />,
+  [AlertTypeEnum.WARNING]: <InfoCircleFilled />,
   [AlertTypeEnum.PRIMARY]: <InfoCircleFilled />,
 }
 
