cc0a8292206fc9e6850d6109ac955d686d45819b
zhouyun <zhouyun1@xiaomi.com>
2025-07-14 21:25:57 +0800

    perf(cascader): 增加 virtual 参数，支持虚拟滚动 (5.0) (#3225)
    * feat(cascader): 支持虚拟滚动 (5.0)
    
    * fix(cascader): 为使用虚拟列表的子项设置 ref 传入 (5.0)
    
    * perf(cascader): 性能优化 (5.0)

--- packages/ui/check-cascader/src/CheckCascader.tsx
+++ packages/ui/check-cascader/src/CheckCascader.tsx
@@ -81,6 +81,7 @@ export const CheckCascader = forwardRef<HTMLDivElement | null, CheckCascaderProp
       fieldNames,
       label,
       showOnlyShowChecked,
+      virtual,
       ...rest
     },
     ref
@@ -372,6 +373,7 @@ export const CheckCascader = forwardRef<HTMLDivElement | null, CheckCascaderProp
             onChangeData={setCascaderData}
             checkedMode={checkedMode}
             dropdownColumnRender={dropdownColumnRender}
+            virtual={virtual}
           />
         ) : null}
       </Picker>
@@ -521,6 +523,10 @@ export interface CheckCascaderProps extends Omit<PickerProps, 'trigger' | 'scrol
    * 是否只展示选中的选项
    */
   showOnlyShowChecked?: boolean
+  /**
+   * 是否开启虚拟滚动
+   */
+  virtual?: boolean
 }
 
 if (__DEV__) {
