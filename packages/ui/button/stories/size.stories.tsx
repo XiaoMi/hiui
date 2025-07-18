1c6e5c7d9aeaf1ce2ea4360b67f485ff19ae659e
zhouyun <zhouyun1@xiaomi.com>
2025-07-18 15:32:12 +0800

    style: 修改 UI (5.0) (#3233)
    * style: 修改 UI (5.0)
    
    * style(popover): 修改样式 (5.0)

--- packages/ui/button/stories/size.stories.tsx
+++ packages/ui/button/stories/size.stories.tsx
@@ -6,10 +6,6 @@ import { PlusOutlined } from '@hi-ui/icons'
  * @title 不同尺寸
  */
 export const Size = () => {
-  const btnRef = React.useRef(null)
-  React.useEffect(() => {
-    console.log(btnRef)
-  }, [])
   return (
     <>
       <h1>不同尺寸</h1>
@@ -30,6 +26,7 @@ export const Size = () => {
           </Button>
           <Button type="primary" icon={<PlusOutlined />} size="lg" />
         </div>
+
         <div style={{ marginBottom: 24, display: 'flex', alignItems: 'center' }}>
           <Button type="primary" appearance="line" size="xs">
             超小号按钮
@@ -48,7 +45,8 @@ export const Size = () => {
           </Button>
           <Button type="primary" appearance="line" icon={<PlusOutlined />} size="lg" />
         </div>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+
+        <div style={{ marginBottom: 24, display: 'flex', alignItems: 'center' }}>
           <Button type="primary" appearance="link" size="xs">
             超小号按钮
           </Button>
@@ -65,6 +63,25 @@ export const Size = () => {
             大号按钮
           </Button>
           <Button type="primary" appearance="link" icon={<PlusOutlined />} size="lg" />
+        </div>
+
+        <div style={{ marginBottom: 24, display: 'flex' }}>
+          <Button type="primary" appearance="text" size="xs">
+            超小号按钮
+          </Button>
+          <Button type="primary" appearance="text" icon={<PlusOutlined />} size="xs" />
+          <Button type="primary" appearance="text" size="sm">
+            小号按钮
+          </Button>
+          <Button type="primary" appearance="text" icon={<PlusOutlined />} size="sm" />
+          <Button type="primary" appearance="text">
+            正常按钮
+          </Button>
+          <Button type="primary" appearance="text" icon={<PlusOutlined />} />
+          <Button type="primary" appearance="text" size="lg">
+            大号按钮
+          </Button>
+          <Button type="primary" appearance="text" icon={<PlusOutlined />} size="lg" />
         </div>
       </div>
     </>
