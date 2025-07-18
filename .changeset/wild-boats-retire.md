8ad0381b4804f5986b0b7060357757f23e40d3bb
zhouyun <zhouyun1@xiaomi.com>
2025-07-18 15:32:12 +0800

    style: 修改 UI (5.0) (#3233)
    * style: 修改 UI (5.0)
    
    * style(popover): 修改样式 (5.0)

---
+++ .changeset/wild-boats-retire.md
@@ -0,0 +1,13 @@
+---
+"@hi-ui/alert": patch
+"@hi-ui/button": patch
+"@hi-ui/collapse": patch
+"@hi-ui/hiui": patch
+"@hi-ui/popover": patch
+"@hi-ui/popper": patch
+"@hi-ui/tabs": patch
+"@hi-ui/time-picker": patch
+"@hi-ui/timeline": patch
+---
+
+style: 修改 UI (5.0)
--- packages/ui/time-picker/src/TimePicker.tsx
+++ packages/ui/time-picker/src/TimePicker.tsx
@@ -248,7 +248,7 @@ export const TimePicker = forwardRef<HTMLDivElement | null, TimePickerProps>(
           {type === 'single' && !isInSingleValueFormat && (
             <Button
               className={`${prefixCls}__pop-now-btn`}
-              appearance="link"
+              appearance="text"
               type="primary"
               size="sm"
               onClick={() => {
