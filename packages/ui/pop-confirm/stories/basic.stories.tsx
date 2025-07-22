7da56a1b9b47eb372123a4fd1e07207dc31391f2
zhouyun <zhouyun1@xiaomi.com>
2025-07-22 19:15:00 +0800

    style(pop-confirm): 新版样式修改 (5.0) (#3237)

--- packages/ui/pop-confirm/stories/basic.stories.tsx
+++ packages/ui/pop-confirm/stories/basic.stories.tsx
@@ -10,7 +10,7 @@ export const Basic = () => {
     <>
       <h1>Basic</h1>
       <div className="pop-confirm-basic__wrap">
-        <PopConfirm title="Delete this item along with the entered content?">
+        <PopConfirm style={{ minWidth: 280 }} title="Hello! Are you OK?">
           <Button>Trigger</Button>
         </PopConfirm>
       </div>
