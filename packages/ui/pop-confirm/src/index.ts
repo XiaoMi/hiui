704733bb9d711f25fc3ddaa4fecd7f897fd150e3
wangzhuojian <wangzhuojian@xiaomi.com>
2025-07-25 15:45:10 +0800

    feat(pop-confirm): PopConfirm组件支持以api方式调用

--- packages/ui/pop-confirm/src/index.ts
+++ packages/ui/pop-confirm/src/index.ts
@@ -1,4 +1,9 @@
 import './styles/index.scss'
 
+import { PopConfirm as PurePopConfirm } from './PopConfirm'
+import { withPopConfirm } from './with-api'
+
 export * from './PopConfirm'
-export { PopConfirm as default } from './PopConfirm'
+
+export const PopConfirm = withPopConfirm(PurePopConfirm)
+export default PopConfirm
