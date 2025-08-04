6d3e4d3073d5420c36616b40b97e03e5e053fb74
zhouyun <zhouyun1@xiaomi.com>
2025-08-04 16:25:47 +0800

    style(card): 新版样式调整 (5.0) (#3252)

--- packages/ui/card/stories/loading.stories.tsx
+++ packages/ui/card/stories/loading.stories.tsx
@@ -10,7 +10,15 @@ export const Loading = () => {
     <>
       <h1>Loading</h1>
       <div className="card-loading__wrap">
-        <Card title="标题" loading extra={<Button appearance="link">链接</Button>}>
+        <Card
+          title="标题"
+          loading
+          extra={
+            <Button type="primary" appearance="link">
+              链接
+            </Button>
+          }
+        >
           基础卡片
         </Card>
       </div>
