2e643a29c5900a397f21b8cfcca2ea3d3d1bd864
zhouyun1 <zhouyun1@xiaomi.com>
2025-08-05 16:35:56 +0800

    fix(check-tree-select): 优化数据处理逻辑

--- packages/ui/check-tree-select/src/CheckTreeSelect.tsx
+++ packages/ui/check-tree-select/src/CheckTreeSelect.tsx
@@ -208,10 +208,15 @@ export const CheckTreeSelect = forwardRef<HTMLDivElement | null, CheckTreeSelect
       ],
     })
 
+    const dataMemo = useMemo(() => {
+      const flattedStateData = flatData(stateInSearch.data)
+      return flattedStateData.length > 0 ? flattedStateData : flattedData
+    }, [flatData, stateInSearch.data, flattedData])
+
     const [value, tryChangeValue, onNodeCheck, checkedNodes, parsedCheckedIds] = useCheck(
       checkedMode,
       disabled,
-      flatData(stateInSearch.data) || flattedData,
+      dataMemo,
       defaultValue,
       valueProp,
       onChange
