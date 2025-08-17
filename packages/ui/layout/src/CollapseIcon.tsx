a1ad318d9f5c3e0683b924392c0449c0cff8fbd0
zhouyun <zhouyun1@xiaomi.com>
2025-08-17 16:46:09 +0800

    feat(menu&layout): 增加 GroupMenu SideMebu FloatMenuContainer 组件 (5.0) (#3282)
    * feat(menu): 增加 GroupMenu 组件 (5.0)
    
    * feat(menu): 增加 SideMenu 组件 (5.0)
    
    * feat(layout): 增加 FloatMenuContainer 组件 (5.0)

---
+++ packages/ui/layout/src/CollapseIcon.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const CollapseIcon = () => {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M10 3L13 3"
+        stroke="#60636B"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10 8L13 8"
+        stroke="#60636B"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3 13L13 13"
+        stroke="#60636B"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6 8L3 5.5L6 3"
+        stroke="#60636B"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
