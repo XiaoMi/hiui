ad1fa0b2417c8a26cb0ae1b0cef42ade0b6c2f22
zhouyun <zhouyun1@xiaomi.com>
2025-08-17 16:46:09 +0800

    feat(menu&layout): 增加 GroupMenu SideMebu FloatMenuContainer 组件 (5.0) (#3282)
    * feat(menu): 增加 GroupMenu 组件 (5.0)
    
    * feat(menu): 增加 SideMenu 组件 (5.0)
    
    * feat(layout): 增加 FloatMenuContainer 组件 (5.0)

--- packages/ui/menu/src/index.ts
+++ packages/ui/menu/src/index.ts
@@ -5,6 +5,8 @@ export { Menu as default } from './Menu'
 
 export * from './Sidebar'
 export * from './MenuSearch'
+export * from './GroupMenu'
+export * from './SideMenu'
 
 export { filterTreeData, getParentId, getAncestorIds } from './util'
 
