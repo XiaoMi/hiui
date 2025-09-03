ac8f4f237a74858d3ce2b260be9531bd6f61130b
zhouyun <zhouyun1@xiaomi.com>
2025-09-03 21:30:33 +0800

    perf(layout&menu): 优化导航菜单交互体验 (5.0) (#3310)
    * perf(layout): 优化导航菜单交互体验 & 修改分组菜单样式问题 (5.0)
    
    * perf(menu): 优化菜单搜索组件的样式和结构 (5.0)
    
    * fix(Sider): 修改 Sider 组件的宽度判断逻辑，确保宽度不小于最小值，并调整 with-menu.stories 中的内边距样式 (5.0)

--- packages/ui/layout/src/index.ts
+++ packages/ui/layout/src/index.ts
@@ -1,8 +1,21 @@
 import './styles/index.scss'
 
-export * from './Layout'
-export { Layout as default } from './Layout'
+import { Layout as _Layout } from './Layout'
+import { Sider } from './Sider'
+import { Content } from './Content'
+import { SearchTrigger } from './SearchTrigger'
+import { FloatMenuContainer } from './FloatMenuContainer'
 
+const Layout = Object.assign(_Layout, {
+  Sider,
+  Content,
+  SearchTrigger,
+  FloatMenuContainer,
+})
+
+export default Layout
+
+export * from './Layout'
 export * from './Sider'
 export * from './Content'
 export * from './SearchTrigger'
