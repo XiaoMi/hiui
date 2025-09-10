7d8cdac5780e832ba0259f1fef43071a761ab6c6
zhouyun <zhouyun1@xiaomi.com>
2025-09-10 11:13:00 +0800

    feat(check-cascader): 增加 checkbox 级联禁用功能和相关样式调整 (5.0) (#3318)
    * feat(check-cascader): 增加 checkbox 级联禁用功能和相关样式调整 (5.0)
    
    * docs(check-cascader): 添加禁用复选框级联的示例 (5.0)

--- packages/ui/check-cascader/src/CheckCascaderMenu.tsx
+++ packages/ui/check-cascader/src/CheckCascaderMenu.tsx
@@ -151,7 +151,7 @@ const MenuItem = forwardRef<
             className={`${prefixCls}-checkbox`}
             indeterminate={semiChecked}
             checked={checked}
-            disabled={disabled || option.disabledCheckbox}
+            disabled={disabled || option.disabledCheckbox || option.disabledCheckboxCascaded}
             onClick={(evt) => evt.stopPropagation()}
             // 当前是半选时，点击将设置为全选，
             // 又因子节点某个选项未选中但是禁用，当前态将一直是半选，那操作将一直是设置全选操作
