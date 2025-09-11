17546c079167ebacefdb5af9312d42f7d22e8114
zhouyun <zhouyun1@xiaomi.com>
2025-09-11 17:19:53 +0800

    feat(layout): 添加 AppListPopover 组件并更新布局样式和示例 (5.0) (#3319)
    * feat(layout): 添加 AppListPopover 组件并更新布局样式和示例 (5.0)
    
    * chore(layout): 修复 AppList 组件文档注释，简化 AppListPopover 组件的属性传递逻辑 (5.0)
    
    * chore(layout): 修改示例 (5.0)

--- packages/ui/layout/stories/with-menu.stories.tsx
+++ packages/ui/layout/stories/with-menu.stories.tsx
@@ -3,6 +3,8 @@ import Menu from '@hi-ui/menu'
 import Scrollbar from '@hi-ui/scrollbar'
 import { AppStoreFilled, UserFilled, SunFilled, PadFilled } from '@hi-ui/icons'
 import Layout, { Sider, Content, SearchTrigger } from '../src'
+import Button from '@hi-ui/button'
+import Avatar from '@hi-ui/avatar'
 
 /**
  * @title 带菜单的布局
@@ -63,15 +65,32 @@ export const WithMenu = () => {
   return (
     <>
       <h1>WithMenu</h1>
-      <div className="layout-with-menu__wrap" style={{ width: 800, height: 600 }}>
-        <Layout style={{ height: '100%' }}>
+      <div className="layout-with-menu__wrap" style={{ width: '100%', height: 600 }}>
+        <Layout style={{ height: '100%', backgroundColor: '#f5f8fc' }}>
           <Sider
             style={{ backgroundColor: '#edf2ff' }}
             collapsed={collapsed}
             onCollapse={setCollapsed}
           >
-            <div style={{ padding: '16px 14px' }}>
-              <div style={{ height: 32, backgroundColor: '#f2f4f7' }}></div>
+            <div
+              style={{
+                padding: '16px',
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: 8,
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: collapsed ? 'column' : 'row',
+              }}
+            >
+              <div
+                style={{
+                  width: collapsed ? 28 : `100%`,
+                  height: 28,
+                  borderRadius: 6,
+                  backgroundColor: 'rgba(124, 135, 166, 0.12)',
+                }}
+              ></div>
             </div>
             <SearchTrigger mini={collapsed} data={data} />
             <Scrollbar>
@@ -88,13 +107,41 @@ export const WithMenu = () => {
                 data={data}
               />
             </Scrollbar>
-            <div style={{ padding: '16px 14px' }}>
-              <div style={{ height: 32, backgroundColor: '#f2f4f7' }}></div>
+            <div
+              style={{
+                padding: '16px',
+                display: 'flex',
+                gap: 4,
+                alignItems: 'center',
+              }}
+            >
+              <Avatar size="xs" />
+              {collapsed ? null : <span>用户名</span>}
             </div>
           </Sider>
-          <Content style={{ backgroundColor: '#f7f9fc' }}>
-            <div style={{ height: 32, margin: '14px 0', backgroundColor: '#f2f4f7' }}></div>
-            <div style={{ flex: 1, marginBottom: 16, backgroundColor: '#f2f4f7' }}></div>
+          <Content>
+            <div
+              style={{
+                height: 60,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <div style={{ fontSize: 18, fontWeight: 500, color: '#161a2d' }}>标题</div>
+              <div style={{}}>
+                <Button type="primary">操作按钮</Button>
+              </div>
+            </div>
+            <div
+              style={{
+                flex: 1,
+                marginBottom: 16,
+                borderRadius: 12,
+                backgroundColor: '#fff',
+                boxShadow: '0 0 4px rgba(92, 94, 102, 0.06)',
+              }}
+            ></div>
           </Content>
         </Layout>
       </div>
