aa420178430926ca1057c30b96c22e3b562c737d
zhouyun1 <zhouyun1@xiaomi.com>
2025-09-11 16:44:02 +0800

    feat(layout): 添加 AppListPopover 组件并更新布局样式和示例 (5.0)

---
+++ packages/ui/layout/src/AppList.tsx
@@ -0,0 +1,232 @@
+import React, { forwardRef, useCallback } from 'react'
+import { cx, getPrefixCls } from '@hi-ui/classname'
+import { __DEV__ } from '@hi-ui/env'
+import { HiBaseHTMLProps } from '@hi-ui/core'
+import { useUncontrolledState } from '@hi-ui/use-uncontrolled-state'
+import { useUncontrolledToggle } from '@hi-ui/use-toggle'
+import Popover, { PopoverProps } from '@hi-ui/popover'
+import { CheckOutlined } from '@hi-ui/icons'
+
+const APP_LIST_PREFIX = getPrefixCls('app-list')
+
+/**
+ * 应用列表组件，用于展示应用列表
+ */
+export const AppList = forwardRef<HTMLDivElement | null, AppListProps>(
+  (
+    {
+      prefixCls = APP_LIST_PREFIX,
+      role = 'app-list',
+      className,
+      data,
+      activeId: activeIdProp,
+      defaultActiveId,
+      titleRender,
+      onItemClick,
+      ...rest
+    },
+    ref
+  ) => {
+    const cls = cx(prefixCls, className)
+
+    const [activeId, tryChangeActiveId] = useUncontrolledState<React.ReactText | undefined>(
+      defaultActiveId,
+      activeIdProp
+    )
+
+    const handleItemClick = useCallback(
+      (item: AppListItem, event: React.MouseEvent<HTMLDivElement>) => {
+        tryChangeActiveId(item.id)
+        onItemClick?.(item, event)
+
+        event.stopPropagation()
+      },
+      [tryChangeActiveId, onItemClick]
+    )
+
+    const renderIcon = useCallback((item: AppListItem) => {
+      const { icon, title } = item
+
+      if (!icon) {
+        if (typeof title === 'string') {
+          return title.slice(0, 1)
+        }
+        return null
+      }
+
+      return icon
+    }, [])
+
+    return (
+      <div ref={ref} role={role} className={cls} {...rest}>
+        <div className={`${prefixCls}-wrapper`}>
+          {data?.map((item) => {
+            const { id, title, iconBgColor } = item
+
+            return (
+              <div
+                className={cx(`${prefixCls}-item`, {
+                  [`${prefixCls}-item--active`]: activeId === id,
+                })}
+                key={id}
+                onClick={(event) => handleItemClick(item, event)}
+              >
+                <div
+                  className={cx(`${prefixCls}-item__icon`)}
+                  style={{
+                    backgroundColor: iconBgColor ? IconBgColorMap[iconBgColor] : undefined,
+                  }}
+                >
+                  {renderIcon(item)}
+                </div>
+                <div className={`${prefixCls}-item__title`}>{titleRender?.(item) || title}</div>
+                <div className={`${prefixCls}-item__checked`}>
+                  <CheckOutlined />
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+)
+
+export const IconBgColorMap = {
+  // 深蓝色、浅蓝色、青色、绿色、紫色、黄色、橙色
+  BLUE: '#2660ff',
+  SKYBLUE: '#0aadff',
+  CYAN: '#04c2ac',
+  GREEN: '#24b237',
+  PURPLE: '#7f3df2',
+  YELLOW: '#ffbe0a',
+  ORANGE: '#ff7700',
+} as const
+
+export interface AppListItem {
+  /**
+   * 应用图标
+   */
+  icon?: React.ReactNode
+  /**
+   * 应用图标背景颜色
+   */
+  iconBgColor?: keyof typeof IconBgColorMap
+  /**
+   * 应用 id
+   */
+  id: React.ReactText
+  /**
+   * 应用标题
+   */
+  title?: React.ReactNode
+}
+
+export interface AppListProps extends HiBaseHTMLProps<'div'> {
+  /**
+   * 应用列表数据
+   */
+  data?: AppListItem[]
+  /**
+   * 默认激活的应用 id
+   */
+  defaultActiveId?: React.ReactText
+  /**
+   * 激活的应用 id
+   */
+  activeId?: React.ReactText
+  /**
+   * 应用标题渲染
+   */
+  titleRender?: (item: AppListItem) => React.ReactNode
+  /**
+   * 应用列表点击回调
+   */
+  onItemClick?: (item: AppListItem, event: React.MouseEvent<HTMLDivElement>) => void
+}
+
+if (__DEV__) {
+  AppList.displayName = 'AppList'
+}
+
+export const AppListPopover = forwardRef<HTMLDivElement | null, AppListPopoverProps>(
+  (
+    {
+      onOpen,
+      onClose,
+      visible: visibleProp,
+      children,
+      data,
+      defaultActiveId,
+      activeId,
+      titleRender,
+      onItemClick,
+      ...restProps
+    },
+    ref
+  ) => {
+    const [visible, visibleAction] = useUncontrolledToggle({
+      defaultVisible: false,
+      visible: visibleProp,
+    })
+
+    const handleOpen = useCallback(() => {
+      visibleAction.on()
+      onOpen?.()
+    }, [onOpen, visibleAction])
+
+    const handleClose = useCallback(() => {
+      visibleAction.off()
+      onClose?.()
+    }, [onClose, visibleAction])
+
+    return (
+      <Popover
+        {...restProps}
+        style={{
+          paddingLeft: 12,
+          paddingRight: 12,
+          maxHeight: 382,
+          overflow: 'auto',
+        }}
+        visible={visible}
+        arrow={false}
+        placement="right-start"
+        gutterGap={4}
+        content={
+          <AppList
+            ref={ref}
+            data={data}
+            defaultActiveId={defaultActiveId}
+            activeId={activeId}
+            titleRender={titleRender}
+            onItemClick={onItemClick}
+          />
+        }
+        onOpen={handleOpen}
+        onClose={handleClose}
+      >
+        {children}
+      </Popover>
+    )
+  }
+)
+
+export interface AppListPopoverProps extends AppListProps, Omit<PopoverProps, 'content'> {
+  /**
+   * 应用列表是否可见
+   */
+  visible?: boolean
+  /**
+   * 应用列表打开回调
+   */
+  onOpen?: () => void
+  /**
+   * 应用列表关闭回调
+   */
+  onClose?: () => void
+}
+
+if (__DEV__) {
+  AppListPopover.displayName = 'AppListPopover'
+}
--- packages/ui/layout/stories/basic.stories.tsx
+++ packages/ui/layout/stories/basic.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout, { Sider, Content } from '../src'
+import Button from '@hi-ui/button'
 
 /**
  * @title 基础布局
@@ -12,15 +13,36 @@ export const Basic = () => {
     <>
       <h1>Basic</h1>
       <div className="layout-basic__wrap" style={{ width: 800, height: 600 }}>
-        <Layout style={{ height: '100%' }}>
+        <Layout style={{ height: '100%', backgroundColor: '#f5f8fc' }}>
           <Sider
-            style={{ backgroundColor: '#f2f4f7' }}
+            style={{ backgroundColor: '#edf2ff' }}
             collapsed={collapsed}
             onCollapse={setCollapsed}
           ></Sider>
-          <Content style={{ backgroundColor: '#f7f9fc' }}>
-            <div style={{ height: 32, margin: '14px 0', backgroundColor: '#f2f4f7' }}></div>
-            <div style={{ flex: 1, marginBottom: 16, backgroundColor: '#f2f4f7' }}></div>
+          <Content>
+            {/* <div
+              style={{
+                height: 60,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <div style={{ fontSize: 18, fontWeight: 500, color: '#161a2d' }}>标题</div>
+              <div style={{}}>
+                <Button type="primary">操作按钮</Button>
+              </div>
+            </div> */}
+            <div
+              style={{
+                flex: 1,
+                marginTop: 16,
+                marginBottom: 16,
+                borderRadius: 12,
+                backgroundColor: '#fff',
+                boxShadow: '0 0 4px rgba(92, 94, 102, 0.06)',
+              }}
+            ></div>
           </Content>
         </Layout>
       </div>
