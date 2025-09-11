6d280920f4582b74de6e87e263b91ce7ff192874
zhouyun <zhouyun1@xiaomi.com>
2025-09-11 17:19:53 +0800

    feat(layout): 添加 AppListPopover 组件并更新布局样式和示例 (5.0) (#3319)
    * feat(layout): 添加 AppListPopover 组件并更新布局样式和示例 (5.0)
    
    * chore(layout): 修复 AppList 组件文档注释，简化 AppListPopover 组件的属性传递逻辑 (5.0)
    
    * chore(layout): 修改示例 (5.0)

--- packages/ui/layout/stories/basic.stories.tsx
+++ packages/ui/layout/stories/basic.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout, { Sider, Content } from '../src'
+import Button from '@hi-ui/button'
 
 /**
  * @title 基础布局
@@ -12,15 +13,23 @@ export const Basic = () => {
     <>
       <h1>Basic</h1>
       <div className="layout-basic__wrap" style={{ width: 800, height: 600 }}>
-        <Layout style={{ height: '100%' }}>
+        <Layout style={{ height: '100%', backgroundColor: '#f5f8fc' }}>
           <Sider
-            style={{ backgroundColor: '#f2f4f7' }}
+            style={{ backgroundColor: '#edf2ff' }}
             collapsed={collapsed}
             onCollapse={setCollapsed}
           ></Sider>
-          <Content style={{ backgroundColor: '#f7f9fc' }}>
-            <div style={{ height: 32, margin: '14px 0', backgroundColor: '#f2f4f7' }}></div>
-            <div style={{ flex: 1, marginBottom: 16, backgroundColor: '#f2f4f7' }}></div>
+          <Content>
+            <div
+              style={{
+                flex: 1,
+                marginTop: 16,
+                marginBottom: 16,
+                borderRadius: 12,
+                backgroundColor: '#fff',
+                boxShadow: '0 0 4px rgba(92, 94, 102, 0.06)',
+              }}
+            ></div>
           </Content>
         </Layout>
       </div>
