3f6edab92346196e30f8915ad312eebcf9e15f38
zhouyun <zhouyun1@xiaomi.com>
2025-09-25 10:30:26 +0800

    feat(layout): 增加 Header 和 Footer 组件 & 完善示例 (5.0) (#3348)

---
+++ .changeset/shiny-mangos-sneeze.md
@@ -0,0 +1,6 @@
+---
+"@hi-ui/hiui": minor
+"@hi-ui/layout": minor
+---
+
+feat(layout): 增加 Header 和 Footer 组件 & 完善示例 (5.0)
--- packages/ui/layout/stories/basic.stories.tsx
+++ packages/ui/layout/stories/basic.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Layout, { Sider, Content } from '../src'
-import Button from '@hi-ui/button'
+import Layout, { Header, Sider, Content, Footer } from '../src'
 
 /**
  * @title 基础布局
@@ -12,26 +11,186 @@ export const Basic = () => {
   return (
     <>
       <h1>Basic</h1>
-      <div className="layout-basic__wrap" style={{ width: 800, height: 600 }}>
-        <Layout style={{ height: '100%', backgroundColor: '#f5f8fc' }}>
-          <Sider
-            style={{ backgroundColor: '#edf2ff' }}
-            collapsed={collapsed}
-            onCollapse={setCollapsed}
-          ></Sider>
-          <Content>
-            <div
-              style={{
-                flex: 1,
-                marginTop: 16,
-                marginBottom: 16,
-                borderRadius: 12,
-                backgroundColor: '#fff',
-                boxShadow: '0 0 4px rgba(92, 94, 102, 0.06)',
-              }}
-            ></div>
-          </Content>
-        </Layout>
+      <div className="layout-basic__wrap" style={{ width: 800 }}>
+        <div>
+          <div style={{ height: 300, marginBottom: 16 }}>
+            <Layout direction="column" style={{ height: '100%', backgroundColor: '#f5f8fc' }}>
+              <Header
+                style={{
+                  height: 60,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#74a2ff',
+                }}
+              >
+                <div>Header</div>
+              </Header>
+              <Content
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#2660ff',
+                }}
+              >
+                Content
+              </Content>
+              <Footer
+                style={{
+                  height: 60,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#74a2ff',
+                }}
+              >
+                Footer
+              </Footer>
+            </Layout>
+          </div>
+          <div style={{ height: 300, marginBottom: 16 }}>
+            <Layout direction="column" style={{ height: '100%', backgroundColor: '#f5f8fc' }}>
+              <Header
+                style={{
+                  height: 60,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#74a2ff',
+                }}
+              >
+                <div>Header</div>
+              </Header>
+              <Layout direction="row">
+                <Sider
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#4d82ff',
+                    boxShadow: 'none',
+                  }}
+                  resizable={false}
+                  collapsed={collapsed}
+                  onCollapse={setCollapsed}
+                >
+                  Sider
+                </Sider>
+                <Content
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#2660ff',
+                  }}
+                >
+                  Content
+                </Content>
+              </Layout>
+              <Footer
+                style={{
+                  height: 60,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#74a2ff',
+                }}
+              >
+                Footer
+              </Footer>
+            </Layout>
+          </div>
+          <div style={{ height: 300, marginBottom: 16 }}>
+            <Layout direction="column" style={{ height: '100%', backgroundColor: '#f5f8fc' }}>
+              <Header
+                style={{
+                  height: 60,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#74a2ff',
+                }}
+              >
+                <div>Header</div>
+              </Header>
+              <Layout direction="row">
+                <Content
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#2660ff',
+                  }}
+                >
+                  Content
+                </Content>
+                <Sider
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#4d82ff',
+                    boxShadow: 'none',
+                  }}
+                  resizable={false}
+                  collapsed={collapsed}
+                  onCollapse={setCollapsed}
+                >
+                  Sider
+                </Sider>
+              </Layout>
+              <Footer
+                style={{
+                  height: 60,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#74a2ff',
+                }}
+              >
+                Footer
+              </Footer>
+            </Layout>
+          </div>
+          <div style={{ height: 300, marginBottom: 16 }}>
+            <Layout style={{ height: '100%', backgroundColor: '#f5f8fc' }}>
+              <Sider
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#4d82ff',
+                  boxShadow: 'none',
+                }}
+                resizable={false}
+                collapsed={collapsed}
+                onCollapse={setCollapsed}
+              >
+                Sider
+              </Sider>
+              <Layout direction="column">
+                <Header
+                  style={{
+                    height: 60,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#74a2ff',
+                  }}
+                >
+                  <div>Header</div>
+                </Header>
+                <Content
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#2660ff',
+                  }}
+                >
+                  Content
+                </Content>
+                <Footer
+                  style={{
+                    height: 60,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#74a2ff',
+                  }}
+                >
+                  Footer
+                </Footer>
+              </Layout>
+            </Layout>
+          </div>
+        </div>
       </div>
     </>
   )
