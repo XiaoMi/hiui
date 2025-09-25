cc3fa1f50ea455997f493cde9726806c098f9949
zhouyun1 <zhouyun1@xiaomi.com>
2025-09-25 10:16:19 +0800

    feat(layout): 增加 Header 和 Footer 组件 & 完善示例 (5.0)

--- packages/ui/layout/stories/index.stories.tsx
+++ packages/ui/layout/stories/index.stories.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Layout from '../src'
 
 export * from './basic.stories'
-export * from './float-menu.stories'
 export * from './with-menu.stories'
+export * from './float-menu.stories'
+export * from './with-header.stories'
 
 export default {
   title: 'Layout/Layout',
