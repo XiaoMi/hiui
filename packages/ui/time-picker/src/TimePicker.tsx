61a68c8efb7abe8deccf90923c4e66fe4bb5be6d
zhouyun1 <zhouyun1@xiaomi.com>
2025-09-28 16:37:15 +0800

    fix(time-picker): 修改 notifyOutside 回调逻辑，修复清空要点击2次才生效问题 (5.0)

--- packages/ui/time-picker/src/TimePicker.tsx
+++ packages/ui/time-picker/src/TimePicker.tsx
@@ -90,7 +90,7 @@ export const TimePicker = forwardRef<HTMLDivElement | null, TimePickerProps>(
     const formatNotifyOutside = useCallback(
       (disposeValue: string[]) => {
         const result = disposeValue.filter((item) => item)
-        notifyOutside && notifyOutside(result.length > 1 ? result : result[0])
+        notifyOutside && notifyOutside(result.length > 1 ? result : result[0] || '')
       },
       [notifyOutside]
     )
