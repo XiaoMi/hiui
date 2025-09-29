1e52c492909b059bc716fdf4faf0833585e72009
兰之 <lizhan3@xiaomi.com>
2025-09-29 19:52:39 +0800

    feat: 新增react-compat/patch-for-react等工具包,用于解决不同版本React之间的兼容性问题 (#3351)
    * feat: 新增@hi-ui/react-compat工具包,用于兼容不同版本ReactDOM渲染函数的差异
    
    * feat: 新增@hi-ui/patch-for-react补丁包,用于兼容React19的破坏性变更
    
    * refactor(modal): 使用@hi-ui/react-compat工具包替换直接引用的react-dom依赖
    
    * refactor(toast): 使用@hi-ui/react-compat工具包替换直接引用的react-dom依赖

---
+++ packages/utils/react-compat/src/adapter.tsx
@@ -0,0 +1,80 @@
+import type React from 'react'
+import ReactDOM from 'react-dom'
+
+const MARK = '__hi_react_root__'
+
+type Root = {
+  render: (node: React.ReactElement) => void
+  unmount: () => void
+}
+
+type ContainerType = (Element | DocumentFragment) & {
+  [MARK]?: Root
+}
+
+type CreateRootFn = (container: ContainerType) => Root
+
+type MockReactDOM = typeof ReactDOM & {
+  createRoot?: CreateRootFn
+}
+
+// 引用原始 ReactDOM 对象，但不修改
+const cloneReactDOM = { ...ReactDOM } as MockReactDOM
+
+const { version, render: reactLegacyRender, unmountComponentAtNode } = cloneReactDOM
+
+let createRoot: CreateRootFn | undefined
+try {
+  const mainVersion = Number((version || '').split('.')[0])
+  if (mainVersion >= 18) {
+    // React 18+ 中 createRoot 在 react-dom/client 中
+    const ReactDOMClient = require('react-dom/client')
+    createRoot = ReactDOMClient.createRoot
+  }
+} catch (e) {
+  // Do nothing;
+}
+
+function modernRender(node: React.ReactElement, container: ContainerType) {
+  const root = container[MARK] || createRoot!(container)
+
+  root.render(node)
+
+  container[MARK] = root
+}
+
+function legacyRender(node: React.ReactElement, container: ContainerType) {
+  reactLegacyRender?.(node, container)
+}
+
+export function render(node: React.ReactElement, container: ContainerType) {
+  if (createRoot) {
+    modernRender(node, container)
+    return
+  }
+
+  legacyRender(node, container)
+}
+
+function modernUnmount(container: ContainerType) {
+  // 避免 React18 的 sync warning
+  return Promise.resolve().then(() => {
+    container[MARK]?.unmount()
+
+    delete container[MARK]
+  })
+}
+
+function legacyUnmount(container: ContainerType) {
+  unmountComponentAtNode(container)
+}
+
+export function unmount(container: ContainerType) {
+  if (createRoot !== undefined) {
+    // 避免 React18 的 sync warning
+    return modernUnmount(container)
+  }
+
+  legacyUnmount(container)
+  return Promise.resolve() // 保持接口签名一致
+}
