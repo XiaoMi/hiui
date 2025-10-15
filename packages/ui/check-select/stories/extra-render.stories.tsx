95053f280f5a05cb88579db1b91ecbeff48fbd95
wanjinping <wanjinping@xiaomi.com>
2025-10-15 14:09:15 +0800

    feat(check-select): 增加示例 (5.0)

---
+++ packages/ui/check-select/stories/extra-render.stories.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import CheckSelect from '../src'
+
+/**
+ * @title 自定义渲染头部和底部内容
+ */
+export const ExtraRender = () => {
+  const [data] = useState([
+    { title: '电视', id: '3', disabled: false },
+    { title: '手机', id: '2' },
+    { title: '笔记本', id: '4', disabled: false },
+    { title: '生活周边', id: '5' },
+    { title: '办公', id: '6' },
+  ])
+
+  return (
+    <>
+      <h1>ExtraRender</h1>
+      <div className="check-select-extra-render">
+        <CheckSelect
+          style={{ width: 240 }}
+          clearable={false}
+          data={data}
+          renderExtraHeader={() => 'customer header'}
+          renderExtraFooter={() => 'customer footer'}
+        />
+      </div>
+    </>
+  )
+}
