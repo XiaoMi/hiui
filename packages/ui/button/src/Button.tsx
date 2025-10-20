4006b2c8cabba5ec553b69a17e4bf3fb53fe9180
zhouyun <zhouyun1@xiaomi.com>
2025-10-20 14:42:44 +0800

    feat(loading): 新版 loading 样式 (5.0) (#3373)
    * feat(loading): 默认效果改为 spin 类型 & 增加 contentPosition、size 和 color 参数 (5.0)
    
    * style(button): loading 改为使用 Spinner 组件 (5.0)

--- packages/ui/button/src/Button.tsx
+++ packages/ui/button/src/Button.tsx
@@ -2,6 +2,7 @@ import React, { forwardRef } from 'react'
 import { cx, getPrefixCls } from '@hi-ui/classname'
 import { __DEV__ } from '@hi-ui/env'
 import { HiBaseHTMLProps } from '@hi-ui/core'
+import Spinner from '@hi-ui/spinner'
 
 const _role = 'button'
 const _prefix = getPrefixCls(_role)
@@ -32,7 +33,7 @@ export const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonPr
     const isNonInteractive = disabled || loading
 
     const prefix = loading ? (
-      <IconLoading className={`${prefixCls}__icon ${prefixCls}__icon--prefix`} />
+      <Spinner color="currentColor" className={`${prefixCls}__icon ${prefixCls}__icon--prefix`} />
     ) : icon && (!Array.isArray(icon) || icon[0]) ? (
       <span className={`${prefixCls}__icon ${prefixCls}__icon--prefix`}>
         {Array.isArray(icon) ? icon[0] : icon}
@@ -127,20 +128,4 @@ Button.HiName = 'Button'
 
 if (__DEV__) {
   Button.displayName = 'Button'
-}
-
-function IconLoading({ className = '', size = '0.8em' }) {
-  return (
-    <i className={cx(className)}>
-      <svg viewBox="0 0 18 18" width={size} height={size} fill="currentColor">
-        <g>
-          {/* <path
-            d="m9 18c-4.9706 0-9-4.0294-9-9 0-4.9706 4.0294-9 9-9 4.9706 0 9 4.0294 9 9 0 4.9706-4.0294 9-9 9zm0-2c3.866 0 7-3.134 7-7 0-3.866-3.134-7-7-7-3.866 0-7 3.134-7 7 0 3.866 3.134 7 7 7z"
-            opacity=".15"
-          /> */}
-          <path d="m15.547 2.8242c0.37904 0.40168 0.36068 1.0346-0.040996 1.4136-0.40168 0.37904-1.0346 0.36068-1.4136-0.040996-1.315-1.3935-3.1381-2.1969-5.0922-2.1969-3.866 0-7 3.134-7 7 0 0.55228-0.44772 1-1 1s-1-0.44772-1-1c0-4.9706 4.0294-9 9-9 2.5103 0 4.8578 1.0343 6.5468 2.8242z" />
-        </g>
-      </svg>
-    </i>
-  )
 }
