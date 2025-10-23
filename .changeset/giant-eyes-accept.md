69eb320a09ae0a3adaf4d4f7e6097275c60f648a
zhouyun1 <zhouyun1@xiaomi.com>
2025-10-23 19:22:22 +0800

    chore(locale-context): Transfer 组件增加文案 (5.0)

---
+++ .changeset/giant-eyes-accept.md
@@ -0,0 +1,6 @@
+---
+"@hi-ui/core": patch
+"@hi-ui/locale-context": patch
+---
+
+chore(locale-context): Transfer 组件增加文案 (5.0)
--- packages/ui/locale-context/src/locale/en-US.ts
+++ packages/ui/locale-context/src/locale/en-US.ts
@@ -93,6 +93,9 @@ export default {
     searchPlaceholder: 'Please search',
     emptyContent: 'No data',
     limit: 'Reached limit, unable to add',
+    allItems: 'All options',
+    checkedItems: 'Checked',
+    clear: 'Clear',
   },
   upload: {
     buttonText: 'Upload',
