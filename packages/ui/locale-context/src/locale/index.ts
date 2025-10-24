6aa41081d60f7d99f7f4803c93d50a72d3f7ad4e
gaoxiong1 <gaoxiong1@xiaomi.com>
2025-10-24 16:36:10 +0800

    feat(locale): add Thai (th-TH) localization support

--- packages/ui/locale-context/src/locale/index.ts
+++ packages/ui/locale-context/src/locale/index.ts
@@ -2,6 +2,7 @@ import zhCN from './zh-CN'
 import enUS from './en-US'
 import zhHK from './zh-HK'
 import zhTW from './zh-TW'
+import thTH from './th-TH'
 
 export default {
   'zh-CN': zhCN,
@@ -9,4 +10,5 @@ export default {
   'en-US': enUS,
   'zh-HK': zhHK,
   'zh-TW': zhTW,
+  'th-TH': thTH,
 }
