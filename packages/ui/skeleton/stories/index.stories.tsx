bc71ca692fbe5ad6d10c17c4beecb2971df15e4f
yejinran <yejinran@xiaomi.com>
2025-11-03 20:36:21 +0800

    fix: 修改storyboard页面分类

--- packages/ui/skeleton/stories/index.stories.tsx
+++ packages/ui/skeleton/stories/index.stories.tsx
@@ -9,7 +9,7 @@ export * from './usage.stories'
 export * from './group.stories'
 
 export default {
-  title: 'Skeleton',
+  title: 'Data Display/Skeleton',
   component: Skeleton,
   decorators: [(story: Function) => <div>{story()}</div>],
 }
