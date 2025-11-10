8e969ad731a6b709b1324a9eab12f4742f26c12c
zhouyun <zhouyun1@xiaomi.com>
2025-11-10 15:15:18 +0800

    feat(button): appearance 参数增加 solid 类型 (5.0) (#3401)

--- packages/ui/button/stories/shape.stories.tsx
+++ packages/ui/button/stories/shape.stories.tsx
@@ -14,40 +14,39 @@ export const Shape = () => {
     <>
       <h1>不同形状</h1>
       <div className="button-basic__wrap">
-        <div style={{ marginBottom: 24 }}>
-          <Button type="primary" size="xs">
-            超小号按钮
-          </Button>
-          <Button type="primary" icon={<PlusOutlined />} size="xs" />
-          <Button type="primary" size="sm">
-            小号按钮
-          </Button>
-          <Button type="primary" icon={<PlusOutlined />} size="sm" />
-          <Button type="primary">正常按钮</Button>
-          <Button type="primary" icon={<PlusOutlined />} />
-          <Button type="primary" size="lg">
-            大号按钮
-          </Button>
-          <Button type="primary" icon={<PlusOutlined />} size="lg" />
-        </div>
-        <div style={{ marginBottom: 24 }}>
-          <Button type="primary" shape="round" size="xs">
-            超小号按钮
-          </Button>
-          <Button type="primary" shape="round" icon={<PlusOutlined />} size="xs" />
-          <Button type="primary" shape="round" size="sm">
-            小号按钮
-          </Button>
-          <Button type="primary" shape="round" icon={<PlusOutlined />} size="sm" />
-          <Button type="primary" shape="round">
-            正常按钮
-          </Button>
-          <Button type="primary" shape="round" icon={<PlusOutlined />} />
-          <Button type="primary" shape="round" size="lg">
-            大号按钮
-          </Button>
-          <Button type="primary" shape="round" icon={<PlusOutlined />} size="lg" />
-        </div>
+        <h2>square</h2>
+        <Button type="primary" size="xs">
+          超小号按钮
+        </Button>
+        <Button type="primary" icon={<PlusOutlined />} size="xs" />
+        <Button type="primary" size="sm">
+          小号按钮
+        </Button>
+        <Button type="primary" icon={<PlusOutlined />} size="sm" />
+        <Button type="primary">正常按钮</Button>
+        <Button type="primary" icon={<PlusOutlined />} />
+        <Button type="primary" size="lg">
+          大号按钮
+        </Button>
+        <Button type="primary" icon={<PlusOutlined />} size="lg" />
+
+        <h2>round</h2>
+        <Button type="primary" shape="round" size="xs">
+          超小号按钮
+        </Button>
+        <Button type="primary" shape="round" icon={<PlusOutlined />} size="xs" />
+        <Button type="primary" shape="round" size="sm">
+          小号按钮
+        </Button>
+        <Button type="primary" shape="round" icon={<PlusOutlined />} size="sm" />
+        <Button type="primary" shape="round">
+          正常按钮
+        </Button>
+        <Button type="primary" shape="round" icon={<PlusOutlined />} />
+        <Button type="primary" shape="round" size="lg">
+          大号按钮
+        </Button>
+        <Button type="primary" shape="round" icon={<PlusOutlined />} size="lg" />
       </div>
     </>
   )
