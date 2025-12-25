dc130fcaf43befe3fd51721eaff67ee4823162e3
zhouyun1 <zhouyun1@xiaomi.com>
2025-12-25 21:03:33 +0800

    chore(table): add fixed column and update column widths in stories (5.0)

--- packages/ui/table/stories/resizable.stories.tsx
+++ packages/ui/table/stories/resizable.stories.tsx
@@ -41,12 +41,12 @@ export const Resizable = () => {
     {
       title: 'Phone',
       dataKey: 'phone',
-      width: 150,
+      width: 200,
     },
     {
       title: 'Phone2',
       dataKey: 'phone2',
-      width: 150,
+      width: 200,
     },
   ])
 
@@ -56,6 +56,8 @@ export const Resizable = () => {
       age: 31,
       address: '45 Sunbeam Lane, Mistville',
       email: 'raynor.mav@maildemo.net',
+      phone: '1234567890',
+      phone2: '1234567890',
       key: 1,
     },
     {
@@ -63,6 +65,8 @@ export const Resizable = () => {
       age: 26,
       address: '83 Dewdrop Road, Rivertown',
       email: 'elina.voss@sampleinbox.cc',
+      phone: '1234567890',
+      phone2: '1234567890',
       key: 2,
     },
     {
@@ -70,6 +74,8 @@ export const Resizable = () => {
       age: 37,
       address: '12 Blossom Close, Newcrest',
       email: 'darin.poe@mockpost.io',
+      phone: '1234567890',
+      phone2: '1234567890',
       key: 3,
     },
   ])
