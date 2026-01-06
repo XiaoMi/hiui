d2cdb25371397d644041a3624643678e9588a312
zhouyun <zhouyun1@xiaomi.com>
2026-01-06 17:41:02 +0800

    fix(table): 修复在stretchHeight模式下，冻结列表头和单元格层级问题 (5.0) (#3457)
    * fix(table): 修复在stretchHeight模式下，冻结列表头和单元格层级问题 (5.0)
    
    * chore(table): 代码调整

---
+++ .changeset/curvy-ravens-swim.md
@@ -0,0 +1,6 @@
+---
+"@hi-ui/hiui": patch
+"@hi-ui/table": patch
+---
+
+fix(table): 修复在 stretchHeight 模式下，冻结列表头和单元格层级问题 (5.0)
--- packages/ui/table/src/TheadContent.tsx
+++ packages/ui/table/src/TheadContent.tsx
@@ -68,7 +68,7 @@ export const TheadContent = forwardRef<HTMLDivElement | null, TheadContentProps>
                   }
                 }
 
-                const stickyColProps = getStickyColProps(col)
+                const stickyColProps = getStickyColProps(col, 'th')
 
                 const cell = (
                   <th
@@ -76,9 +76,9 @@ export const TheadContent = forwardRef<HTMLDivElement | null, TheadContentProps>
                     {...stickyColProps}
                     style={{
                       ...stickyColProps.style,
+                      insetBlockStart: trRefs.current[colsIndex]?.offsetTop,
                       // 表头合并场景下，被合并的表头需要隐藏
                       display: col?.colSpan === 0 ? 'none' : undefined,
-                      top: trRefs.current[colsIndex]?.offsetTop,
                     }}
                     className={cx(
                       `${prefixCls}-cell`,
