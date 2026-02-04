f9f1355b8554c009ae5c84fa21880f10aaeb4a02
Lanzhi <lizhan3@xiaomi.com>
2026-02-04 17:41:14 +0800

    feat(group): 新增@hi-ui/group

---
+++ packages/schema/group/README.md
@@ -0,0 +1,11 @@
+# `@hi-ui/group`
+
+> TODO: description
+
+## Usage
+
+```
+const group = require('@hi-ui/group');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/schema/group/src/index.ts
@@ -0,0 +1,6 @@
+export { GroupContainer } from './container'
+export type { GroupContainerProps } from './container'
+
+export { GroupPanel } from './panel'
+export type { GroupPanelProps } from './panel'
+export { getGroupId } from './_utils'
